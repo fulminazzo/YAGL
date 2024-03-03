3b525d0635c2e6b1f5af623e7a715036fff4ae1c
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 20:48:42 +0100

    Updated javadoc

--- item/src/main/java/it/angrybear/items/recipes/ShapedRecipe.java
+++ item/src/main/java/it/angrybear/items/recipes/ShapedRecipe.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 /**
  * An implementation of {@link Recipe} to express the shaped recipes in Minecraft.
+ * An example of shaped recipe is the TNT, which requires all the items in an exact position.
  */
 public class ShapedRecipe implements Recipe {
     @Getter
