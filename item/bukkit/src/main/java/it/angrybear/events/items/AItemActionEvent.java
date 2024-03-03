aec01405e2adf7f4065185dff0f93b4ef09fc3b2
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 00:59:49 +0100

    Implemented cancellable interface

--- item/bukkit/src/main/java/it/angrybear/events/items/AItemActionEvent.java
+++ item/bukkit/src/main/java/it/angrybear/events/items/AItemActionEvent.java
@@ -1,8 +1,10 @@
 package it.angrybear.events.items;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
@@ -12,11 +14,13 @@ import org.jetbrains.annotations.NotNull;
  * An abstract event that represents a call to a {@link it.angrybear.actions.BukkitItemAction#execute(Player, ItemStack)} method.
  */
 @Getter
-abstract class AItemActionEvent extends Event {
+abstract class AItemActionEvent extends Event implements Cancellable {
     @Getter
     private static final HandlerList handlerList = new HandlerList();
     protected final Player player;
     protected final ItemStack itemStack;
+    @Setter
+    protected boolean cancelled;
 
     /**
      * Instantiates a new item action event.
