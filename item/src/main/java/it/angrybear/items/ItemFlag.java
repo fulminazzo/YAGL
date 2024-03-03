d642725f80f82ce49f1040ffc4d69c5e9caadf90
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 17:29:43 +0100

    Created ItemFlag enum

---
+++ item/src/main/java/it/angrybear/items/ItemFlag.java
@@ -0,0 +1,13 @@
+package it.angrybear.items;
+
+/**
+ * Wrapper for Bukkit ItemFlag counterpart.
+ */
+public enum ItemFlag {
+    HIDE_ENCHANTS,
+    HIDE_ATTRIBUTES,
+    HIDE_UNBREAKABLE,
+    HIDE_DESTROYS,
+    HIDE_PLACED_ON,
+    HIDE_POTION_EFFECTS;
+}
