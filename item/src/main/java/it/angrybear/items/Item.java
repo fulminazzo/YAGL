cb7bdd219782854f6f233c3b0e0d340a96549bde
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 19:36:45 +0100

    Fixed JavaDoc

--- item/src/main/java/it/angrybear/items/Item.java
+++ item/src/main/java/it/angrybear/items/Item.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * An interface that represents the general item.
+ * An interface that represents an item.
  */
 @SuppressWarnings({"unused", "UnusedReturnValue"})
 public interface Item {
