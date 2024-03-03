3030f49596d32148439d3240c67aef3c2c55e9fc
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 19:51:55 +0100

    Added Javadoc Changed return type of every setter method to BukkitItem

--- item/bukkit/src/main/java/it/angrybear/items/BukkitItem.java
+++ item/bukkit/src/main/java/it/angrybear/items/BukkitItem.java
@@ -3,11 +3,41 @@ package it.angrybear.items;
 import it.angrybear.utils.MaterialUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+
+/**
+ * An implementation of {@link Item} for Bukkit.
+ */
 public class BukkitItem extends ItemImpl {
 
     @Override
-    public Item setMaterial(@NotNull String material) {
+    public BukkitItem setMaterial(@NotNull String material) {
         MaterialUtils.getMaterial(material, true);
-        return super.setMaterial(material);
+        return (BukkitItem) super.setMaterial(material);
+    }
+
+    @Override
+    public BukkitItem setAmount(final int amount) {
+        return (BukkitItem) super.setAmount(amount);
+    }
+
+    @Override
+    public BukkitItem setDurability(final short durability) {
+        return (BukkitItem) super.setDurability(durability);
+    }
+
+    @Override
+    public BukkitItem setDisplayName(final @NotNull String displayName) {
+        return (BukkitItem) super.setDisplayName(displayName);
+    }
+
+    @Override
+    public BukkitItem setLore(final @NotNull Collection<String> lore) {
+        return (BukkitItem) super.setLore(lore);
+    }
+
+    @Override
+    public BukkitItem setUnbreakable(final boolean unbreakable) {
+        return (BukkitItem) super.setUnbreakable(unbreakable);
     }
 }
