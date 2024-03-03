842548b8f7f767d54905aef198711f8324e435af
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 20:12:06 +0100

    Added MessageUtils class

---
+++ item/src/main/java/it/angrybear/utils/MessageUtils.java
@@ -0,0 +1,26 @@
+package it.angrybear.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * The type Message utils.
+ */
+public class MessageUtils {
+    private static final String COLOR_CHAR = "§";
+    private static final String COLOR_REGEX = "[A-Fa-f0-9]";
+
+    /**
+     * Recolors the given string using Minecraft default color codes.
+     *
+     * @param message the message
+     * @return the string
+     */
+    public static String color(String message) {
+        if (message == null) return null;
+        Matcher matcher = Pattern.compile("&(" + COLOR_REGEX + ")").matcher(message);
+        while (matcher.find())
+            message = message.replace(matcher.group(), COLOR_CHAR + matcher.group(1));
+        return message;
+    }
+}
