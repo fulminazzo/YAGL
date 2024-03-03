b335dc5e3c11f1799b282d9c5b6d7abddbfa6bdd
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 00:19:31 +0100

    Added listeners for drop, click and drag events

--- item/bukkit/src/main/java/it/angrybear/listeners/PersistentListener.java
+++ item/bukkit/src/main/java/it/angrybear/listeners/PersistentListener.java
@@ -1,11 +1,18 @@
 package it.angrybear.listeners;
 
 import it.angrybear.items.PersistentItem;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 
 /**
@@ -19,6 +26,36 @@ public class PersistentListener implements Listener {
      */
     public PersistentListener() {
         INITIALIZED = true;
+    }
+
+    @EventHandler
+    void on(PlayerDropItemEvent event) {
+        findPersistentItem(event.getItemDrop().getItemStack(), cancelled(event));
+    }
+
+    @EventHandler
+    void on(InventoryClickEvent event) {
+        findPersistentItem(event.getCurrentItem(), cancelled(event), () ->
+                findPersistentItem(event.getCursor(), cancelled(event)));
+    }
+
+    @EventHandler
+    void on(InventoryDragEvent event) {
+        findPersistentItem(event.getCursor(), cancelled(event), () -> findPersistentItem(event.getOldCursor(), cancelled(event), () -> {
+            Collection<ItemStack> items = event.getNewItems().values();
+            AtomicBoolean check = new AtomicBoolean(true);
+            for (ItemStack i : items) {
+                findPersistentItem(i, p -> {
+                    cancelled(event).accept(p);
+                    check.set(false);
+                });
+                if (!check.get()) break;
+            }
+        }));
+    }
+
+    private Consumer<PersistentItem> cancelled(Cancellable event) {
+        return p -> event.setCancelled(true);
     }
 
     private void findPersistentItem(final @Nullable ItemStack itemStack,
