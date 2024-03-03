cae45451e3edfcfd7624ca9d90af1b2907be8936
Fulminazzo <fulminazzo21@gmail.com>
2024-03-03 21:03:26 +0100

    Overridden setRecipe return type

--- item/bukkit/src/main/java/it/angrybear/items/BukkitRecipeItem.java
+++ item/bukkit/src/main/java/it/angrybear/items/BukkitRecipeItem.java
@@ -1,7 +1,13 @@
 package it.angrybear.items;
 
+import it.angrybear.items.recipes.Recipe;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * An implementation of {@link RecipeItem} and {@link BukkitItem}
  */
 public interface BukkitRecipeItem extends RecipeItem, BukkitItem {
+
+    @Override
+    BukkitRecipeItem setRecipe(@Nullable Recipe recipe);
 }
