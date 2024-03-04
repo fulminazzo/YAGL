b76a5b074efca4ab66a2e412ea6536f03a55f9d8
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 01:18:40 +0100

    Renamed to PreClickItemEvent

--- item/bukkit/src/main/java/it/angrybear/events/items/click/PreClickItemEvent.java
+++ item/bukkit/src/main/java/it/angrybear/events/items/click/PreClickItemEvent.java
@@ -11,17 +11,17 @@ import org.jetbrains.annotations.NotNull;
  * An event called <b>before</b> executing a {@link it.angrybear.actions.ClickItemAction}.
  */
 @Getter
-public class PreClickActionEvent extends PreItemActionEvent implements ClickEvent {
+public final class PreClickItemEvent extends PreItemActionEvent implements ClickEvent {
     private final ClickType clickType;
 
     /**
-     * Instantiates a new Pre click action event.
+     * Instantiates a new Pre click item event.
      *
      * @param player    the player
      * @param itemStack the item stack
      * @param clickType the click type
      */
-    public PreClickActionEvent(@NotNull Player player, @NotNull ItemStack itemStack, @NotNull ClickType clickType) {
+    public PreClickItemEvent(@NotNull Player player, @NotNull ItemStack itemStack, @NotNull ClickType clickType) {
         super(player, itemStack);
         this.clickType = clickType;
     }
