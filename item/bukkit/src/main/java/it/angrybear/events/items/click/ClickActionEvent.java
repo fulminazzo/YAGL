7da88ab7e50b91baa14c97402a7b10ad0a8271ef
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 01:07:01 +0100

    Created ClickActionEvent

---
+++ item/bukkit/src/main/java/it/angrybear/events/items/click/ClickActionEvent.java
@@ -0,0 +1,28 @@
+package it.angrybear.events.items.click;
+
+import it.angrybear.events.items.PreItemActionEvent;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An event called <b>after</b> executing a {@link it.angrybear.actions.ClickItemAction}.
+ */
+@Getter
+public class ClickActionEvent extends PreItemActionEvent implements ClickEvent {
+    private final ClickType clickType;
+
+    /**
+     * Instantiates a new Click action event.
+     *
+     * @param player    the player
+     * @param itemStack the item stack
+     * @param clickType the click type
+     */
+    public ClickActionEvent(@NotNull Player player, @NotNull ItemStack itemStack, @NotNull ClickType clickType) {
+        super(player, itemStack);
+        this.clickType = clickType;
+    }
+}
