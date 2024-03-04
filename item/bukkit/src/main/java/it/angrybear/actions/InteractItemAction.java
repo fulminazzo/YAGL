526c74d10d61d56fc80834617061dcc0c98b7792
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 01:33:05 +0100

    Created InteractItemAction

---
+++ item/bukkit/src/main/java/it/angrybear/actions/InteractItemAction.java
@@ -0,0 +1,24 @@
+package it.angrybear.actions;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The interface Interact item action.
+ */
+@FunctionalInterface
+public interface InteractItemAction {
+
+    /**
+     * Execute.
+     *
+     * @param player         the player
+     * @param itemStack      the item stack
+     * @param interactAction the interact action
+     * @deprecated this method should be used only for defining the interface. This will NOT call the relative events.
+     */
+    @Deprecated
+    void execute(final @NotNull Player player, final @NotNull ItemStack itemStack, final @NotNull Action interactAction);
+}
