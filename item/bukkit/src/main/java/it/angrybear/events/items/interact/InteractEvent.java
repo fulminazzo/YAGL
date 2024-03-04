9e03cf80ed72a56a5298744c2f2d8c4419406478
Fulminazzo <fulminazzo21@gmail.com>
2024-03-04 01:34:04 +0100

    Created InteractEvent

---
+++ item/bukkit/src/main/java/it/angrybear/events/items/interact/InteractEvent.java
@@ -0,0 +1,16 @@
+package it.angrybear.events.items.interact;
+
+import org.bukkit.event.block.Action;
+
+/**
+ * The interface Interact event.
+ */
+interface InteractEvent {
+
+    /**
+     * Gets interact action.
+     *
+     * @return the interact action
+     */
+    Action getInteractAction();
+}
