efcbb6803d03854a59ccba213410f9a16605fac5
Fulminazzo <fulminazzo21@gmail.com>
2024-03-05 12:12:47 +0100

    Created ItemParser and added test suite

---
+++ item/serializer/src/test/java/it/angrybear/parsers/ItemParserTest.java
@@ -0,0 +1,38 @@
+package it.angrybear.parsers;
+
+import it.angrybear.items.Item;
+import it.angrybear.items.fields.ItemFlag;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import it.fulminazzo.yamlparser.utils.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemParserTest {
+
+    @Test
+    void testSaveAndLoad() throws IOException {
+        FileConfiguration.addParsers(new EnchantmentParser());
+        FileConfiguration.addParsers(new ItemParser());
+
+        File output = new File("build/resources/test/item.yml");
+        if (output.exists()) FileUtils.deleteFile(output);
+        FileUtils.createNewFile(output);
+        Item item = Item.newItem().setMaterial("STONE").setAmount(2).setDurability(15)
+                .setDisplayName("Cool stone").setLore("Click on this", "To be OP")
+                .addEnchantment("enchant1", 10).addEnchantment("enchant2", 20)
+                .addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_DESTROYS)
+                .setUnbreakable(true)
+                .setCustomModelData(7);
+        FileConfiguration configuration = new FileConfiguration(output);
+        configuration.set("item", item);
+        configuration.save();
+
+        configuration = new FileConfiguration(output);
+        Item item2 = configuration.get("item", Item.class);
+        assertEquals(item, item2);
+    }
+}
