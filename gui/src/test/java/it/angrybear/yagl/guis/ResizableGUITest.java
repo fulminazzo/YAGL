8bbab11eaf67f03ccb0d30936b3641b0bc279924
Fulminazzo <fulminazzo21@gmail.com>
2024-03-19 16:45:11 +0100

    Added tests for add method

--- gui/src/test/java/it/angrybear/yagl/guis/ResizableGUITest.java
+++ gui/src/test/java/it/angrybear/yagl/guis/ResizableGUITest.java
@@ -1,9 +1,14 @@
 package it.angrybear.yagl.guis;
 
+import it.angrybear.yagl.contents.GUIContent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 class ResizableGUITest {
     private GUI gui;
@@ -11,6 +16,23 @@ class ResizableGUITest {
     @BeforeEach
     void setUp() {
         this.gui = new ResizableGUI(9);
+    }
+
+    @Test
+    void testValidAdd() {
+        List<GUIContent> contents = new LinkedList<>();
+        for (int i = 0; i < GUIImpl.MAX_SIZE; i++)
+            contents.add(new GUIImplTest.MockContent());
+        assertDoesNotThrow(() -> this.gui.addContent(contents.toArray(new GUIContent[0])));
+    }
+
+    @Test
+    void testInvalidAdd() {
+        List<GUIContent> contents = new LinkedList<>();
+        for (int i = 0; i < GUIImpl.MAX_SIZE + 1; i++)
+            contents.add(new GUIImplTest.MockContent());
+        assertThrowsExactly(IllegalArgumentException.class, () ->
+                this.gui.addContent(contents.toArray(new GUIContent[0])));
     }
 
     @Test
