87a59575d10d0a91489b9347907e1be3bd9b606a
Fulminazzo <fulminazzo21@gmail.com>
2024-03-19 15:41:55 +0100

    Created GUIContent interface

---
+++ gui/src/main/java/it/angrybear/yagl/contents/GUIContent.java
@@ -0,0 +1,62 @@
+package it.angrybear.yagl.contents;
+
+import it.angrybear.yagl.viewers.Viewer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Predicate;
+
+/**
+ * Represents the generic content of a GUI.
+ */
+public interface GUIContent {
+
+    /**
+     * Sets priority.
+     * If two {@link GUIContent} share the same slot,
+     * only the one with higher priority will be shown.
+     *
+     * @param priority the priority
+     * @return this content
+     */
+    @NotNull GUIContent setPriority(final int priority);
+
+    /**
+     * Gets priority.
+     *
+     * @return the priority
+     */
+    int getPriority();
+
+    /**
+     * Sets the sound played upon clicking on the content in the GUI.
+     * This is NOT checked until the sound is actually played.
+     *
+     * @param rawSound the raw sound
+     * @return this content
+     */
+    @NotNull GUIContent setClickSound(final String rawSound);
+
+    /**
+     * Gets sound.
+     *
+     * @return the sound, if set
+     */
+    @Nullable String getSound();
+
+    /**
+     * Sets the given predicate as a checker for {@link #hasViewRequirements(Viewer)}.
+     *
+     * @param requirements the requirements
+     * @return this content
+     */
+    @NotNull GUIContent setViewRequirements(final @NotNull Predicate<? super Viewer> requirements);
+
+    /**
+     * Check if the given {@link Viewer} has enough requirements to view this content.
+     *
+     * @param viewer the viewer
+     * @return the boolean
+     */
+    boolean hasViewRequirements(final @NotNull Viewer viewer);
+}
