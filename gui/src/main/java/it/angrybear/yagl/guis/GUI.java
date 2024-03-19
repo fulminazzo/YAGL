2af68574c8a43fb44f530045fbb982a0ab64b075
Fulminazzo <fulminazzo21@gmail.com>
2024-03-19 16:03:40 +0100

    Created GUI interface

---
+++ gui/src/main/java/it/angrybear/yagl/guis/GUI.java
@@ -0,0 +1,91 @@
+package it.angrybear.yagl.guis;
+
+import it.angrybear.yagl.contents.GUIContent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * The general interface to represent a GUI.
+ */
+public interface GUI extends Iterable<GUIContent> {
+
+    /**
+     * Gets the previous GUI.
+     *
+     * @return the previous
+     */
+    @Nullable GUI getPrevious();
+
+    /**
+     * Gets the next GUI.
+     *
+     * @return the next
+     */
+    @Nullable GUI getNext();
+
+    /**
+     * Gets the GUI opened before opening this GUI (not previous).
+     *
+     * @return the back
+     */
+    @Nullable GUI getBack();
+
+    /**
+     * Gets the content at the given slot.
+     *
+     * @param slot the slot
+     * @return the content
+     */
+    @Nullable GUIContent getContent(int slot);
+
+    /**
+     * Gets size.
+     *
+     * @return the size
+     */
+    int getSize();
+
+    /**
+     * Checks if the content at the given slot is movable.
+     *
+     * @param slot the slot
+     * @return true if it is
+     */
+    boolean isMovable(int slot);
+
+    /**
+     * Tries to add all the contents in the GUI.
+     * If it fails (because of empty GUI) it throws an {@link IllegalStateException}.
+     *
+     * @param contents the contents
+     * @return this gui
+     */
+    @NotNull GUI addContent(final GUIContent @NotNull ... contents);
+
+    /**
+     * Sets the content at the given index.
+     *
+     * @param slot    the slot
+     * @param content the content
+     * @return this gui
+     */
+    @NotNull GUI setContent(int slot, final @NotNull GUIContent content);
+
+    /**
+     * Removes the content from the given index.
+     *
+     * @param slot the slot
+     * @return this gui
+     */
+    @NotNull GUI unsetContent(int slot);
+
+    /**
+     * Gets a copy of all the contents.
+     * To get the actual content, use the GUI in a for-enhanced loop or use {@link #iterator()}.
+     *
+     * @return the contents
+     */
+    @NotNull List<GUIContent> getContents();
+}
