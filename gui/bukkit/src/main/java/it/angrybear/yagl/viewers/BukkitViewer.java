709321908642fefdd2d197ea4011896359b75636
Fulminazzo <fulminazzo21@gmail.com>
2024-03-19 17:33:34 +0100

    Created raw implementation of BukkitViewer

---
+++ gui/bukkit/src/main/java/it/angrybear/yagl/viewers/BukkitViewer.java
@@ -0,0 +1,66 @@
+package it.angrybear.yagl.viewers;
+
+import it.angrybear.yagl.contents.GUIContent;
+import it.angrybear.yagl.guis.GUI;
+import it.angrybear.yagl.items.Item;
+import it.angrybear.yagl.listeners.GUIListener;
+import it.angrybear.yagl.utils.MessageUtils;
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class BukkitViewer extends Viewer {
+
+    /**
+     * Instantiates a new Viewer.
+     *
+     * @param uniqueId the unique id
+     * @param name     the name
+     */
+    BukkitViewer(UUID uniqueId, String name) {
+        super(uniqueId, name);
+    }
+
+    @Override
+    //TODO:
+    public void openGUI(@NotNull GUI gui) {
+        Player player = getPlayer();
+        if (player == null) throw new IllegalStateException(String.format("%s is not online", this.name));
+        Inventory inventory = Bukkit.createInventory(null, gui.getSize(), MessageUtils.color(gui.getTitle()));
+        for (int i = 0; i < gui.getSize(); i++) {
+            GUIContent content = gui.getContent(i);
+            if (content != null) {
+                ItemStack o = new Refl<>(content.render())
+                        .invokeMethodRefl("copy", ReflectionUtils.getClass(Item.class.getCanonicalName()
+                                .replace("Item", "BukkitItem")))
+                        .invokeMethod("create");
+                inventory.setItem(i, o);
+            }
+        }
+        GUIListener.openGUI(this, gui);
+        player.openInventory(inventory);
+    }
+
+    @Override
+    public boolean hasPermission(@NotNull String permission) {
+        Player player = getPlayer();
+        return player != null && player.hasPermission(permission);
+    }
+
+    private Player getPlayer() {
+        Player player = Bukkit.getPlayer(this.uniqueId);
+        if (player == null) return null;
+        else if (player.isOnline()) return player;
+        else return null;
+    }
+
+    public static Viewer newViewer(final @NotNull Player player) {
+        return new BukkitViewer(player.getUniqueId(), player.getName());
+    }
+}
