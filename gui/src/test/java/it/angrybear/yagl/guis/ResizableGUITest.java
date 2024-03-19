151db77f3893f58dc5b6f45a84a7f518e00fefc6
Fulminazzo <fulminazzo21@gmail.com>
2024-03-19 16:42:36 +0100

    Added test suite for ResizableGUI

---
+++ gui/src/test/java/it/angrybear/yagl/guis/ResizableGUITest.java
@@ -0,0 +1,20 @@
+package it.angrybear.yagl.guis;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResizableGUITest {
+    private GUI gui;
+
+    @BeforeEach
+    void setUp() {
+        this.gui = new ResizableGUI(9);
+    }
+
+    @Test
+    void testValidSet() {
+        assertDoesNotThrow(() -> this.gui.setContent(9, new GUIImplTest.MockContent()));
+    }
+}
