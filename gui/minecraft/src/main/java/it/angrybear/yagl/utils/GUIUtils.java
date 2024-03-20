5ba3b225455e5a2d4c039caccd027ba64e17df38
Fulminazzo <fulminazzo21@gmail.com>
2024-03-20 10:37:28 +0100

    Added guiToInventoryType function

--- gui/minecraft/src/main/java/it/angrybear/yagl/utils/GUIUtils.java
+++ gui/minecraft/src/main/java/it/angrybear/yagl/utils/GUIUtils.java
@@ -1,7 +1,10 @@
 package it.angrybear.yagl.utils;
 
 import it.angrybear.yagl.guis.GUI;
+import it.angrybear.yagl.guis.GUIType;
+import it.angrybear.yagl.guis.TypeGUI;
 import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,5 +25,15 @@ public class GUIUtils {
         if (title == null) inventory = Bukkit.createInventory(null, gui.getSize());
         else inventory = Bukkit.createInventory(null, gui.getSize(), title);
         return inventory;
+    }
+
+    /**
+     * Converts the given {@link GUIType} to a {@link InventoryType}.
+     *
+     * @param guiType the gui type
+     * @return the inventory type
+     */
+    public static InventoryType guiToInventoryType(final @NotNull GUIType guiType) {
+        return InventoryType.valueOf(guiType.name());
     }
 }
