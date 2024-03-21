ff7a925273c416c65b9fc1151aad5bfd0f99ddc6
Fulminazzo <fulminazzo21@gmail.com>
2024-03-21 18:00:49 +0100

    Added test suite for action parsers

---
+++ gui/serializer/src/test/java/it/angrybear/yagl/actions/ActionParsersTest.java
@@ -0,0 +1,36 @@
+package it.angrybear.yagl.actions;
+
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import it.fulminazzo.yamlparser.utils.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionParsersTest {
+
+    private static CommandAction[] getActions() {
+        return new CommandAction[]{
+                new BiGUICommand("command"),
+                new GUIItemCommand("command2"),
+                new GUICommand("command3"),
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("getActions")
+    void testCommandActions(CommandAction action) throws IOException {
+        ActionParsers.addParsers();
+        File file = new File("build/resources/test/command-action.yml");
+        if (file.exists()) FileUtils.deleteFile(file);
+        FileUtils.createNewFile(file);
+        FileConfiguration configuration = new FileConfiguration(file);
+        configuration.set("tmp", action);
+        CommandAction action1 = configuration.get("tmp", action.getClass());
+        assertEquals(action, action1);
+    }
+}
