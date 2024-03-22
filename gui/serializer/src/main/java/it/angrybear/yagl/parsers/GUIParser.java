a816531d2db00c31a157159024a832338d6e1b08
Fulminazzo <fulminazzo21@gmail.com>
2024-03-22 22:36:32 +0100

    Reimplemented using GUIElementParser

--- gui/serializer/src/main/java/it/angrybear/yagl/parsers/GUIParser.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/parsers/GUIParser.java
@@ -4,26 +4,16 @@ import it.angrybear.yagl.guis.GUI;
 import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
 import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
 import it.fulminazzo.fulmicollection.objects.Refl;
-import it.fulminazzo.fulmicollection.utils.ClassUtils;
 import it.fulminazzo.yamlparser.configuration.IConfiguration;
-import it.fulminazzo.yamlparser.parsers.CallableYAMLParser;
-import it.fulminazzo.yamlparser.utils.FileUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Set;
 
-public class GUIParser extends CallableYAMLParser<GUI> {
+public class GUIParser extends GUIElementParser<GUI> {
 
     public GUIParser() {
-        super(GUI.class, (c) -> {
-            String type = c.getString("type");
-            if (type == null) throw new NullPointerException("'type' cannot be null");
-            Class<? extends GUI> clazz = typeToContentClass(type);
-            Refl<? extends GUI> gui = new Refl<>(clazz, new Object[0]);
-            return gui.getObject();
-        });
+        super(GUI.class);
     }
 
     @Override
@@ -45,30 +35,7 @@ public class GUIParser extends CallableYAMLParser<GUI> {
         return (c, s, g) -> {
             super.getDumper().accept(c, s, g);
             if (g == null) return;
-            c.set(s + ".type", contentClassToType(g.getClass()));
             c.set(s + ".size", g.getSize());
         };
-    }
-
-    @SuppressWarnings("unchecked")
-    private static Class<? extends GUI> typeToContentClass(final @NotNull String type) {
-        Class<?> mainClass = GUI.class;
-        String packageName = mainClass.getPackage().getName();
-        final @NotNull Set<Class<?>> classes = ClassUtils.findClassesInPackage(packageName);
-        for (Class<?> clazz : classes)
-            if (!mainClass.equals(clazz) && mainClass.isAssignableFrom(clazz)) {
-                Class<? extends GUI> guiClazz = (Class<? extends GUI>) clazz;
-                if (contentClassToType(guiClazz).equals(type)) return guiClazz;
-            }
-        throw new RuntimeException(String.format("Could not find corresponding %s class from type '%s'",
-                GUI.class.getSimpleName(), type));
-    }
-
-    private static String contentClassToType(final @NotNull Class<? extends GUI> clazz) {
-        String name = clazz.getSimpleName();
-        final String guiContent = GUI.class.getSimpleName();
-        if (name.endsWith(guiContent)) name = name.substring(0, name.length() - guiContent.length());
-        name = FileUtils.formatStringToYaml(name);
-        return name.replace("-", "_").toUpperCase();
     }
 }
