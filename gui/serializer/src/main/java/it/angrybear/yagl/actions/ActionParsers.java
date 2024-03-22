2adb42cbfc991f3029b08e59038ae4459a082da2
Fulminazzo <fulminazzo21@gmail.com>
2024-03-22 23:41:58 +0100

    Rewritten addParsers with the three main Action functions

--- gui/serializer/src/main/java/it/angrybear/yagl/actions/ActionParsers.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/actions/ActionParsers.java
@@ -1,23 +1,13 @@
 package it.angrybear.yagl.actions;
 
-import it.angrybear.yagl.parsers.CommandActionParser;
-import it.fulminazzo.fulmicollection.utils.ClassUtils;
+import it.angrybear.yagl.parsers.SerializableFunctionParser;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
-import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Modifier;
-import java.util.Set;
 
 public class ActionParsers {
 
     public static void addParsers() {
-        final String packageName = ActionParsers.class.getPackage().getName();
-        try {
-            Class<?> commandAction = Class.forName(packageName + ".CommandAction");
-            @NotNull Set<Class<?>> classes = ClassUtils.findClassesInPackage(packageName);
-            for (Class<?> clazz : classes)
-                if (!Modifier.isAbstract(clazz.getModifiers()) && commandAction.isAssignableFrom(clazz))
-                    FileConfiguration.addParsers(new CommandActionParser<>(clazz));
-        } catch (ClassNotFoundException ignored) {}
+        FileConfiguration.addParsers(new SerializableFunctionParser<>(GUIAction.class));
+        FileConfiguration.addParsers(new SerializableFunctionParser<>(GUIItemAction.class));
+        FileConfiguration.addParsers(new SerializableFunctionParser<>(BiGUIAction.class));
     }
 }
