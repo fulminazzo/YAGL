dbf53c6ed1e232004b925251e6ac43a96392a60a
Fulminazzo <fulminazzo21@gmail.com>
2024-03-22 22:26:35 +0100

    Added automatic parsing of actions and contents

--- gui/serializer/src/main/java/it/angrybear/yagl/parsers/YAGLParser.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/parsers/YAGLParser.java
@@ -1,10 +1,14 @@
 package it.angrybear.yagl.parsers;
 
+import it.angrybear.yagl.actions.ActionParsers;
+import it.angrybear.yagl.guis.ContentsParser;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 
 public class YAGLParser {
 
     public static void addAllParsers() {
         FileConfiguration.addParsers(YAGLParser.class.getPackage().getName());
+        ActionParsers.addParsers();
+        FileConfiguration.addParsers(new ContentsParser());
     }
 }
