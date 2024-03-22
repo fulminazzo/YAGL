754d6dbf1f9ce121472ddc3ec1ccb172b523a0dc
Fulminazzo <fulminazzo21@gmail.com>
2024-03-22 23:48:39 +0100

    Reimplemented without predicate

--- gui/src/main/java/it/angrybear/yagl/contents/RequirementChecker.java
+++ gui/src/main/java/it/angrybear/yagl/contents/RequirementChecker.java
@@ -3,8 +3,8 @@ package it.angrybear.yagl.contents;
 import it.angrybear.yagl.SerializableFunction;
 import it.angrybear.yagl.viewers.Viewer;
 
-import java.util.function.Predicate;
-
 @FunctionalInterface
-public interface RequirementChecker extends SerializableFunction, Predicate<Viewer> {
+public interface RequirementChecker extends SerializableFunction {
+
+    boolean test(Viewer viewer);
 }
