ab0fd03dacc1e643f397f716f8cbbeadee67d36a
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 19:52:02 +0100

    Created ParserUtils to convert classes to types and vice-versa

---
+++ common/serializer/src/main/java/it/angrybear/yagl/utils/ParserUtils.java
@@ -0,0 +1,32 @@
+package it.angrybear.yagl.utils;
+
+import it.fulminazzo.fulmicollection.utils.ClassUtils;
+import it.fulminazzo.yamlparser.utils.FileUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class ParserUtils {
+
+    @SuppressWarnings("unchecked")
+    protected static <C> Class<? extends C> typeToClass(final Class<C> coreClass, final @NotNull String toConvert) {
+        String packageName = coreClass.getPackage().getName();
+        final @NotNull Set<Class<?>> classes = ClassUtils.findClassesInPackage(packageName);
+        for (Class<?> clazz : classes)
+            if (!coreClass.equals(clazz) && coreClass.isAssignableFrom(clazz)) {
+                Class<? extends C> guiClazz = (Class<? extends C>) clazz;
+                if (classToType(coreClass, guiClazz).equals(toConvert)) return guiClazz;
+            }
+        throw new RuntimeException(String.format("Could not find corresponding %s class from type '%s'",
+                coreClass.getSimpleName(), toConvert));
+    }
+
+    protected static <C> String classToType(final Class<C> coreClass, final @NotNull Class<? extends C> toConvert) {
+        final String mainClassName = coreClass.getSimpleName();
+        String name = toConvert.getSimpleName();
+        if (name.contains("$")) name = mainClassName;
+        else if (name.endsWith(mainClassName)) name = name.substring(0, name.length() - mainClassName.length());
+        name = FileUtils.formatStringToYaml(name);
+        return name.replace("-", "_").toUpperCase();
+    }
+}
