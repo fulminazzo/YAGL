71c1ec3754ed853563f9353b6d59c87e622d84c6
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 18:12:48 +0100

    Created Wrapper class to easily compare and print wrapper classes

---
+++ wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Wrapper.java
@@ -0,0 +1,32 @@
+package it.angrybear.yagl.wrappers;
+
+import it.fulminazzo.fulmicollection.objects.Printable;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+abstract class Wrapper {
+
+    /**
+     * Compare this wrapper with the given one.
+     *
+     * @param wrapper the wrapper
+     * @return true if they are equal
+     */
+    public boolean equals(final @Nullable Wrapper wrapper) {
+        if (wrapper == null) return false;
+        return ReflectionUtils.equalsFields(this, wrapper);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Wrapper) return equals((Wrapper) o);
+        return super.equals(o);
+    }
+
+    @Override
+    public String toString() {
+        return Printable.convertToJson(this);
+    }
+}
