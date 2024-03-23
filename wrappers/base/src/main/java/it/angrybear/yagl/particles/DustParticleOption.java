c57c7334507c6c5057aece30c66aa6f4232fbd92
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 19:32:35 +0100

    Created DustParticleOption class

---
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/DustParticleOption.java
@@ -0,0 +1,22 @@
+package it.angrybear.yagl.particles;
+
+import it.angrybear.yagl.Color;
+import it.fulminazzo.fulmicollection.structures.Tuple;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class DustParticleOption extends ParticleOption<Tuple<Color, Float>> {
+    private final Color color;
+    private final float size;
+
+    public DustParticleOption(final @NotNull Color color, final float size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    @Override
+    public Tuple<Color, Float> getOption() {
+        return new Tuple<>(this.color, this.size);
+    }
+}
