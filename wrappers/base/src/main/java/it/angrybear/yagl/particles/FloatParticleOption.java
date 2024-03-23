418e91bfa58ff87d3dcac60fac0aa1e75286af32
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 19:03:13 +0100

    Created specific implementations of PrimitiveParticleOption

---
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/FloatParticleOption.java
@@ -0,0 +1,16 @@
+package it.angrybear.yagl.particles;
+
+/**
+ * The type Float particle option.
+ */
+public class FloatParticleOption extends PrimitiveParticleOption<Float> {
+
+    /**
+     * Instantiates a new Float particle option.
+     *
+     * @param value the value
+     */
+    public FloatParticleOption(Float value) {
+        super(value);
+    }
+}
---
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/IntegerParticleOption.java
@@ -0,0 +1,16 @@
+package it.angrybear.yagl.particles;
+
+/**
+ * The type Integer particle option.
+ */
+public class IntegerParticleOption extends PrimitiveParticleOption<Integer> {
+
+    /**
+     * Instantiates a new Integer particle option.
+     *
+     * @param value the value
+     */
+    public IntegerParticleOption(Integer value) {
+        super(value);
+    }
+}
