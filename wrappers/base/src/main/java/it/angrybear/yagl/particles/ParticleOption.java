69335692cdf67d3668d1b6548b35456b1820ce8d
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 19:08:55 +0100

    Added getOption method and javadoc

--- wrappers/base/src/main/java/it/angrybear/yagl/particles/ParticleOption.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/ParticleOption.java
@@ -1,4 +1,17 @@
 package it.angrybear.yagl.particles;
 
-abstract class ParticleOption {
+/**
+ * Represents a general option for a particle.
+ * This can either be an integer, float, or a full-fledged object.
+ *
+ * @param <O> the type parameter
+ */
+abstract class ParticleOption<O> {
+
+    /**
+     * Gets option to send the particle.
+     *
+     * @return the option
+     */
+    public abstract O getOption();
 }
