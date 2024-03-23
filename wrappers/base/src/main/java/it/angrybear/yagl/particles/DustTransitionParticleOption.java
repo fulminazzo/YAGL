33e877573f9a69e780fc6dfe413697e1d33251aa
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 19:36:01 +0100

    Created DustTransitionParticleOption class

---
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/DustTransitionParticleOption.java
@@ -0,0 +1,22 @@
+package it.angrybear.yagl.particles;
+
+import it.angrybear.yagl.Color;
+import it.fulminazzo.fulmicollection.structures.Triple;
+import org.jetbrains.annotations.NotNull;
+
+public class DustTransitionParticleOption extends ParticleOption<Triple<Color, Color, Float>> {
+    private final Color from;
+    private final Color to;
+    private final float size;
+
+    public DustTransitionParticleOption(final @NotNull Color from, final @NotNull Color color, final float size) {
+        this.from = from;
+        to = color;
+        this.size = size;
+    }
+
+    @Override
+    public Triple<Color, Color, Float> getOption() {
+        return new Triple<>(this.from, this.to, this.size);
+    }
+}
