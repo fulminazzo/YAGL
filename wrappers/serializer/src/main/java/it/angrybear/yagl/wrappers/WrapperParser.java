ef81ac4eccf516810327ea95056c884bf3fc7873
Fulminazzo <fulminazzo21@gmail.com>
2024-03-23 20:30:28 +0100

    Created WrapperParser to parse Wrapper classes

---
+++ wrappers/serializer/src/main/java/it/angrybear/yagl/wrappers/WrapperParser.java
@@ -0,0 +1,71 @@
+package it.angrybear.yagl.wrappers;
+
+import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
+import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import it.fulminazzo.yamlparser.parsers.YAMLParser;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Comparator;
+
+@SuppressWarnings("unchecked")
+public class WrapperParser<W extends Wrapper> extends YAMLParser<W> {
+
+    public WrapperParser(Class<W> clazz) {
+        super(clazz);
+    }
+
+    @Override
+    protected BiFunctionException<@NotNull IConfiguration, @NotNull String, @Nullable W> getLoader() {
+        return (c, s) -> {
+            String raw = c.getString(s);
+            if (raw == null || raw.trim().isEmpty()) return null;
+            String[] tmp = raw.split(":");
+            Constructor<W> constructor = (Constructor<W>) Arrays.stream(getOClass().getConstructors())
+                    .filter(t -> t.getParameterCount() <= tmp.length)
+                    .min(Comparator.comparing(t -> -t.getParameterCount())).orElse(null);
+            if (constructor == null) {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < tmp.length; i++) builder.append("?, ");
+                throw new NoSuchMethodException(String.format("Could not find method %s(%s)",
+                        getOClass().getSimpleName(), builder.substring(0, Math.max(0, builder.length() - 2))));
+            }
+
+            Object[] parameters = new Object[tmp.length];
+
+            Class<?>[] types = constructor.getParameterTypes();
+            for (int i = 0; i < types.length; i++) {
+                Class<?> type = types[i];
+                if (!ReflectionUtils.isPrimitiveOrWrapper(type))
+                    throw new IllegalArgumentException(String.format("Cannot parse type %s", type.getCanonicalName()));
+                type = ReflectionUtils.getWrapperClass(type);
+                String t = tmp[i];
+                if (type.equals(String.class)) parameters[i] = t;
+                else parameters[i] = new Refl<>(type).invokeMethod("valueOf", t);
+            }
+
+            return new Refl<>(getOClass(), parameters).getObject();
+        };
+    }
+
+    @Override
+    protected TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable W> getDumper() {
+        return (c, s, w) -> {
+            c.set(s, null);
+            if (w == null) return;
+            StringBuilder tmp = new StringBuilder();
+            Refl<?> wRefl = new Refl<>(w);
+            for (Field field : wRefl.getNonStaticFields()) {
+                Object o = wRefl.getFieldObject(field);
+                tmp.append(o == null ? "" : o.toString()).append(":");
+            }
+            c.set(s, tmp.substring(0, Math.max(0, tmp.length() - 1)));
+        };
+    }
+}
