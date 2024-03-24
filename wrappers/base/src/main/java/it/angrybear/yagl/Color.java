651e348250e8459264cd2823fef8ef954fcdd439
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 00:24:05 +0100

    Extended classenum and added javadoc

--- wrappers/base/src/main/java/it/angrybear/yagl/Color.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/Color.java
@@ -1,27 +1,80 @@
 package it.angrybear.yagl;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.LinkedList;
 
 /**
  * The type Color.
  */
-public class Color {
+public class Color extends ClassEnum {
+    /**
+     * The constant WHITE.
+     */
     public static final Color WHITE = fromARGB("#FFFFFFFF");
+    /**
+     * The constant SILVER.
+     */
     public static final Color SILVER = fromARGB("#FFC0C0C0");
+    /**
+     * The constant GRAY.
+     */
     public static final Color GRAY = fromARGB("#FF808080");
+    /**
+     * The constant BLACK.
+     */
     public static final Color BLACK = fromARGB("#FF000000");
+    /**
+     * The constant RED.
+     */
     public static final Color RED = fromARGB("#FFFF0000");
+    /**
+     * The constant MAROON.
+     */
     public static final Color MAROON = fromARGB("#FF800000");
+    /**
+     * The constant YELLOW.
+     */
     public static final Color YELLOW = fromARGB("#FFFFFF00");
+    /**
+     * The constant OLIVE.
+     */
     public static final Color OLIVE = fromARGB("#FF808000");
+    /**
+     * The constant LIME.
+     */
     public static final Color LIME = fromARGB("#FF00FF00");
+    /**
+     * The constant GREEN.
+     */
     public static final Color GREEN = fromARGB("#FF008000");
+    /**
+     * The constant AQUA.
+     */
     public static final Color AQUA = fromARGB("#FF00FFFF");
+    /**
+     * The constant TEAL.
+     */
     public static final Color TEAL = fromARGB("#FF008080");
+    /**
+     * The constant BLUE.
+     */
     public static final Color BLUE = fromARGB("#FF0000FF");
+    /**
+     * The constant NAVY.
+     */
     public static final Color NAVY = fromARGB("#FF000080");
+    /**
+     * The constant FUCHSIA.
+     */
     public static final Color FUCHSIA = fromARGB("#FFFF00FF");
+    /**
+     * The constant PURPLE.
+     */
     public static final Color PURPLE = fromARGB("#FF800080");
+    /**
+     * The constant ORANGE.
+     */
     public static final Color ORANGE = fromARGB("#FFFFA500");
     private static final int MAX = 255;
 
@@ -103,5 +156,24 @@ public class Color {
         }
         if (nums.size() < 4) nums.addFirst(MAX);
         return new Color(nums.get(0), nums.get(1), nums.get(2), nums.get(3));
+    }
+
+    /**
+     * Gets the corresponding static color
+     *
+     * @param color the color name
+     * @return the color
+     */
+    public static Color valueOf(final @NotNull String color) {
+        return valueOf(color, Color.class);
+    }
+
+    /**
+     * Gets all the static colors.
+     *
+     * @return the colors
+     */
+    public static Color[] values() {
+        return values(Color.class);
     }
 }
