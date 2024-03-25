316a3fbea91fc0553ad5e5db10b0f32a75d38c76
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 22:53:22 +0100

    Added javadoc

--- wrappers/base/src/main/java/it/angrybear/yagl/structures/EnchantmentSet.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/structures/EnchantmentSet.java
@@ -2,8 +2,16 @@ package it.angrybear.yagl.structures;
 
 import it.angrybear.yagl.wrappers.Enchantment;
 
+/**
+ * A {@link java.util.Set} that uses {@link Enchantment#compareLevels(Enchantment)} and
+ * {@link Enchantment#isSimilar(Enchantment)} to compare enchantments.
+ * One is added only if the previous two are false.
+ */
 public class EnchantmentSet extends PComparatorSet<Enchantment> {
 
+    /**
+     * Instantiates a new Enchantment set.
+     */
     public EnchantmentSet() {
         super(Enchantment::compareLevels, Enchantment::isSimilar);
     }
