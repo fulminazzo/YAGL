cc2efa89ef952ae0bb0014f877719a4e2ff44003
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 13:15:06 +0100

    Extracted valueOf method to support multiple method names

--- common/base/src/main/java/it/angrybear/yagl/utils/EnumUtils.java
+++ common/base/src/main/java/it/angrybear/yagl/utils/EnumUtils.java
@@ -10,34 +10,52 @@ import java.util.regex.Pattern;
 /**
  * The type Enum utils.
  */
-public class EnumUtils {
+public class EnumUtils {//
 
     /**
      * Uses the given class method <code>valueOf</code> to obtain the corresponding value with the given name.
      * If none is found, an {@link IllegalArgumentException} is thrown.
      *
-     * @param <E>       the type parameter
-     * @param enumClass the enum class
-     * @param name      the name
+     * @param <E>         the type parameter
+     * @param enumClass   the enum class
+     * @param name        the name
      * @return the value
      */
     public static <E> @NotNull E valueOf(final @NotNull Class<E> enumClass, final @NotNull String name) {
+        return valueOf(enumClass, name, "valueOf");
+    }
+
+    /**
+     * Uses the given methods from the class to obtain the corresponding value with the given name.
+     * If none is found, an {@link IllegalArgumentException} is thrown.
+     *
+     * @param <E>         the type parameter
+     * @param enumClass   the enum class
+     * @param name        the name
+     * @param methodNames the method names
+     * @return the value
+     */
+    public static <E> @NotNull E valueOf(final @NotNull Class<E> enumClass, final @NotNull String name, final String @NotNull ... methodNames) {
         try {
-            E object = new Refl<>(enumClass).invokeMethod("valueOf", name.toUpperCase());
+            final Refl<Class<E>> enumRefl = new Refl<>(enumClass);
+            E object = null;
+            for (final String methodName : methodNames)
+                try {
+                    object = enumRefl.invokeMethod(enumClass, methodName, name.toUpperCase());
+                    if (object != null) break;
+                } catch (RuntimeException e) {
+                    Throwable throwable = ExceptionUtils.unwrapRuntimeException(e);
+                    if (!(throwable instanceof IllegalArgumentException)) ExceptionUtils.throwException(throwable);
+                }
             if (object == null) throw new IllegalArgumentException();
             return object;
-        } catch (RuntimeException e) {
-            Throwable throwable = ExceptionUtils.unwrapRuntimeException(e);
-            if (throwable instanceof IllegalArgumentException) {
-                String typeName = enumClass.getSimpleName();
-                Matcher matcher = Pattern.compile("[A-Z]").matcher(typeName);
-                while (matcher.find())
-                    typeName = typeName.replaceAll(matcher.group(), " " + matcher.group());
-                while (typeName.startsWith(" ")) typeName = typeName.substring(1);
-                throw new IllegalArgumentException(String.format("Could not find %s '%s'", typeName.toLowerCase(), name));
-            } else if (throwable instanceof RuntimeException)
-                throw ((RuntimeException) throwable);
-            else throw new RuntimeException(throwable);
+        } catch (IllegalArgumentException e) {
+            String typeName = enumClass.getSimpleName();
+            Matcher matcher = Pattern.compile("[A-Z]").matcher(typeName);
+            while (matcher.find())
+                typeName = typeName.replaceAll(matcher.group(), " " + matcher.group());
+            while (typeName.startsWith(" ")) typeName = typeName.substring(1);
+            throw new IllegalArgumentException(String.format("Could not find %s '%s'", typeName.toLowerCase(), name));
         }
     }
 }
