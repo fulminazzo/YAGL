2c3a61df2f3400d08c283eb88c8c1eefaf13b086
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 16:29:35 +0100

    Created WrappersYAGLParser

---
+++ wrappers/serializer/src/main/java/it/angrybear/yagl/parsers/WrappersYAGLParser.java
@@ -0,0 +1,22 @@
+package it.angrybear.yagl.parsers;
+
+import it.angrybear.yagl.particles.ParticleOptionParser;
+import it.angrybear.yagl.particles.ParticleParser;
+import it.angrybear.yagl.wrappers.WrapperParser;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+
+/**
+ * The type Wrappers YAGL parser.
+ */
+public class WrappersYAGLParser {
+
+    /**
+     * Adds all the module specific parsers.
+     */
+    public static void addAllParsers() {
+        YAGLParser.addAllParsers();
+        FileConfiguration.addParsers(new ParticleParser());
+        ParticleOptionParser.addAllParsers();
+        WrapperParser.addAllParsers();
+    }
+}
