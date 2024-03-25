3e5b28e44398b1bcb751dc1ae2c0b6b98080c4a6
Fulminazzo <fulminazzo21@gmail.com>
2024-03-26 00:54:41 +0100

    Added javadoc

--- item/serializer/src/main/java/it/angrybear/yagl/parsers/ShapelessRecipeParser.java
+++ item/serializer/src/main/java/it/angrybear/yagl/parsers/ShapelessRecipeParser.java
@@ -1,10 +1,17 @@
 package it.angrybear.yagl.parsers;
 
+import it.angrybear.yagl.items.recipes.FurnaceRecipe;
 import it.angrybear.yagl.items.recipes.ShapelessRecipe;
 import it.fulminazzo.yamlparser.parsers.CallableYAMLParser;
 
+/**
+ * A parser to serialize {@link ShapelessRecipe}.
+ */
 public class ShapelessRecipeParser extends CallableYAMLParser<ShapelessRecipe> {
 
+    /**
+     * Instantiates a new Shapeless recipe parser.
+     */
     public ShapelessRecipeParser() {
         super(ShapelessRecipe.class, (c) -> new ShapelessRecipe(c.getName()));
     }
