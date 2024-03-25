9332b27573f79d7bc83a07351cd66e5ff5dca51e
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 16:28:02 +0100

    Created YAGLParser

---
+++ common/serializer/src/main/java/it/angrybear/yagl/parsers/YAGLParser.java
@@ -0,0 +1,18 @@
+package it.angrybear.yagl.parsers;
+
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+
+/**
+ * The type YAGL parser.
+ */
+public class YAGLParser {
+
+    /**
+     * Adds all the parsers from the {@link it.angrybear.yagl.parsers} package to {@link FileConfiguration}.
+     * @deprecated This will NOT add module specific parsers. It is recommended to look for the <b>&lt;module&gt;YAGLParser</b> class instead.
+     */
+    @Deprecated
+    public static void addAllParsers() {
+        FileConfiguration.addParsers(YAGLParser.class.getPackage().getName());
+    }
+}
