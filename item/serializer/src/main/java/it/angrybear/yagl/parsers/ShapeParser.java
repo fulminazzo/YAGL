4a8c7c365c9eaf043d2db3b51b931de75bfc1c14
Fulminazzo <fulminazzo21@gmail.com>
2024-03-26 00:54:43 +0100

    Added javadoc

--- item/serializer/src/main/java/it/angrybear/yagl/parsers/ShapeParser.java
+++ item/serializer/src/main/java/it/angrybear/yagl/parsers/ShapeParser.java
@@ -3,8 +3,14 @@ package it.angrybear.yagl.parsers;
 import it.angrybear.yagl.items.recipes.ShapedRecipe;
 import it.fulminazzo.yamlparser.parsers.CallableYAMLParser;
 
+/**
+ * A parser to serialize {@link it.angrybear.yagl.items.recipes.ShapedRecipe.Shape}.
+ */
 public class ShapeParser extends CallableYAMLParser<ShapedRecipe.Shape> {
 
+    /**
+     * Instantiates a new Shape parser.
+     */
     public ShapeParser() {
         super(ShapedRecipe.Shape.class, c -> new ShapedRecipe.Shape());
     }
