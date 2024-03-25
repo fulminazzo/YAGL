baf2729b162aa6d7496dd3e2d869dd53319c5da5
Fulminazzo <fulminazzo21@gmail.com>
2024-03-25 13:15:13 +0100

    Updated tests

--- common/base/src/test/java/it/angrybear/yagl/utils/EnumUtilsTest.java
+++ common/base/src/test/java/it/angrybear/yagl/utils/EnumUtilsTest.java
@@ -12,11 +12,31 @@ class EnumUtilsTest {
     }
 
     @Test
+    void testValidComplete() {
+        assertEquals(MockEnum.VALUE1, EnumUtils.valueOf(MockEnum.class, MockEnum.VALUE1.name().toLowerCase(),
+                "value", "valueOf"));
+    }
+
+    @Test
+    void testInvalidMethod() {
+        assertThrowsExactly(RuntimeException.class, () -> EnumUtils.valueOf(MockEnum.class, MockEnum.VALUE1.name().toLowerCase(),
+                "value", "invalid", "valueOf"));
+    }
+
+    @Test
     void testInvalid() {
         assertThrowsExactly(IllegalArgumentException.class, () -> EnumUtils.valueOf(MockEnum.class, "VALUE3"));
     }
 
     private enum MockEnum {
-        VALUE1
+        VALUE1;
+
+        public static MockEnum value(String name) {
+            return null;
+        }
+
+        public static void invalid(String name) {
+
+        }
     }
 }
