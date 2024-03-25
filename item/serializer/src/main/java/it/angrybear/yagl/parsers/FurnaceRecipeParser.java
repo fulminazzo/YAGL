f9655b1991052d071252d3b4fbbb46e6e69c72c8
Fulminazzo <fulminazzo21@gmail.com>
2024-03-26 00:54:37 +0100

    Added javadoc

--- item/serializer/src/main/java/it/angrybear/yagl/parsers/FurnaceRecipeParser.java
+++ item/serializer/src/main/java/it/angrybear/yagl/parsers/FurnaceRecipeParser.java
@@ -3,8 +3,14 @@ package it.angrybear.yagl.parsers;
 import it.angrybear.yagl.items.recipes.FurnaceRecipe;
 import it.fulminazzo.yamlparser.parsers.CallableYAMLParser;
 
+/**
+ * A parser to serialize {@link FurnaceRecipe}.
+ */
 public class FurnaceRecipeParser extends CallableYAMLParser<FurnaceRecipe> {
 
+    /**
+     * Instantiates a new Furnace recipe parser.
+     */
     public FurnaceRecipeParser() {
         super(FurnaceRecipe.class, (c) -> new FurnaceRecipe(c.getName()));
     }
