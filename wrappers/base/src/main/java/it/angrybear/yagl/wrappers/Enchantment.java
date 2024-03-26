0027df98df6ad19641162b4b3d0f177ad02ecf04
Fulminazzo <fulminazzo21@gmail.com>
2024-03-26 18:53:07 +0100

    Fixed enchantment level lower than 0

--- wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Enchantment.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Enchantment.java
@@ -19,14 +19,14 @@ public class Enchantment extends Wrapper {
      * @param enchantment the enchantment
      */
     public Enchantment(final @NotNull String enchantment) {
-        this(enchantment, 0);
+        this(enchantment, 1);
     }
 
     /**
      * Instantiates a new Enchantment.
      *
      * @param enchantment the enchantment
-     * @param level       the level (0 is the lowest)
+     * @param level       the level (1 is the lowest)
      */
     public Enchantment(final @NotNull String enchantment, final int level) {
         this.enchantment = enchantment;
@@ -47,11 +47,11 @@ public class Enchantment extends Wrapper {
     /**
      * Sets level.
      *
-     * @param level the level
+     * @param level the level (1 is the lowest)
      * @return this enchantment
      */
     public Enchantment setLevel(final int level) {
-        if (level < 0) throw new IllegalArgumentException("Level cannot be lower than 0");
+        if (level < 1) throw new IllegalArgumentException("Level cannot be lower than 1");
         this.level = level;
         return this;
     }
