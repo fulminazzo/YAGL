9b11145ee7bca3c17f55b7c1ce8a4544a8b8f747
Fulminazzo <fulminazzo21@gmail.com>
2024-03-28 23:02:25 +0100

    Refactored code

--- common/base/src/test/java/it/angrybear/yagl/ClassEnumTest.java
+++ common/base/src/test/java/it/angrybear/yagl/ClassEnumTest.java
@@ -2,11 +2,10 @@ package it.angrybear.yagl;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClassEnumTest {
 
