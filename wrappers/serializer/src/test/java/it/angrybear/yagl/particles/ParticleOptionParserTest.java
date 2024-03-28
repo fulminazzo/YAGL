e339e58f5963525e129562d72eb557bfbfcd1869
Fulminazzo <fulminazzo21@gmail.com>
2024-03-28 19:47:13 +0100

    Added test for ColorParticleOption

--- wrappers/serializer/src/test/java/it/angrybear/yagl/particles/ParticleOptionParserTest.java
+++ wrappers/serializer/src/test/java/it/angrybear/yagl/particles/ParticleOptionParserTest.java
@@ -20,6 +20,7 @@ class ParticleOptionParserTest {
                 new PrimitiveParticleOption<>(3),
                 new PrimitiveParticleOption<>(3f),
                 new PrimitiveParticleOption<>("Aint happening"),
+                new ColorParticleOption(Color.RED),
                 new DustParticleOption(Color.WHITE, 3f),
                 new DustTransitionParticleOption(Color.BLACK, Color.WHITE, 3f),
                 new BlockDataOption("oak_fence", "east=false,north=false,south=false,waterlogged=false,west=false")
