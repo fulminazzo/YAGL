3489bfbd71766c4bd3beb038025090e6c61e42f9
Fulminazzo <fulminazzo21@gmail.com>
2024-03-29 22:14:06 +0100

    Added getName and isSimilar method

--- wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Wrapper.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Wrapper.java
@@ -2,14 +2,30 @@ package it.angrybear.yagl.wrappers;
 
 import it.fulminazzo.fulmicollection.objects.Printable;
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
-import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * A general class that represents a wrapper for another object.
  */
 abstract class Wrapper {
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public abstract String getName();
+
+    /**
+     * Compare this {@link Wrapper} with the given one
+     *
+     * @param wrapper the wrapper
+     * @return true, if they have the same name
+     */
+    public boolean isSimilar(final @Nullable Wrapper wrapper) {
+        if (wrapper == null) return false;
+        return getClass().equals(wrapper.getClass()) && getName().equalsIgnoreCase(wrapper.getName());
+    }
 
     /**
      * Compare this wrapper with the given one.
