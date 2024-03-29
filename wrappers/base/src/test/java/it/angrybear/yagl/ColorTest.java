40e0f34ae0116fbed68572415ad8e9ae4960bbe9
Fulminazzo <fulminazzo21@gmail.com>
2024-03-29 22:26:31 +0100

    Added tests for invalid Color parameters

--- wrappers/base/src/test/java/it/angrybear/yagl/ColorTest.java
+++ wrappers/base/src/test/java/it/angrybear/yagl/ColorTest.java
@@ -1,6 +1,9 @@
 package it.angrybear.yagl;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +24,27 @@ class ColorTest {
         Color color = Color.fromARGB(argb);
         assertEquals("#" + argb.substring(2), color.toRGB());
         assertEquals("#" + argb, color.toARGB());
+    }
+
+    @Test
+    void testInvalidColorNegative() {
+        final int max = 4;
+        for (int i = 0; i < max; i++) {
+            Integer[] params = new Integer[max];
+            Arrays.fill(params, 0);
+            params[i] = -1;
+            assertThrowsExactly(IllegalArgumentException.class, () -> new Refl<>(Color.class, (Object[]) params));
+        }
+    }
+
+    @Test
+    void testInvalidColorTooHigh() {
+        final int max = 4;
+        for (int i = 0; i < max; i++) {
+            Integer[] params = new Integer[max];
+            Arrays.fill(params, 0);
+            params[i] = 256;
+            assertThrowsExactly(IllegalArgumentException.class, () -> new Refl<>(Color.class, (Object[]) params));
+        }
     }
 }
