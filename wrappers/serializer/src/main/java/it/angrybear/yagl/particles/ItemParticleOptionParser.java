ba8bab92b3cec62dfa2c2b43270acc07ee64452c
Fulminazzo <fulminazzo21@gmail.com>
2024-03-29 20:08:43 +0100

    Created ItemParticleOptionParser

---
+++ wrappers/serializer/src/main/java/it/angrybear/yagl/particles/ItemParticleOptionParser.java
@@ -0,0 +1,37 @@
+package it.angrybear.yagl.particles;
+
+import it.angrybear.yagl.items.Item;
+import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
+import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import it.fulminazzo.yamlparser.parsers.YAMLParser;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A parser to serialize {@link ItemParticleOption}.
+ * Should take priority over {@link ParticleOptionParser}.
+ * It requires the <i>item:serializer</i> module to be added.
+ */
+public class ItemParticleOptionParser extends YAMLParser<ItemParticleOption> {
+
+    public ItemParticleOptionParser() {
+        super(ItemParticleOption.class);
+    }
+
+    @Override
+    protected BiFunctionException<@NotNull IConfiguration, @NotNull String, @Nullable ItemParticleOption> getLoader() {
+        return (c, s) -> {
+            Item item = c.get(s, Item.class);
+            if (item == null) return null;
+            return new ItemParticleOption(item);
+        };
+    }
+
+    @Override
+    protected TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable ItemParticleOption> getDumper() {
+        return (c, s, o) -> {
+            c.set(s, o == null ? null : o.getOption());
+        };
+    }
+}
