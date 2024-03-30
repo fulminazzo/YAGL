5e2b0e26100a44af1763b25a0b9ad48c4b2afef1
Fulminazzo <fulminazzo21@gmail.com>
2024-03-30 23:43:07 +0100

    Added support for extra interfaces

--- item/bukkit/src/test/java/it/angrybear/yagl/utils/BukkitUtils.java
+++ item/bukkit/src/test/java/it/angrybear/yagl/utils/BukkitUtils.java
@@ -5,9 +5,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.inventory.ItemFactory;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -17,8 +23,10 @@ public class BukkitUtils {
     public static void setupItemFactory() {
         ItemFactory factory = mock(ItemFactory.class);
         when(factory.getItemMeta(any())).thenAnswer(r -> {
-            if (r.getArgument(0).equals(Material.ENCHANTED_BOOK)) return mock(EnchantmentStorageMeta.class);
-            return mock(ItemMeta.class);
+            List<Class<?>> extraInterfaces = new LinkedList<>();
+            extraInterfaces.add(Damageable.class);
+            if (r.getArgument(0).equals(Material.ENCHANTED_BOOK)) extraInterfaces.add(EnchantmentStorageMeta.class);
+            return mock(ItemMeta.class, withSettings().extraInterfaces(extraInterfaces.toArray(new Class[0])));
         });
         Server server = mock(Server.class);
         when(server.getItemFactory()).thenReturn(factory);
