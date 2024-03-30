f7291d05f1488013aee294971269a211f334c805
Fulminazzo <fulminazzo21@gmail.com>
2024-03-30 18:36:25 +0100

    Rewritten ordinal method using new cache system

--- common/base/src/main/java/it/angrybear/yagl/ClassEnum.java
+++ common/base/src/main/java/it/angrybear/yagl/ClassEnum.java
@@ -29,7 +29,7 @@ public abstract class ClassEnum {
      * @return the index
      */
     public int ordinal() {
-        return this.index;
+        return getCache().ordinal(this);
     }
 
     /**
