f86e6eb0b2efbb0d817cf9792d1c58b88345182c
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 00:11:11 +0100

    Added MockEnchantment class

---
+++ item/bukkit/src/test/java/it/angrybear/yagl/utils/MockEnchantment.java
@@ -0,0 +1,56 @@
+package it.angrybear.yagl.utils;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+class MockEnchantment extends Enchantment {
+
+    public MockEnchantment(@NotNull NamespacedKey key) {
+        super(key);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return getKey().getKey();
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return null;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack item) {
+        return false;
+    }
+}
