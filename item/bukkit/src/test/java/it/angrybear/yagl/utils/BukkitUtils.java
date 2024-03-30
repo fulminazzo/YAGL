a97593124dbf6bfae150bb53029977833c8e1eda
Fulminazzo <fulminazzo21@gmail.com>
2024-03-30 23:48:54 +0100

    Added setupServer method with getRecipe implementation

--- item/bukkit/src/test/java/it/angrybear/yagl/utils/BukkitUtils.java
+++ item/bukkit/src/test/java/it/angrybear/yagl/utils/BukkitUtils.java
@@ -3,8 +3,10 @@ package it.angrybear.yagl.utils;
 import it.fulminazzo.fulmicollection.objects.Refl;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Server;
 import org.bukkit.inventory.ItemFactory;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -19,8 +21,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 public class BukkitUtils {
+    private static final List<Recipe> RECIPES = new LinkedList<>();
+
+    public static void setupServer() {
+        Server server = mock(Server.class);
+        when(server.getRecipe(any())).thenAnswer(r -> {
+            NamespacedKey key = r.getArgument(0);
+            for (Recipe recipe : RECIPES)
+                if (key.equals(new Refl<>(recipe).getFieldObject("key")))
+                    return recipe;
+            return null;
+        });
+        new Refl<>(Bukkit.class).setFieldObject("server", server);
+    }
 
     public static void setupItemFactory() {
+        setupServer();
         ItemFactory factory = mock(ItemFactory.class);
         when(factory.getItemMeta(any())).thenAnswer(r -> {
             List<Class<?>> extraInterfaces = new LinkedList<>();
@@ -33,9 +49,7 @@ public class BukkitUtils {
             ItemMeta m2 = r.getArgument(1);
             return m1 != null && m2 != null && Arrays.equals(m1.getClass().getInterfaces(), m2.getClass().getInterfaces());
         });
-        Server server = mock(Server.class);
-        when(server.getItemFactory()).thenReturn(factory);
-        new Refl<>(Bukkit.class).setFieldObject("server", server);
+        when(Bukkit.getServer().getItemFactory()).thenReturn(factory);
     }
 
     @Test
