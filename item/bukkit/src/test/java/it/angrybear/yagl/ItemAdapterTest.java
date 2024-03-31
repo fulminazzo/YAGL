084fdea752fc98937e9704c153c3c6b8c6e0cb73
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 01:50:31 +0100

    Added test for shapeless recipe

--- item/bukkit/src/test/java/it/angrybear/yagl/ItemAdapterTest.java
+++ item/bukkit/src/test/java/it/angrybear/yagl/ItemAdapterTest.java
@@ -2,10 +2,17 @@ package it.angrybear.yagl;
 
 import it.angrybear.yagl.items.Item;
 import it.angrybear.yagl.items.fields.ItemFlag;
+import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.BukkitUtils;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,5 +36,21 @@ class ItemAdapterTest {
         ItemStack itemStack = ItemAdapter.itemToItemStack(expected);
 
         assertEquals(expected, ItemAdapter.itemStackToItem(itemStack));
+    }
+
+    @Test
+    void testShapelessRecipeConversion() {
+        ShapelessRecipe expected = new ShapelessRecipe(new NamespacedKey("yagl", "test"),
+                new ItemStack(Material.STONE));
+        expected.addIngredient(new RecipeChoice.ExactChoice(new ItemStack(Material.GRASS)));
+
+        it.angrybear.yagl.items.recipes.ShapelessRecipe recipe = new it.angrybear.yagl.items.recipes.ShapelessRecipe("test")
+                .setOutput(Item.newItem().setMaterial("STONE")).addIngredient(Item.newItem().setMaterial("GRASS"));
+
+        Refl<?> r1 = new Refl<>(expected);
+        Refl<?> r2 = new Refl<>(ItemAdapter.recipeToMinecraft(recipe));
+
+        for (Field field : r1.getNonStaticFields())
+            assertEquals((Object) r1.getFieldObject(field), r2.getFieldObject(field));
     }
 }
