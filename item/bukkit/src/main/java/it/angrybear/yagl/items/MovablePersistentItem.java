5f6b1cbf3eec7f7a98be0f5651f55ed0755f7492
Fulminazzo <fulminazzo21@gmail.com>
2024-03-31 19:43:20 +0200

    Added missing constructors

--- item/bukkit/src/main/java/it/angrybear/yagl/items/MovablePersistentItem.java
+++ item/bukkit/src/main/java/it/angrybear/yagl/items/MovablePersistentItem.java
@@ -1,7 +1,56 @@
 package it.angrybear.yagl.items;
 
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /**
  * An implementation of {@link PersistentItem} that allows to be moved inside the player's inventory.
  */
 public class MovablePersistentItem extends PersistentItem {
+
+    /**
+     * Instantiates a new Movable persistent item.
+     */
+    public MovablePersistentItem() {
+        super();
+    }
+
+    /**
+     * Instantiates a new Movable persistent item.
+     *
+     * @param material the material
+     */
+    public MovablePersistentItem(@NotNull Material material) {
+        super(material);
+    }
+
+    /**
+     * Instantiates a new Movable persistent item.
+     *
+     * @param material the material
+     */
+    public MovablePersistentItem(@Nullable String material) {
+        super(material);
+    }
+
+    /**
+     * Instantiates a new Movable persistent item.
+     *
+     * @param material the material
+     * @param amount   the amount
+     */
+    public MovablePersistentItem(@NotNull Material material, int amount) {
+        super(material, amount);
+    }
+
+    /**
+     * Instantiates a new Movable persistent item.
+     *
+     * @param material the material
+     * @param amount   the amount
+     */
+    public MovablePersistentItem(@Nullable String material, int amount) {
+        super(material, amount);
+    }
 }
