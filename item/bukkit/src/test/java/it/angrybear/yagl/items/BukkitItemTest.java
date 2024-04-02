b5e5285c1da31acc7dcf93c098c277804afb1211
Fulminazzo <fulminazzo21@gmail.com>
2024-04-02 11:07:15 +0200

    Rewritten return types tests with new TestUtils methods

--- item/bukkit/src/test/java/it/angrybear/yagl/items/BukkitItemTest.java
+++ item/bukkit/src/test/java/it/angrybear/yagl/items/BukkitItemTest.java
@@ -1,9 +1,7 @@
 package it.angrybear.yagl.items;
 
+import it.angrybear.yagl.TestUtils;
 import it.angrybear.yagl.items.fields.ItemFlag;
-import it.fulminazzo.fulmicollection.objects.Refl;
-import it.fulminazzo.fulmicollection.utils.ExceptionUtils;
-import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import it.fulminazzo.jbukkit.BukkitUtils;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -14,15 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BukkitItemTest {
 
@@ -101,48 +95,8 @@ class BukkitItemTest {
 
     @ParameterizedTest
     @MethodSource("testItems")
-    void testItemsReturnType(Item item) {
-        final Refl<Item> itemRefl = new Refl<>(item);
-
-        for (Method method : itemRefl.getNonStaticMethods()) {
-            final Class<?>[] parameters = method.getParameterTypes();
-            final String methodString = String.format("%s(%s)", method.getName(), Arrays.stream(parameters)
-                            .map(Class::getSimpleName).collect(Collectors.joining(", ")));
-            try {
-                final Class<?> returnType = method.getReturnType();
-                if (!Item.class.isAssignableFrom(returnType)) continue;
-                if (method.equals(Item.class.getMethod("copy", Class.class))) continue;
-
-                String errorMessage = String.format("Method '%s' of class '%s' did not return itself",
-                        methodString, item.getClass().getSimpleName());
-
-                Object[] mockParameters = Arrays.stream(parameters).map(this::mockParameter).toArray(Object[]::new);
-                method.setAccessible(true);
-                Object o = method.invoke(item, mockParameters);
-
-                if (method.getName().equals("copy"))
-                    assertInstanceOf(item.getClass(), o, String.format("Returned object from %s call should have been %s but was %s",
-                            methodString, item.getClass(), o.getClass()));
-                else assertEquals(item.hashCode(), o.hashCode(), errorMessage);
-            } catch (Exception e) {
-                System.err.printf("An exception occurred while testing method '%s'%n", methodString);
-                ExceptionUtils.throwException(e);
-            }
-        }
-    }
-
-    private Object mockParameter(Class<?> clazz) {
-        clazz = ReflectionUtils.getWrapperClass(clazz);
-        if (Number.class.isAssignableFrom(clazz)) return 1;
-        if (String.class.isAssignableFrom(clazz)) return "STONE";
-        if (Boolean.class.isAssignableFrom(clazz)) return false;
-        if (clazz.isEnum()) {
-            Enum<?>[] enums = new Refl<>(clazz).invokeMethod("values");
-            if (enums == null) return null;
-            return enums[0];
-        }
-        if (clazz.isArray()) return Array.newInstance(clazz.componentType(), 0);
-        if (Collection.class.isAssignableFrom(clazz)) return new ArrayList<>();
-        return mock(clazz);
+    void testItemsReturnType(Item item) throws NoSuchMethodException {
+        final Method copyMethod = Item.class.getMethod("copy", Class.class);
+        TestUtils.testReturnType(item, Item.class, m -> m.equals(copyMethod));
     }
 }
