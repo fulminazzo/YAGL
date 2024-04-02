4d2268ff7cabc4651e0230aa2a23f56a74299573
Fulminazzo <fulminazzo21@gmail.com>
2024-04-02 16:03:09 +0200

    Added testSingleMethod function with javadocs

--- testing/src/main/java/it/angrybear/yagl/TestUtils.java
+++ testing/src/main/java/it/angrybear/yagl/TestUtils.java
@@ -5,23 +5,78 @@ import it.fulminazzo.fulmicollection.utils.ExceptionUtils;
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mockito.ArgumentCaptor;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.reset;
 
 /**
  * The type Test utils.
  */
 public class TestUtils {
+
+    /**
+     * Allows to test the given <i>targetMethod</i>.
+     * It verifies that the <i>executor</i> invokes <i>targetMethod</i> and that,
+     * upon execution, <i>target</i> invokes <i>invokedMethod</i>.
+     *
+     * @param executor                the executor
+     * @param targetMethod            the target method
+     * @param staticObjects           the objects that will be used for the creation of the parameters of <i>targetMethod</i>. If the required class is present among these objects, then the one provided will be used.                                Otherwise, {@link #mockParameter(Class)} will be called.
+     * @param target                  the target
+     * @param invokedMethod           the invoked method
+     * @param invokedMethodParamTypes the type of the parameters when invoking <i>invokedMethod</i>. These will also be the types of the returned captors
+     * @return the argument captors of the invoked parameters
+     */
+    public static ArgumentCaptor<?> @NotNull [] testSingleMethod(final @NotNull Object executor, final @NotNull Method targetMethod,
+                                                                 final Object @NotNull [] staticObjects,
+                                                                 final @NotNull Object target, final String invokedMethod,
+                                                                 final Class<?> @NotNull ... invokedMethodParamTypes) {
+        try {
+            // Execute target method
+            final Object[] parameters = initializeParameters(targetMethod.getParameterTypes(), staticObjects);
+            targetMethod.setAccessible(true);
+            targetMethod.invoke(executor, parameters);
+
+            // Prepare argument captors
+            final ArgumentCaptor<?>[] captors = initializeCaptors(invokedMethodParamTypes);
+
+            // Verify execution with mock
+            new Refl<>(verify(target)).invokeMethod(invokedMethod, Arrays.stream(captors)
+                    .map(ArgumentCaptor::capture).toArray(Object[]::new));
+
+            return captors;
+        } catch (Exception e) {
+            System.err.printf("An exception occurred while testing method '%s'%n", methodToString(targetMethod));
+            ExceptionUtils.throwException(e);
+            throw new IllegalStateException("Unreachable code");
+        }
+    }
+
+    private static Object[] initializeParameters(final Class<?> @NotNull [] classes, final Object @NotNull ... staticObjects) {
+        return Arrays.stream(classes).map(c -> initializeSingle(c, staticObjects)).toArray(Object[]::new);
+    }
+
+    private static Object initializeSingle(final Class<?> clazz, final Object @NotNull ... staticObjects) {
+        for (Object o : staticObjects) if (clazz.isAssignableFrom(o.getClass())) return o;
+        return TestUtils.mockParameter(clazz);
+    }
+
+    private static ArgumentCaptor<?>[] initializeCaptors(final Class<?> @NotNull [] classes) {
+        return Arrays.stream(classes).map(ArgumentCaptor::forClass).toArray(ArgumentCaptor[]::new);
+    }
 
     /**
      * Many objects have setter, adder or remover methods which return the object itself,
