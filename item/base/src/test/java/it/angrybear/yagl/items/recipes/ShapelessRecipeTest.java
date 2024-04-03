ccefed90b9f2f54bc965bd4fd9e44885f00478df
Fulminazzo <fulminazzo21@gmail.com>
2024-04-03 14:48:42 +0200

    Added test suite for ShapelessRecipe methods

---
+++ item/base/src/test/java/it/angrybear/yagl/items/recipes/ShapelessRecipeTest.java
@@ -0,0 +1,29 @@
+package it.angrybear.yagl.items.recipes;
+
+import it.angrybear.yagl.items.Item;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapelessRecipeTest {
+
+    @Test
+    void testAddTooMuchItems() {
+        Item[] items = new Item[ShapelessRecipe.MAX_SIZE];
+        Arrays.fill(items, Item.newItem());
+        assertThrowsExactly(IllegalStateException.class, () -> new ShapelessRecipe("tmp").addIngredients(items));
+    }
+
+    @Test
+    void testRemoveItems() {
+        ShapelessRecipe recipe = new ShapelessRecipe("tmp");
+        List<Item> items = new LinkedList<>();
+        for (int i = 0; i < ShapelessRecipe.MAX_SIZE; i++) items.add(Item.newItem("MOCK_MATERIAL_" + i));
+        recipe.removeIngredients(items.toArray(new Item[0]));
+        assertTrue(recipe.isEmpty(), "Recipe should have been empty after removing items");
+    }
+}
