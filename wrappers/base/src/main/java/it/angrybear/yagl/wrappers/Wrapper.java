8bbfd0ca348a3a08f3a0b30e59c321936c7886ec
Fulminazzo <fulminazzo21@gmail.com>
2024-04-03 23:44:46 +0200

    Extracted static check method

--- wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Wrapper.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/wrappers/Wrapper.java
@@ -15,21 +15,34 @@ import java.lang.reflect.Field;
 public abstract class Wrapper extends FieldEquable {
 
     /**
+     * Uses {@link #check(Object, Number)} to check the given value.
+     *
+     * @param <N>   the type parameter
+     * @param value the value
+     * @return the value
+     */
+    protected <N extends Number> N check(final @NotNull N value) {
+        return check(this, value);
+    }
+
+    /**
      * Allows to check the given number.
      * It uses the {@link Range} annotation.
      * If the given value is lower than {@link Range#min()} or higher than {@link Range#max()},
      * an {@link IllegalArgumentException} is thrown.
-     * @param <N> the type parameter
-     * @param value   the value
+     *
+     * @param <N>    the type parameter
+     * @param object the object to check
+     * @param value  the value
      * @return the passed value
      */
-    protected <N extends Number> N check(final @NotNull N value) {
+    public static <N extends Number> N check(final Object object, final @NotNull N value) {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
         // The first element should be the getStackTrace invocation, the second this method.
         // So we are looking for the third.
         String method = trace[2].getMethodName().toLowerCase();
         if (method.startsWith("set") || method.startsWith("get")) method = method.substring(3);
-        Field field = ReflectionUtils.getField(this, method);
+        Field field = ReflectionUtils.getField(object, method);
         if (field.isAnnotationPresent(Range.class)) {
             Range range = field.getAnnotation(Range.class);
             int min = range.min();
