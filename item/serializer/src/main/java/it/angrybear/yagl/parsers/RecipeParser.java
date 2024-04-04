58d6309421fc516830a74a8520e83afda4c320e0
Fulminazzo <fulminazzo21@gmail.com>
2024-04-04 15:34:44 +0200

    Updated RecipeParser to remove any value-class field

--- item/serializer/src/main/java/it/angrybear/yagl/parsers/RecipeParser.java
+++ item/serializer/src/main/java/it/angrybear/yagl/parsers/RecipeParser.java
@@ -1,6 +1,17 @@
 package it.angrybear.yagl.parsers;
 
+import it.angrybear.yagl.items.Item;
+import it.angrybear.yagl.items.recipes.FurnaceRecipe;
 import it.angrybear.yagl.items.recipes.Recipe;
+import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
+import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A parser to serialize {@link Recipe} and derivatives.
@@ -12,5 +23,25 @@ public class RecipeParser extends TypedParser<Recipe> {
      */
     public RecipeParser() {
         super(Recipe.class);
+    }
+
+    @Override
+    protected BiFunctionException<IConfiguration, String, Recipe> getLoader() {
+        return (c, s) -> {
+            Recipe r = super.getLoader().apply(c, s);
+            if (r == null) return null;
+            if (r instanceof FurnaceRecipe) return r;
+            List<Item> ingredients = c.getList(s + ".ingredients", Item.class);
+            if (ingredients == null) ingredients = new LinkedList<>();
+            return new Refl<>(r).setFieldObject("ingredients", ingredients).getObject();
+        };
+    }
+
+    @Override
+    protected @NotNull TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable Recipe> getDumper() {
+        return super.getDumper().andThen((c, s, r) -> {
+            if (r == null) return;
+            if (!r.isEmpty()) c.set(s + ".ingredients.value-class", null);
+        });
     }
 }
