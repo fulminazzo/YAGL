5f54a28ebef00916484c2319947ea366a0f330fc
Fulminazzo <fulminazzo21@gmail.com>
2024-04-04 16:05:57 +0200

    Created Constants class with TICKS_IN_SECOND field

---
+++ wrappers/base/src/main/java/it/angrybear/yagl/Constants.java
@@ -0,0 +1,12 @@
+package it.angrybear.yagl;
+
+/**
+ * A collection of Minecraft constants used throughout the library.
+ */
+public class Constants {
+
+    /**
+     * The number of ticks in one second.
+     */
+    public static final long TICKS_IN_SECOND = 20;
+}
