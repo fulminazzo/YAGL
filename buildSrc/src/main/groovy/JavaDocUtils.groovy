f8276c7772bbc60f06fee2dd8da7d57571ad46e1
Fulminazzo <fulminazzo21@gmail.com>
2024-04-05 18:43:32 +0200

    Added basic aggregateJavaDoc method implementation with associated recursive method

--- buildSrc/src/main/groovy/JavaDocUtils.groovy
+++ buildSrc/src/main/groovy/JavaDocUtils.groovy
@@ -1,4 +1,36 @@
 class JavaDocUtils {
+    private static final def DOCS_DIR = "javadoc"
+    private static final def IGNORE_DIRS = [ "main", "java", "groovy", "src", "buildSrc", "resources" ]
+
+    static def aggregateJavaDoc(String output) {
+        def current = new File(System.getProperty("user.dir"))
+        def outputDir = new File(output)
+
+        if (outputDir.exists() && !outputDir.deleteDir())
+            throw new IllegalStateException("Could not delete previous directory ${output}")
+        if (!outputDir.mkdirs()) throw new IllegalStateException("Could not create directory ${output}")
+
+        aggregateJavaDocRec(current, outputDir)
+    }
+
+    private static def aggregateJavaDocRec(File current, File output) {
+        if (!current.isDirectory()) return
+
+        def files = current.listFiles()
+        if (files == null) return
+
+        main_loop:
+        for (file in files) {
+            def fileName = file.getName()
+            for (dir in IGNORE_DIRS)
+                if (fileName == dir) continue main_loop
+
+            if (file.isDirectory())
+                if (file.getName() == DOCS_DIR) {
+                    //TODO: save in output
+                } else aggregateJavaDocRec(file, output)
+        }
+    }
 
     static def commonPath(File file1, File file2) {
         def path1 = file1.getAbsolutePath()
