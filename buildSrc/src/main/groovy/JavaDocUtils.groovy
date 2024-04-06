c0fe25aa89fe1fe02ab9a1c4d6575579f7ad4638
Fulminazzo <fulminazzo21@gmail.com>
2024-04-06 18:57:00 +0200

    Replaced GString declarations with actual strings

--- buildSrc/src/main/groovy/JavaDocUtils.groovy
+++ buildSrc/src/main/groovy/JavaDocUtils.groovy
@@ -5,11 +5,11 @@ import java.nio.file.StandardCopyOption
  * A collection of utilities to aggregate Javadocs
  */
 class JavaDocUtils {
-    private static final def DOCS_DIR = "javadoc"
-    private static final def IGNORE_DIRS = [ "main", "java", "groovy", "src", "buildSrc", "resources" ]
-    private static final def RESOURCES = [ "index.html", "javadoc-stylesheet.css" ]
-    private static final def MODULE_PLACEHOLDER = "%modules%"
-    private static final def MODULE_FORMAT_FILE = "module-format.html"
+    private static final def DOCS_DIR = 'javadoc'
+    private static final def IGNORE_DIRS = [ 'main', 'java', 'groovy', 'src', 'buildSrc', 'resources' ]
+    private static final def RESOURCES = [ 'index.html', 'javadoc-stylesheet.css' ]
+    private static final def MODULE_PLACEHOLDER = '%modules%'
+    private static final def MODULE_FORMAT_FILE = 'module-format.html'
 
     /**
      * Aggregates the javadoc of all the root and subprojects into a single directory
@@ -18,8 +18,8 @@ class JavaDocUtils {
      * @param ignoreDirs the directories (modules) to be ignore
      */
     static void aggregateJavaDoc(String output, String name, String version, String... ignoreDirs) {
-        def current = new File(System.getProperty("user.dir"))
-        if (current.name == "buildSrc") current = current.parentFile
+        def current = new File(System.getProperty('user.dir'))
+        if (current.name == 'buildSrc') current = current.parentFile
         def outputDir = new File(current, output)
 
         if (outputDir.exists() && !outputDir.deleteDir())
@@ -51,7 +51,7 @@ class JavaDocUtils {
         if (!file.directory) return
         def files = file.listFiles()
         if (files == null) return
-        if (files.any { it.name.contains(".html") } ) return
+        if (files.any { it.name.contains('.html') } ) return
 
         RESOURCES.each { parseResource(file, it, name, version, files) }
 
@@ -59,12 +59,12 @@ class JavaDocUtils {
     }
 
     private static void parseResource(File parentFile, String resource, String name, String version, File[] files) {
-        getResource("/${resource}").withReader { reader ->
+        getResource('/${resource}').withReader { reader ->
             new File(parentFile, resource).withWriter { writer ->
                 String line
                 while ((line = reader.readLine()) != null) {
-                    line = line.replace("%module_name%", name)
-                            .replace("%module_version%", version)
+                    line = line.replace('%module_name%', name)
+                            .replace('%module_version%', version)
                     if (line.contains(MODULE_PLACEHOLDER))
                         line = parseModulesPlaceholder(line, files)
                     writer.write(line)
@@ -75,12 +75,12 @@ class JavaDocUtils {
     }
 
     private static String parseModulesPlaceholder(String line, File[] files) {
-        String output = ""
+        String output = ''
         files.collect { it.name } .each { n -> getResource("/${MODULE_FORMAT_FILE}").withReader { r ->
             String l
             while ((l = r.readLine()) != null)
-                output += l.replace("%submodule_name%", n)
-                        .replace("%submodule_path%", "${n}${File.separator}index.html")
+                output += l.replace('%submodule_name%', n)
+                        .replace('%submodule_path%', "${n}${File.separator}index.html")
         } }
         return line.replace(MODULE_PLACEHOLDER, output)
     }
@@ -102,13 +102,13 @@ class JavaDocUtils {
 
     private static String getDestinationFromModule(File output, File file) {
         def parent = new File(commonPath(output, file))
-        def module = new File(file, "")
+        def module = new File(file, '')
         while (module.parentFile.parentFile != parent)
             module = module.parentFile
         def moduleName = module.name
 
         def dst = "${module.parentFile.name}"
-        if (moduleName != "build") dst += "${File.separator}${module.name}"
+        if (moduleName != 'build') dst += "${File.separator}${module.name}"
         return dst
     }
 
@@ -122,7 +122,7 @@ class JavaDocUtils {
     static String commonPath(File file1, File file2) {
         def path1 = file1.absolutePath
         def path2 = file2.absolutePath
-        def result = ""
+        def result = ''
 
         for (i in 0..Math.min(path1.length(), path2.length())) {
             def c1 = path1[i]
@@ -135,9 +135,9 @@ class JavaDocUtils {
     }
 
     private static InputStream getResource(String name) {
-        while (name.startsWith("/")) name = name.substring(1)
+        while (name.startsWith('/')) name = name.substring(1)
         def resource = JavaDocUtils.class.getResourceAsStream(name)
-        if (resource == null) resource = JavaDocUtils.class.getResourceAsStream("/" + name)
+        if (resource == null) resource = JavaDocUtils.class.getResourceAsStream("/${name}")
         if (resource == null) throw new IllegalArgumentException("Could not find resource '${name}'")
         return resource
     }
--- buildSrc/src/test/groovy/JavaDocUtilsTest.groovy
+++ buildSrc/src/test/groovy/JavaDocUtilsTest.groovy
@@ -2,11 +2,11 @@ import spock.lang.Specification
 
 class JavaDocUtilsTest extends Specification {
 
-    void "test common paths"() {
+    void 'test common paths'() {
         given:
-            def expected = System.getProperty("user.dir") + "/this/path/is/expected/"
-            def path1 = new File(expected, "this/is/not")
-            def path2 = new File(expected, "me/neither")
+            def expected = "${System.getProperty("user.dir")}/this/path/is/expected/"
+            def path1 = new File(expected, 'this/is/not')
+            def path2 = new File(expected, 'me/neither')
 
         when:
             def actual = JavaDocUtils.commonPath(path1, path2)
