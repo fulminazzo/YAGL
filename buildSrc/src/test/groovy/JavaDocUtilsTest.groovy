f262eb9a5619ad2ce654ab1d0268e19781f34444
Fulminazzo <fulminazzo21@gmail.com>
2024-04-06 19:07:41 +0200

    Fixed indentation

--- buildSrc/src/test/groovy/JavaDocUtilsTest.groovy
+++ buildSrc/src/test/groovy/JavaDocUtilsTest.groovy
@@ -4,14 +4,14 @@ class JavaDocUtilsTest extends Specification {
 
     void 'test common paths'() {
         given:
-            def expected = "${System.getProperty("user.dir")}/this/path/is/expected/"
-            def path1 = new File(expected, 'this/is/not')
-            def path2 = new File(expected, 'me/neither')
+        def expected = "${System.getProperty("user.dir")}/this/path/is/expected/"
+        def path1 = new File(expected, 'this/is/not')
+        def path2 = new File(expected, 'me/neither')
 
         when:
-            def actual = JavaDocUtils.commonPath(path1, path2)
+        def actual = JavaDocUtils.commonPath(path1, path2)
 
         then:
-            expected == actual
+        expected == "${actual}"
     }
 }
