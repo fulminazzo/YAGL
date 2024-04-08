8545f3ca84a907164c23cbd97838843eb195bb19
Fulminazzo <fulminazzo21@gmail.com>
2024-04-08 03:03:54 +0200

    Reworked if statements

--- item/serializer/src/main/java/it/angrybear/yagl/parsers/ItemParser.java
+++ item/serializer/src/main/java/it/angrybear/yagl/parsers/ItemParser.java
@@ -35,28 +35,29 @@ public class ItemParser extends YAMLParser<Item> {
         return (c, s) -> {
             final ConfigurationSection itemSection = c.getConfigurationSection(s);
             if (itemSection == null) return null;
+            else {
+                Item item = Item.newItem();
 
-            Item item = Item.newItem();
+                final String material = itemSection.getString("material");
+                if (material == null) throw new IllegalArgumentException("'material' cannot be null");
+                item.setMaterial(material);
 
-            final String material = itemSection.getString("material");
-            if (material == null) throw new IllegalArgumentException("'material' cannot be null");
-            item.setMaterial(material);
+                itemSection.getOptional("amount", Integer.class).ifPresent(item::setAmount);
+                itemSection.getOptional("durability", Integer.class).ifPresent(item::setDurability);
+                itemSection.getOptional("display-name", String.class).ifPresent(item::setDisplayName);
+                itemSection.getOptional("lore", List.class).ifPresent(item::setLore);
+                itemSection.getOptional("unbreakable", Boolean.class).ifPresent(item::setUnbreakable);
+                itemSection.getOptional("custom-model-data", Integer.class).ifPresent(item::setCustomModelData);
+                itemSection.getListOptional("enchantments", Enchantment.class).ifPresent(item::addEnchantments);
+                itemSection.getListOptional("item-flags", ItemFlag.class).ifPresent(item::addItemFlags);
 
-            itemSection.getOptional("amount", Integer.class).ifPresent(item::setAmount);
-            itemSection.getOptional("durability", Integer.class).ifPresent(item::setDurability);
-            itemSection.getOptional("display-name", String.class).ifPresent(item::setDisplayName);
-            itemSection.getOptional("lore", List.class).ifPresent(item::setLore);
-            itemSection.getOptional("unbreakable", Boolean.class).ifPresent(item::setUnbreakable);
-            itemSection.getOptional("custom-model-data", Integer.class).ifPresent(item::setCustomModelData);
-            itemSection.getListOptional("enchantments", Enchantment.class).ifPresent(item::addEnchantments);
-            itemSection.getListOptional("item-flags", ItemFlag.class).ifPresent(item::addItemFlags);
+                if (itemSection.contains("recipes")) {
+                    List<Recipe> recipes = itemSection.getList("recipes", Recipe.class);
+                    if (recipes != null) item = item.copy(RecipeItem.class).setRecipes(recipes.toArray(new Recipe[0]));
+                }
 
-            if (itemSection.contains("recipes")) {
-                List<Recipe> recipes = itemSection.getList("recipes", Recipe.class);
-                if (recipes != null) item = item.copy(RecipeItem.class).setRecipes(recipes.toArray(new Recipe[0]));
+                return item;
             }
-
-            return item;
         };
     }
 
@@ -64,25 +65,25 @@ public class ItemParser extends YAMLParser<Item> {
     protected @NotNull TriConsumer<@NotNull IConfiguration, @NotNull String, @Nullable Item> getDumper() {
         return (c, s, i) -> {
             c.set(s, null);
-            if (i == null) return;
+            if (i != null) {
+                final ConfigurationSection itemSection = c.createSection(s);
+                String material = i.getMaterial();
+                if (material != null) itemSection.set("material", material.toLowerCase());
 
-            final ConfigurationSection itemSection = c.createSection(s);
-            String material = i.getMaterial();
-            if (material != null) itemSection.set("material", material.toLowerCase());
+                itemSection.set("amount", i.getAmount());
+                itemSection.set("durability", i.getDurability());
+                itemSection.set("display-name", MessageUtils.decolor(i.getDisplayName()));
+                itemSection.set("lore", i.getLore().stream().map(MessageUtils::decolor).collect(Collectors.toList()));
+                itemSection.setList("enchantments", i.getEnchantments());
+                itemSection.set("item-flags", i.getItemFlags().stream().map(Enum::name).map(String::toLowerCase).collect(Collectors.toList()));
+                itemSection.set("unbreakable", i.isUnbreakable());
+                itemSection.set("custom-model-data", i.getCustomModelData());
 
-            itemSection.set("amount", i.getAmount());
-            itemSection.set("durability", i.getDurability());
-            itemSection.set("display-name", MessageUtils.decolor(i.getDisplayName()));
-            itemSection.set("lore", i.getLore().stream().map(MessageUtils::decolor).collect(Collectors.toList()));
-            itemSection.setList("enchantments", i.getEnchantments());
-            itemSection.set("item-flags", i.getItemFlags().stream().map(Enum::name).map(String::toLowerCase).collect(Collectors.toList()));
-            itemSection.set("unbreakable", i.isUnbreakable());
-            itemSection.set("custom-model-data", i.getCustomModelData());
-
-            if (i instanceof RecipeItem) {
-                List<Recipe> recipes = new ArrayList<>();
-                ((RecipeItem) i).forEach(recipes::add);
-                itemSection.setList("recipes", recipes);
+                if (i instanceof RecipeItem) {
+                    List<Recipe> recipes = new ArrayList<>();
+                    ((RecipeItem) i).forEach(recipes::add);
+                    itemSection.setList("recipes", recipes);
+                }
             }
         };
     }
