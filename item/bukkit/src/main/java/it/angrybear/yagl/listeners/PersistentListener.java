e2a6167a9cf659739f40bd4691124cb0d51f8c96
Fulminazzo <fulminazzo21@gmail.com>
2024-04-08 02:45:56 +0200

    Rewritten interactWithPersistentItem and adjusted visibility for API usage

--- item/bukkit/src/main/java/it/angrybear/yagl/listeners/PersistentListener.java
+++ item/bukkit/src/main/java/it/angrybear/yagl/listeners/PersistentListener.java
@@ -180,14 +180,47 @@ public class PersistentListener implements Listener {
         };
     }
 
-    private boolean interactedWithPersistentItem(final @Nullable ItemStack itemStack,
-                                                 final @NotNull Player player,
-                                                 final @NotNull Action interactAction,
-                                                 final @Nullable Consumer<PersistentItem> ifPresent) {
-        return findPersistentItem(itemStack, p -> {
-            if (itemStack != null) p.interact(player, itemStack, interactAction);
-            if (ifPresent != null) ifPresent.accept(p);
-        });
+    /**
+     * Checks through every {@link ItemStack} provided for a match in {@link PersistentItem#getPersistentItem(ItemStack)}.
+     * For every match, it executes an action.
+     *
+     * @param player     the player
+     * @param interactAction  the interact action
+     * @param ifPresent  the consumer to run if the item is found
+     * @param itemStacks the item stacks
+     * @return true if it was found
+     */
+    protected boolean interactPersistentItem(final @NotNull Player player,
+                                             final @NotNull Action interactAction,
+                                             final @Nullable Consumer<PersistentItem> ifPresent,
+                                             final ItemStack @Nullable ... itemStacks) {
+        return itemStacks != null && interactPersistentItem(player, interactAction, ifPresent, Arrays.asList(itemStacks));
+    }
+
+    /**
+     * Checks through every {@link ItemStack} provided for a match in {@link PersistentItem#getPersistentItem(ItemStack)}.
+     * For every match, it executes an action.
+     *
+     * @param player     the player
+     * @param interactAction  the interact action
+     * @param ifPresent  the consumer to run if the item is found
+     * @param itemStacks the item stacks
+     * @return true if it was found
+     */
+    protected boolean interactPersistentItem(final @NotNull Player player,
+                                             final @NotNull Action interactAction,
+                                             final @Nullable Consumer<PersistentItem> ifPresent,
+                                             final @NotNull Collection<ItemStack> itemStacks) {
+        boolean found = false;
+        for (final ItemStack itemStack : itemStacks) {
+            PersistentItem persistentItem = PersistentItem.getPersistentItem(itemStack);
+            if (persistentItem != null) {
+                if (ifPresent != null) ifPresent.accept(persistentItem);
+                persistentItem.interact(player, itemStack, interactAction);
+                found = true;
+            }
+        }
+        return found;
     }
 
     /**
