3ad39fa6a71e870572a82bd232ac7bb63bf1a281
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 18:04:50 +0200

    Rewritten BlockDataOption with parseRaw method

--- wrappers/base/src/main/java/it/angrybear/yagl/particles/BlockDataOption.java
+++ wrappers/base/src/main/java/it/angrybear/yagl/particles/BlockDataOption.java
@@ -21,13 +21,9 @@ public class BlockDataOption extends ParticleOption<String> {
      * @param blockData the raw block data
      */
     public BlockDataOption(final @NotNull String blockData) {
-        Matcher matcher = Pattern.compile(NBT_REGEX).matcher(blockData);
-        if (matcher.matches()) {
-            this.material = parseMaterial(matcher.group(1));
-            String nbt = matcher.group(2);
-            if (nbt == null) nbt = "";
-            this.nbt = nbt.trim();
-        } else throw new IllegalArgumentException(String.format("Invalid block data '%s'", blockData));
+        String[] parsed = parseRaw(blockData, NBT_REGEX);
+        this.material = parsed[0];
+        this.nbt = parsed[1];
     }
 
     /**
@@ -50,7 +46,30 @@ public class BlockDataOption extends ParticleOption<String> {
         return this.nbt;
     }
 
-    static String parseMaterial(@NotNull String material) {
+    /**
+     * Converts the given data to a pair based on the given regex.
+     * Throws an {@link IllegalArgumentException} in case of failure.
+     *
+     * @param data the data
+     * @return the pair
+     */
+    static String @NotNull [] parseRaw(final @NotNull String data, final @NotNull String nbtRegex) {
+        Matcher matcher = Pattern.compile(nbtRegex).matcher(data);
+        if (matcher.matches()) {
+            String nbt = matcher.group(2);
+            if (nbt == null) nbt = "";
+            return new String[]{parseMaterial(matcher.group(1)), nbt.trim()};
+        } else throw new IllegalArgumentException(String.format("Invalid data '%s'", data));
+    }
+
+    /**
+     * Parses the given raw material by removing any white-space or <i>minecraft:</i> prepending the actual material.
+     * Throws an {@link IllegalArgumentException} in case the material is invalid.
+     *
+     * @param material the material
+     * @return the string
+     */
+    static @NotNull String parseMaterial(@NotNull String material) {
         material = material.toLowerCase();
         if (material.startsWith("minecraft:")) material = material.substring("minecraft:".length());
         if (material.trim().isEmpty() || Pattern.compile("[\r\n\t :]").matcher(material).find())
