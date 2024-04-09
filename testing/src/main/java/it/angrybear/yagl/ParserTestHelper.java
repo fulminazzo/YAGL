8f27a03c023c78c95380d5d73d29407931866f3e
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 13:27:21 +0200

    Added javadoc

--- testing/src/main/java/it/angrybear/yagl/ParserTestHelper.java
+++ testing/src/main/java/it/angrybear/yagl/ParserTestHelper.java
@@ -6,6 +6,7 @@ import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.yamlparser.configuration.IConfiguration;
 import it.fulminazzo.yamlparser.parsers.YAMLParser;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -14,34 +15,71 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+/**
+ * A class to help test a {@link YAMLParser}.
+ */
 public abstract class ParserTestHelper {
 
+    /**
+     * Tests if {@link YAMLParser#load(IConfiguration, String)} returns null when the object is null.
+     *
+     * @throws Exception the exception
+     */
     @Test
     void testLoadNull() throws Exception {
         assertNull(getLoader().apply(prepareConfiguration(null), "path"));
     }
 
+    /**
+     * Tests if {@link YAMLParser#dump(IConfiguration, String, Object)} returns null when the object is null.
+     */
     @Test
     void testSaveNull() {
         assertDoesNotThrow(() -> getDumper()
                 .accept(prepareConfiguration(null), "path", null));
     }
 
+    /**
+     * Gets loader from {@link #getYamlParser()}.
+     *
+     * @return the loader
+     */
     protected BiFunctionException<IConfiguration, String, Object> getLoader() {
         return getYamlParser().invokeMethod("getLoader");
     }
 
+    /**
+     * Gets dumper from {@link #getYamlParser()}.
+     *
+     * @return the dumper
+     */
     protected TriConsumer<IConfiguration, String, Object> getDumper() {
         return getYamlParser().invokeMethod("getLoader");
     }
 
+    /**
+     * Gets an implementation of {@link #getParser()}.
+     *
+     * @return the parser
+     */
     protected Refl<YAMLParser<?>> getYamlParser() {
         return new Refl<>(getParser(), new Object[0]);
     }
 
+    /**
+     * The {@link YAMLParser} class to test.
+     *
+     * @return the parser
+     */
     protected abstract Class<YAMLParser<?>> getParser();
 
-    protected static @NotNull IConfiguration prepareConfiguration(String returnValue) {
+    /**
+     * Creates mock of {@link IConfiguration} that returns the given value when using {@link IConfiguration#getString(String)}.
+     *
+     * @param returnValue the return value
+     * @return the configuration
+     */
+    protected static @NotNull IConfiguration prepareConfiguration(final @Nullable String returnValue) {
         IConfiguration configuration = mock(IConfiguration.class);
         when(configuration.getString(any())).thenReturn(returnValue);
         return configuration;
