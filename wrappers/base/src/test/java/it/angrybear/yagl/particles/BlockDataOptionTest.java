05d909265d0fdaa25248488430ab09695697ea64
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 18:11:42 +0200

    Added tests for invalid data and material

--- wrappers/base/src/test/java/it/angrybear/yagl/particles/BlockDataOptionTest.java
+++ wrappers/base/src/test/java/it/angrybear/yagl/particles/BlockDataOptionTest.java
@@ -1,13 +1,32 @@
 package it.angrybear.yagl.particles;
 
-import org.junit.jupiter.api.Test;
+import it.fulminazzo.fulmicollection.objects.Refl;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BlockDataOptionTest {
 
-    @Test
-    void testInvalidMaterial() {
-        assertThrowsExactly(IllegalArgumentException.class, () -> new BlockDataOption("this:item:is:invalid"));
+    private static Object[][] testData() {
+        return new Object[][]{
+                new Object[]{"this:item:is:invalid", "INVALID_MATERIAL"},
+                new Object[]{"[this one too]", "INVALID_DATA"}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void testInvalidData(String raw, String field) {
+        Throwable throwable = assertThrowsExactly(IllegalArgumentException.class, () -> new BlockDataOption(raw));
+        testThrowable(throwable, field, raw);
+    }
+
+    private void testThrowable(Throwable throwable, String fieldName, String value) {
+        final Refl<?> refl = new Refl<>(BlockDataOption.class);
+        final String actual = throwable.getMessage();
+        assertNotNull(actual, String.format("'%s' message should not be null", throwable.getClass().getSimpleName()));
+        final String expected = String.format((String) refl.getFieldObject(fieldName), value);
+        assertEquals(expected, actual);
     }
 }
