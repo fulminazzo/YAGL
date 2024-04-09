b8d7f3eeb93f5809a58a7c3bfcca83281ec5aaea
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 14:22:02 +0200

    Added tests for null ingredients list

--- item/serializer/src/test/java/it/angrybear/yagl/parsers/RecipeParserTest.java
+++ item/serializer/src/test/java/it/angrybear/yagl/parsers/RecipeParserTest.java
@@ -6,9 +6,11 @@ import it.angrybear.yagl.items.recipes.FurnaceRecipe;
 import it.angrybear.yagl.items.recipes.Recipe;
 import it.angrybear.yagl.items.recipes.ShapedRecipe;
 import it.angrybear.yagl.items.recipes.ShapelessRecipe;
+import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import it.fulminazzo.yamlparser.utils.FileUtils;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -20,6 +22,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class RecipeParserTest extends ParserTestHelper<Recipe> {
 
@@ -70,6 +73,28 @@ class RecipeParserTest extends ParserTestHelper<Recipe> {
             Object obj2 = ReflectionUtils.get(field, recipe2);
             assertEquals(obj1, obj2);
         }
+    }
+
+    @Test
+    void testNullIngredientsList() throws IOException {
+        ItemYAGLParser.addAllParsers();
+        final String path = "null-recipes";
+
+        Recipe recipe = new ShapedRecipe("recipe");
+        new Refl<>(recipe).setFieldObject("ingredients", null);
+
+        File output = new File("build/resources/test/recipe.yml");
+        if (!output.exists()) FileUtils.createNewFile(output);
+
+        FileConfiguration configuration = new FileConfiguration(output);
+        configuration.set(path, recipe);
+        configuration.save();
+
+        configuration = new FileConfiguration(output);
+        Recipe recipe2 = configuration.get(path, Recipe.class);
+
+        assertNotNull(new Refl<>(recipe2).getFieldObject("ingredients"),
+                "Recipe2 ingredients list");
     }
 
     @Override
