731f8fd9ad01e34324bba459de9c3388d84e4850
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 14:51:52 +0200

    Renamed to ValueOfTest

--- common/base/src/test/java/it/angrybear/yagl/utils/EnumUtilsTest.java
+++ common/base/src/test/java/it/angrybear/yagl/utils/EnumUtilsTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EnumUtilsTest {
 
     @Nested
-    class ValueOfTests {
+    class ValueOfTest {
 
         @Test
         void shouldThrowRealException() {
