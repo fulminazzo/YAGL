953dbe2e773197e23e9b26c3c2a710f6bcc8226c
Fulminazzo <fulminazzo21@gmail.com>
2024-04-09 13:18:40 +0200

    Added tests for save and load null

--- item/serializer/src/test/java/it/angrybear/yagl/parsers/ShapeParserTest.java
+++ item/serializer/src/test/java/it/angrybear/yagl/parsers/ShapeParserTest.java
@@ -1,6 +1,7 @@
 package it.angrybear.yagl.parsers;
 
 import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +13,26 @@ class ShapeParserTest {
 
     @Test
     void testInvalidShape() {
-        IConfiguration configuration = mock(IConfiguration.class);
-        when(configuration.getString(any())).thenReturn("INVALID");
         assertThrowsExactly(IllegalArgumentException.class, () ->
-                new ShapeParser().getLoader().apply(configuration, "a"));
+                new ShapeParser().getLoader().apply(prepareConfiguration("INVALID"),
+                        "a"));
+    }
+
+    @Test
+    void testLoadNull() throws Exception {
+        assertNull(new ShapeParser().getLoader()
+                .apply(prepareConfiguration(null), "shape"));
+    }
+
+    @Test
+    void testSaveNull() {
+        assertDoesNotThrow(() -> new ShapeParser().getDumper()
+                .accept(prepareConfiguration(null), "shape", null));
+    }
+
+    private static @NotNull IConfiguration prepareConfiguration(String returnValue) {
+        IConfiguration configuration = mock(IConfiguration.class);
+        when(configuration.getString(any())).thenReturn(returnValue);
+        return configuration;
     }
 }
