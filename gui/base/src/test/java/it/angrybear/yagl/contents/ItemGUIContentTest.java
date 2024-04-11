307e288e5ee2cec90f7d255e8e5e954a924b8aeb
Fulminazzo <fulminazzo21@gmail.com>
2024-04-11 18:59:16 +0200

    Added tests for replacement of render method

---
+++ gui/base/src/test/java/it/angrybear/yagl/contents/ItemGUIContentTest.java
@@ -0,0 +1,24 @@
+package it.angrybear.yagl.contents;
+
+import it.angrybear.yagl.items.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemGUIContentTest {
+
+    @Test
+    void testMetadatableReplacement() {
+        GUIContent guiContent = new ItemGUIContent().setMaterial("stone")
+                .setAmount(3).setDisplayName("Hello %var1%")
+                .setLore("%lore1%", "%lore2%")
+                .setVariable("var1", "world")
+                .setVariable("lore1", "Hello friend")
+                .setVariable("lore2", "Do you like this lore?");
+        Item expected = Item.newItem().setMaterial("stone")
+                .setAmount(3).setDisplayName("Hello world")
+                .setLore("Hello friend", "Do you like this lore?");
+
+        assertEquals(expected, guiContent.render());
+    }
+}
