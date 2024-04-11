fc21332098a8b7b34b1e3ab8d4a5c08885d8c5f6
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 00:38:12 +0200

    Added test suite for CommandAction and implementations

---
+++ gui/base/src/test/java/it/angrybear/yagl/actions/CommandActionTest.java
@@ -0,0 +1,42 @@
+package it.angrybear.yagl.actions;
+
+import it.angrybear.yagl.TestUtils;
+import it.fulminazzo.fulmicollection.utils.ClassUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CommandActionTest {
+
+    private static Object[] commandClasses() {
+        final Class<?> clazz = CommandAction.class;
+        return ClassUtils.findClassesInPackage(clazz.getPackage().getName()).stream()
+                .filter(clazz::isAssignableFrom).filter(c -> !c.equals(clazz))
+                .toArray(Object[]::new);
+    }
+
+    @ParameterizedTest
+    @MethodSource("commandClasses")
+    @DisplayName("Command Action Inheritors Should Call Parent execute Method On Execute")
+    void testCommandActionImplementations(Class<? extends CommandAction> clazz) throws InvocationTargetException, IllegalAccessException {
+        CommandAction action = mock(clazz);
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getParameterCount() == 1) continue;
+            if (!method.getName().equals("execute")) continue;
+
+            Class<?>[] paramTypes = method.getParameterTypes();
+            Object[] params = Arrays.stream(paramTypes).map(TestUtils::mockParameter).toArray(Object[]::new);
+
+            when(method.invoke(action, params)).thenCallRealMethod();
+            method.invoke(action, params);
+            verify(action).execute(any());
+        }
+    }
+}
