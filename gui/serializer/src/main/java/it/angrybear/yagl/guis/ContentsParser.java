0485972d02b9ad191d5807bc586d74d70d378acd
Fulminazzo <fulminazzo21@gmail.com>
2024-04-11 20:20:32 +0200

    Fixed ContentsParser not threading data as list

--- gui/serializer/src/main/java/it/angrybear/yagl/guis/ContentsParser.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/guis/ContentsParser.java
@@ -3,7 +3,6 @@ package it.angrybear.yagl.guis;
 import it.angrybear.yagl.contents.GUIContent;
 import it.fulminazzo.fulmicollection.interfaces.functions.BiFunctionException;
 import it.fulminazzo.fulmicollection.interfaces.functions.TriConsumer;
-import it.fulminazzo.yamlparser.configuration.ConfigurationSection;
 import it.fulminazzo.yamlparser.configuration.IConfiguration;
 import it.fulminazzo.yamlparser.parsers.YAMLParser;
 import org.jetbrains.annotations.NotNull;
@@ -20,8 +19,6 @@ public class ContentsParser extends YAMLParser<GUIImpl.Contents> {
     @Override
     protected BiFunctionException<@NotNull IConfiguration, @NotNull String, GUIImpl.@Nullable Contents> getLoader() {
         return (c, s) -> {
-            ConfigurationSection section = c.getConfigurationSection(s);
-            if (section == null) return null;
             List<GUIContent> contents = c.getList(s, GUIContent.class);
             if (contents == null) return null;
             return new GUIImpl.Contents(contents.toArray(new GUIContent[0]));
