80dd7a1108a659b767f4517177ed48edf28586d7
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 01:13:49 +0200

    Added tests for item methods returned values

--- gui/base/src/test/java/it/angrybear/yagl/contents/ItemGUIContentTest.java
+++ gui/base/src/test/java/it/angrybear/yagl/contents/ItemGUIContentTest.java
@@ -2,9 +2,19 @@ package it.angrybear.yagl.contents;
 
 import it.angrybear.yagl.TestUtils;
 import it.angrybear.yagl.items.Item;
+import it.angrybear.yagl.items.fields.ItemField;
+import it.angrybear.yagl.items.fields.ItemFlag;
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.fulmicollection.utils.ReflectionUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ItemGUIContentTest {
 
@@ -21,6 +31,31 @@ class ItemGUIContentTest {
                 .setLore("Hello friend", "Do you like this lore?");
 
         assertEquals(expected, guiContent.render());
+    }
+
+    @Test
+    @DisplayName("Item inherited methods should call actual methods of the inner item")
+    void testItemMethods() throws InvocationTargetException, IllegalAccessException {
+        ItemGUIContent actual = new ItemGUIContent()
+                .setMaterial("stone_sword").setAmount(1)
+                .setDurability(1337).setDisplayName("&8Destroyer")
+                .setLore("&eUse this sword to fight your enemies")
+                .addEnchantment("unbreaking", 3)
+                .addEnchantment("sharpness", 5)
+                .addItemFlags(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_PLACED_ON)
+                .setCustomModelData(1);
+        Item expected = new Refl<>(actual).getFieldObject("item");
+
+        for (Method method : Item.class.getDeclaredMethods())
+            if (!Modifier.isStatic(method.getModifiers()) && !Item.class.isAssignableFrom(method.getReturnType())) {
+                final Object[] params;
+                if (method.getName().equals("isSimilar")) params = new Object[]{expected, new ItemField[0]};
+                else params = Arrays.stream(method.getParameterTypes()).map(TestUtils::mockParameter).toArray(Object[]::new);
+
+                Object obj1 = ReflectionUtils.setAccessible(method).invoke(actual, params);
+                Object obj2 = ReflectionUtils.setAccessible(method).invoke(expected, params);
+                assertEquals(obj2, obj1);
+            }
     }
 
     @Test
