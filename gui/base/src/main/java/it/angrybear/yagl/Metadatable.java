4e573c6f804fab3a679bba326a3a3aa16e79500b
Fulminazzo <fulminazzo21@gmail.com>
2024-04-11 18:45:47 +0200

    Added javadoc

--- gui/base/src/main/java/it/angrybear/yagl/Metadatable.java
+++ gui/base/src/main/java/it/angrybear/yagl/Metadatable.java
@@ -13,6 +13,9 @@ import java.util.function.Function;
  * An interface that holds key-value pairs of {@link String}s.
  */
 public interface Metadatable extends Iterable<String> {
+    /**
+     * A function used to convert the variables in {@link #apply(Object)}.
+     */
     Function<@NotNull String, @NotNull String> VARIABLE_FORMAT = s -> "%" + s + "%";
     /**
      * A function used to parse the variables names.
