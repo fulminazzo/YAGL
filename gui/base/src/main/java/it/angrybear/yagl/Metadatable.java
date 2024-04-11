ffabd2170550758d71fba61b1e6f9de79cd1666d
Fulminazzo <fulminazzo21@gmail.com>
2024-04-11 18:34:20 +0200

    Added VARIABLE_PARSER function

--- gui/base/src/main/java/it/angrybear/yagl/Metadatable.java
+++ gui/base/src/main/java/it/angrybear/yagl/Metadatable.java
@@ -5,11 +5,33 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * An interface that holds key-value pairs of {@link String}s.
  */
 public interface Metadatable extends Iterable<String> {
+    /**
+     * A function used to parse the variables names.
+     */
+    Function<@NotNull String, @NotNull String> VARIABLE_PARSER = s -> {
+        final String separator = "_";
+
+        StringBuilder builder = new StringBuilder();
+        for (String string : s.split("")) {
+            if (string.matches("[A-Z]")) builder.append(separator);
+            else if (string.matches("[\r\t\n -]")) {
+                builder.append(separator);
+                continue;
+            }
+            builder.append(string);
+        }
+
+        String output = builder.toString().toLowerCase();
+        while (output.startsWith(separator))
+            output = output.substring(separator.length());
+        return output;
+    };
 
     /**
      * Sets variable.
