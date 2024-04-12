b8dff1c3c4256d9b033662c22050f97016275fb2
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 03:07:52 +0200

    Fixed getLoader test not working properly

--- gui/serializer/src/test/java/it/angrybear/yagl/guis/ContentsParserTest.java
+++ gui/serializer/src/test/java/it/angrybear/yagl/guis/ContentsParserTest.java
@@ -1,11 +1,25 @@
 package it.angrybear.yagl.guis;
 
 import it.angrybear.yagl.ParserTestHelper;
+import it.fulminazzo.yamlparser.configuration.IConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class ContentsParserTest extends ParserTestHelper<GUIImpl.Contents> {
 
     @Override
     protected Class<?> getParser() {
         return ContentsParser.class;
+    }
+
+    @Override
+    protected @NotNull IConfiguration getConfiguration(@Nullable String returnValue) {
+        IConfiguration configuration = super.getConfiguration(returnValue);
+        when(configuration.getList(anyString(), any())).thenReturn(null);
+        return configuration;
     }
 }
