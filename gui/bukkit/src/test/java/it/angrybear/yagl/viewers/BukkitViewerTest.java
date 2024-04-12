170d05f1a106caf6c90bdf7ee53861f6950be8ea
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 17:44:54 +0200

    Added tests for playSound of offline player

--- gui/bukkit/src/test/java/it/angrybear/yagl/viewers/BukkitViewerTest.java
+++ gui/bukkit/src/test/java/it/angrybear/yagl/viewers/BukkitViewerTest.java
@@ -1,20 +1,36 @@
 package it.angrybear.yagl.viewers;
 
+import it.angrybear.yagl.wrappers.Sound;
 import it.fulminazzo.jbukkit.BukkitUtils;
 import org.bukkit.entity.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BukkitViewerTest {
+    private Player player;
+    private Viewer viewer;
+
+    @BeforeEach
+    void setUp() {
+        BukkitUtils.setupServer();
+        this.player = BukkitUtils.addPlayer(UUID.randomUUID(), "Alex");
+        this.viewer = BukkitViewer.newViewer(this.player);
+    }
 
     @Test
     void testNewViewer() {
-        Player player = BukkitUtils.addPlayer(UUID.randomUUID(), "Alex");
-        Viewer viewer = BukkitViewer.newViewer(player);
-        assertEquals(player.getUniqueId(), viewer.getUniqueId());
-        assertEquals(player.getName(), viewer.getName());
+        assertEquals(this.player.getUniqueId(), this.viewer.getUniqueId());
+        assertEquals(this.player.getName(), this.viewer.getName());
     }
+
+    @Test
+    void testPlaySoundOffline() {
+        BukkitUtils.removePlayer(this.player);
+        assertThrowsExactly(PlayerOfflineException.class, () -> this.viewer.playSound(new Sound("tmp")));
+    }
+
 }
