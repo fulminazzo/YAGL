d62d81b1f776d70dbf5c7e10e07b9318cee99d7b
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 18:54:36 +0200

    Added private constructor

--- gui/serializer/src/main/java/it/angrybear/yagl/parsers/GUIYAGLParser.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/parsers/GUIYAGLParser.java
@@ -4,8 +4,11 @@ import it.angrybear.yagl.actions.ActionParsers;
 import it.angrybear.yagl.contents.requirements.RequirementChecker;
 import it.angrybear.yagl.guis.ContentsParser;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-public class GUIYAGLParser {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class GUIYAGLParser {
 
     public static void addAllParsers() {
         ItemYAGLParser.addAllParsers();
