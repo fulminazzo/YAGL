3a60a8d5d64e8679b9dc2f68a6420011a747974c
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 18:00:31 +0200

    Added tests for openGUI for offline player

--- gui/bukkit/src/test/java/it/angrybear/yagl/GUIAdapterTest.java
+++ gui/bukkit/src/test/java/it/angrybear/yagl/GUIAdapterTest.java
@@ -4,6 +4,7 @@ import it.angrybear.yagl.contents.GUIContent;
 import it.angrybear.yagl.contents.ItemGUIContent;
 import it.angrybear.yagl.guis.GUI;
 import it.angrybear.yagl.items.Item;
+import it.angrybear.yagl.viewers.PlayerOfflineException;
 import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.BukkitUtils;
 import org.bukkit.Bukkit;
@@ -85,6 +86,12 @@ class GUIAdapterTest {
 
         for (int i = 0; i < expected.size(); i++)
             assertNull(this.inventory.getItem(i));
+    }
+
+    @Test
+    void testOpenGUIForOfflinePlayer() {
+        BukkitUtils.removePlayer(this.player);
+        assertThrowsExactly(PlayerOfflineException.class, () -> openGUI(GUI.newGUI(9)));
     }
 
     private void openGUI(GUI gui) {
