affdb7122460bd57b3d13ced49f51ab3f361e007
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 02:58:38 +0200

    Added Javadoc

--- gui/serializer/src/main/java/it/angrybear/yagl/actions/ActionParsers.java
+++ gui/serializer/src/main/java/it/angrybear/yagl/actions/ActionParsers.java
@@ -5,9 +5,15 @@ import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+/**
+ * The type Action parsers.
+ */
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class ActionParsers {
 
+    /**
+     * Adds all parsers required to serialize {@link it.angrybear.yagl.SerializableFunction} implementations.
+     */
     public static void addParsers() {
         FileConfiguration.addParsers(new SerializableFunctionParser<>(GUIAction.class));
         FileConfiguration.addParsers(new SerializableFunctionParser<>(GUIItemAction.class));
