ebee90ea4a805ac89d3aa24c0cd84af66f94397c
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 14:53:40 +0200

    Added tests for InventoryOpenEvent

--- gui/bukkit/src/test/java/it/angrybear/yagl/GUIManagerTest.java
+++ gui/bukkit/src/test/java/it/angrybear/yagl/GUIManagerTest.java
@@ -2,21 +2,22 @@ package it.angrybear.yagl;
 
 import it.angrybear.yagl.guis.GUI;
 import it.angrybear.yagl.items.Item;
+import it.angrybear.yagl.utils.GUIUtils;
 import it.fulminazzo.jbukkit.BukkitUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.plugin.PluginManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -42,6 +43,21 @@ class GUIManagerTest {
             this.expected = GUI.newGUI(9)
                     .setTitle("Hello world")
                     .addContent(Item.newItem("stone").setDisplayName("test"));
+
+            GUIUtils.openGUI(this.expected, GUIManager.getViewer(this.player));
+        }
+
+        @Test
+        void testOpenEvent() {
+            AtomicBoolean expected = new AtomicBoolean(false);
+            this.expected.onOpenGUI((v, g) -> expected.set(true));
+
+            InventoryView view = mock(InventoryView.class);
+            when(view.getPlayer()).thenReturn(this.player);
+
+            this.guiManager.on(new InventoryOpenEvent(view));
+
+            assertTrue(expected.get(), "OpenGUI action was not invoked");
         }
 
     }
