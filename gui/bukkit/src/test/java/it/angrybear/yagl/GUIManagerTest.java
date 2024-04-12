f4332f8a6bef05a996d475b3e4d266c00c9409e9
Fulminazzo <fulminazzo21@gmail.com>
2024-04-12 14:32:23 +0200

    Added test suite for events

--- gui/bukkit/src/test/java/it/angrybear/yagl/GUIManagerTest.java
+++ gui/bukkit/src/test/java/it/angrybear/yagl/GUIManagerTest.java
@@ -1,7 +1,49 @@
 package it.angrybear.yagl;
 
+import it.angrybear.yagl.guis.GUI;
+import it.angrybear.yagl.items.Item;
+import it.fulminazzo.jbukkit.BukkitUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.PluginManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GUIManagerTest {
+
+    @Nested
+    class EventsTest {
+        private GUIManager guiManager;
+        private Player player;
+        private GUI expected;
+
+        @BeforeEach
+        void setUp() {
+            this.guiManager = new GUIManager();
+            BukkitUtils.setupServer();
+
+            Server server = Bukkit.getServer();
+            when(server.getPluginManager()).thenReturn(mock(PluginManager.class));
+
+            this.player = BukkitUtils.addPlayer(UUID.randomUUID(), "Alex");
+            when(this.player.isOnline()).thenReturn(true);
+
+            this.expected = GUI.newGUI(9)
+                    .setTitle("Hello world")
+                    .addContent(Item.newItem("stone").setDisplayName("test"));
+        }
+
+    }
 
 }
