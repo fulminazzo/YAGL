f16fc9b466d701e6e949ba164e813525927ef2c2
Fulminazzo <fulminazzo21@gmail.com>
2024-04-14 12:47:58 +0200

    Refactored code

--- example/src/main/resources/commands/GetEnchantment.groovy
+++ example/src/main/resources/commands/GetEnchantment.groovy
@@ -8,19 +8,18 @@ import org.bukkit.inventory.meta.EnchantmentStorageMeta
 
 def run = { sender, label, args ->
     if (sender instanceof Player)
-        if (args.length < 2)
+        try {
+            Enchantment enchantment = new Enchantment(args[0], Integer.valueOf(args[1]))
+            ItemStack book = new ItemStack(Material.ENCHANTED_BOOK)
+            EnchantmentStorageMeta meta = book.getItemMeta()
+            Tuple<org.bukkit.enchantments.Enchantment, Integer> tuple = WrappersAdapter.wEnchantToEnchant(enchantment)
+            meta.addStoredEnchant(tuple.getKey(), tuple.getValue(), true)
+            book.setItemMeta(meta)
+            sender.getInventory().addItem(book)
+        } catch (NumberFormatException ignored) {
+            sender.sendMessage("Invalid number '${args[1]}'")
+        } catch (IndexOutOfBoundsException ignored) {
             sender.sendMessage('Usage: /getenchantment <enchantment> <level>')
-        else
-            try {
-                Enchantment enchantment = new Enchantment(args[0], Integer.valueOf(args[1]))
-                ItemStack book = new ItemStack(Material.ENCHANTED_BOOK)
-                EnchantmentStorageMeta meta = book.getItemMeta()
-                Tuple<org.bukkit.enchantments.Enchantment, Integer> tuple = WrappersAdapter.wEnchantToEnchant(enchantment)
-                meta.addStoredEnchant(tuple.getKey(), tuple.getValue(), true)
-                book.setItemMeta(meta)
-                sender.getInventory().addItem(book)
-            } catch (NumberFormatException ignored) {
-                sender.sendMessage("Invalid number '${args[1]}'")
-            }
+        }
     else sender.sendMessage('Console cannot execute this command!')
 }
