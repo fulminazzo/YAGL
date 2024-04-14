83a172b72bcd4325abe95cb4c25c4eacf63b75ad
Fulminazzo <fulminazzo21@gmail.com>
2024-04-14 14:10:35 +0200

    Added missing option types

--- example/src/main/resources/commands/PlayEffect.groovy
+++ example/src/main/resources/commands/PlayEffect.groovy
@@ -1,12 +1,23 @@
+import it.angrybear.yagl.Color
 import it.angrybear.yagl.WrappersAdapter
+import it.angrybear.yagl.particles.ColorParticleOption
 import it.angrybear.yagl.particles.LegacyParticleType
+import it.angrybear.yagl.particles.MaterialDataOption
 import it.angrybear.yagl.particles.Particle
+import it.angrybear.yagl.particles.PotionParticleOption
 import it.angrybear.yagl.particles.PrimitiveParticleOption
+import it.angrybear.yagl.wrappers.Potion
 import it.fulminazzo.fulmicollection.objects.Refl
 import org.bukkit.entity.Player
 
 def getOption(sender, particleType, optionType, args) {
-    if (optionType);
+    if (optionType == PotionParticleOption)
+        new PotionParticleOption(new Potion(args[0], Integer.valueOf(args[1]),
+                Boolean.valueOf(args[2]), Boolean.valueOf(args[3])))
+    else if (optionType == MaterialDataOption)
+        new MaterialDataOption(args[0])
+    else if (optionType == ColorParticleOption)
+        new ColorParticleOption(Color.fromARGB(args[0]))
     else if (particleType == LegacyParticleType.SMOKE)
         new PrimitiveParticleOption<>(args[0])
     else if (particleType == LegacyParticleType.VILLAGER_PLANT_GROW)
