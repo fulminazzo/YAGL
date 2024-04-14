608456f04a529d84466c0d4eb007b02fed26fae8
Fulminazzo <fulminazzo21@gmail.com>
2024-04-14 13:55:11 +0200

    Added getOption method to convert options passed by players

--- example/src/main/resources/commands/PlayParticle.groovy
+++ example/src/main/resources/commands/PlayParticle.groovy
@@ -1,15 +1,32 @@
+import it.angrybear.yagl.Color
+import it.angrybear.yagl.ItemAdapter
 import it.angrybear.yagl.WrappersAdapter
-import it.angrybear.yagl.particles.Particle
-import it.angrybear.yagl.particles.ParticleType
+import it.angrybear.yagl.particles.*
+import it.fulminazzo.fulmicollection.objects.Refl
 import org.bukkit.entity.Player
+import org.bukkit.inventory.EquipmentSlot
+
+def getOption(sender, particleType, optionType, args) {
+    if (optionType == DustParticleOption)
+        new DustParticleOption(Color.fromARGB(args[0]), Float.valueOf(args[1]))
+    if (optionType == DustTransitionParticleOption)
+        new DustTransitionParticleOption(Color.fromARGB(args[0]), Color.fromARGB(args[1]), Float.valueOf(args[2]))
+    else if (optionType == ItemParticleOption)
+        ItemAdapter.itemStackToItem(sender.getInventory().getItem(EquipmentSlot.HAND))
+    else if (optionType == BlockDataOption)
+        new BlockDataOption(args[0])
+    throw new IllegalArgumentException("Cannot get particle option of ${optionType}")
+}
 
 def run = { sender, label, args ->
     if (sender instanceof Player) {
         try {
             ParticleType type = ParticleType.valueOf(args[0])
+            Class optionType = new Refl<>(type).getFieldObject('optionType')
             Particle particle
-            if (args.length > 1) {
-
+            if (args.length > 1 && optionType != null) {
+                def option = getOption(sender, type, optionType, Arrays.copyOfRange(args, 1, args.length))
+                particle = type.create(option)
             } else particle = type.create()
             WrappersAdapter.spawnParticle(sender, particle, sender.getEyeLocation(), 1)
         } catch (IndexOutOfBoundsException ignored) {
