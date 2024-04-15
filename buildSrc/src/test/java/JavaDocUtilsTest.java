ba5664d0a042a3f6376e3b05c13fe54343dc8ad7
Fulminazzo <fulminazzo21@gmail.com>
2024-04-15 20:59:00 +0200

    Added tests for getResource(String)

--- buildSrc/src/test/java/JavaDocUtilsTest.java
+++ buildSrc/src/test/java/JavaDocUtilsTest.java
@@ -1,6 +1,8 @@
 import org.gradle.internal.impldep.org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +16,21 @@ class JavaDocUtilsTest {
 
         Object actual = JavaDocUtils.commonPath(path1, path2);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetResource() throws IOException {
+        InputStream stream = (InputStream) JavaDocUtils.getResource("mock.txt");
+        assertNotNull(stream);
+        StringBuilder builder = new StringBuilder();
+        while (stream.available() > 0) builder.append((char) stream.read());
+        assertEquals("Hello world", builder.toString());
+    }
+
+    @Test
+    void testInvalidResource() {
+        assertThrowsExactly(IllegalArgumentException.class, () ->
+                JavaDocUtils.getResource("invalid"));
     }
 
 }
