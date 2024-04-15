9aba97a10836e4d8a11ee685690f51ef20bf5a01
Fulminazzo <fulminazzo21@gmail.com>
2024-04-15 14:29:06 +0200

    Added tests for PersistentItem initializer methods

--- item/bukkit/src/test/java/it/angrybear/yagl/items/PersistentItemTest.java
+++ item/bukkit/src/test/java/it/angrybear/yagl/items/PersistentItemTest.java
@@ -1,6 +1,7 @@
 package it.angrybear.yagl.items;
 
 import it.angrybear.yagl.items.fields.ItemFlag;
+import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.jbukkit.BukkitUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -10,7 +11,11 @@ import org.bukkit.inventory.ItemStack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -84,5 +89,35 @@ class PersistentItemTest {
         assertFalse(value.get());
         persistentItem.click(mock(Player.class), persistentItem.create(), ClickType.DOUBLE_CLICK);
         assertTrue(value.get());
+    }
+
+    @Test
+    void testStaticInitializers() {
+        List<PersistentItem> items = new Refl<>(PersistentItem.class).getStaticMethods().stream()
+                .filter(m -> m.getReturnType().equals(PersistentItem.class))
+                .filter(m -> m.getName().equals("newItem"))
+                .map(m -> {
+                    Class<?>[] paramTypes = m.getParameterTypes();
+                    Object[] params = Arrays.stream(paramTypes)
+                            .map(c -> {
+                                if (c.equals(String.class)) return "STONE";
+                                else if (c.equals(int.class)) return 1;
+                                else if (c.equals(Material.class)) return Material.STONE;
+                                else return null;
+                            }).toArray(Object[]::new);
+                    try {
+                        return m.invoke(PersistentItem.class, params);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .map(m -> (PersistentItem) m)
+                .peek(m -> {
+                    if (m.getMaterial() == null) m.setMaterial("STONE");
+                })
+                .collect(Collectors.toList());
+        for (PersistentItem i1 : items)
+            for (PersistentItem i2 : items)
+                assertEquals(i1, i2);
     }
 }
