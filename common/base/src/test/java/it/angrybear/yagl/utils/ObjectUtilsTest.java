15f43da5091989db41fb295c4beba4cd9dcf62ad
Fulminazzo <fulminazzo21@gmail.com>
2024-04-15 16:28:35 +0200

    Added tests for copy of no constructor and iterable

--- common/base/src/test/java/it/angrybear/yagl/utils/ObjectUtilsTest.java
+++ common/base/src/test/java/it/angrybear/yagl/utils/ObjectUtilsTest.java
@@ -1,6 +1,9 @@
 package it.angrybear.yagl.utils;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -13,6 +16,29 @@ class ObjectUtilsTest {
         Object o = ObjectUtils.copy(current);
         assertInstanceOf(CopyNoInterfaceImpl.class, o);
         assertNotEquals(current, o);
+    }
+
+    @Test
+    void testCopyOfIterable() {
+        Object current = new CopyIterable("hello");
+        Object o = ObjectUtils.copy(current);
+        assertInstanceOf(CopyIterableImpl.class, o);
+        assertNotEquals(current, o);
+    }
+
+    private static class CopyIterableImpl { }
+
+    private static class CopyIterable implements Iterable<String> {
+
+        public CopyIterable(String s) {
+
+        }
+
+        @NotNull
+        @Override
+        public Iterator<String> iterator() {
+            return null;
+        }
     }
 
     private static class CopyNoInterfaceImpl { }
