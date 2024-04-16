7ac3ecbff6863f11abc742d6934b319f9f59f85d
Fulminazzo <fulminazzo21@gmail.com>
2024-04-16 13:05:13 +0200

    Fixed index out of bounds exceptions

--- demo/src/main/resources/commands/OpenGUI.groovy
+++ demo/src/main/resources/commands/OpenGUI.groovy
@@ -20,7 +20,7 @@ def run = { sender, label, args ->
                 sender.sendMessage("Usage: /opengui <type|size>")
                 return
             }
-            for (int i = 0; i < columns; i += 1) {
+            for (int i = 0; i < Math.min(gui.size(), columns); i += 1) {
                 gui.setContents(i, border)
                 gui.setContents(gui.size() - i - 1, border)
             }
@@ -32,19 +32,19 @@ def run = { sender, label, args ->
             gui.setContents(middle, Item.newItem("gold_block").setDisplayName("This is a demo GUI!")
                             .addEnchantment("unbreaking", 1)
                             .addItemFlags(ItemFlag.HIDE_ENCHANTS))
-                    .setContents(middle - 1, Item.newItem("diamond_sword")
-                            .setDisplayName("Pick me!")
-                            .addEnchantment("sharpness", 2))
-                    .setContents(middle + 1, Item.newItem("diamond_pickaxe")
-                            .setDisplayName("Can't pick me...")
-                            .addEnchantment("efficiency", 10))
-                    .setMovable(middle - 1, true)
-                    .setTitle("Demo GUI")
-                    .open(GUIManager.getViewer(sender))
+            if (middle - 1 >= 0)
+                gui.setContents(middle - 1, Item.newItem("diamond_sword")
+                        .setDisplayName("Pick me!")
+                        .addEnchantment("sharpness", 2))
+                        .setMovable(middle - 1, true)
+            if (middle + 1 < gui.size())
+                gui.setContents(middle + 1, Item.newItem("diamond_pickaxe")
+                        .setDisplayName("Can't pick me...")
+                        .addEnchantment("efficiency", 10))
+            gui.setTitle("Demo GUI").open(GUIManager.getViewer(sender))
         } catch (NumberFormatException ignored) {
 
         } catch (Exception e) {
-            e.printStackTrace()
             sender.sendMessage(e.getMessage())
         }
     else sender.sendMessage('Console cannot execute this command!')
