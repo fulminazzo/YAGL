873c428eb28d0e96419a277ab4c15f4de8ea13ec
Fulminazzo <fulminazzo21@gmail.com>
2024-04-18 23:25:38 +0200

    Overridden methods return type

--- gui/base/src/main/java/it/angrybear/yagl/guis/DataGUI.java
+++ gui/base/src/main/java/it/angrybear/yagl/guis/DataGUI.java
@@ -1,8 +1,14 @@
 package it.angrybear.yagl.guis;
 
+import it.angrybear.yagl.Metadatable;
+import it.angrybear.yagl.actions.BiGUIAction;
+import it.angrybear.yagl.actions.GUIAction;
 import it.angrybear.yagl.contents.GUIContent;
+import it.angrybear.yagl.contents.ItemGUIContent;
+import it.angrybear.yagl.items.Item;
 import it.angrybear.yagl.viewers.Viewer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -16,6 +22,7 @@ import java.util.function.Predicate;
  *
  * @param <T> the type of the data
  */
+@SuppressWarnings("unchecked")
 public class DataGUI<T> extends PageableGUI {
     private static final String ERROR_MESSAGE = "Pages are dynamically calculated when opening this GUI. They cannot be singly edited";
 
@@ -202,6 +209,531 @@ public class DataGUI<T> extends PageableGUI {
     @Override
     public @NotNull DataGUI<T> setPages(int pages) {
         throw new IllegalStateException(ERROR_MESSAGE);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setPreviousPage(int slot, @NotNull Item previousPage) {
+        return (DataGUI<T>) super.setPreviousPage(slot, previousPage);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setPreviousPage(int slot, @NotNull GUIContent previousPage) {
+        return (DataGUI<T>) super.setPreviousPage(slot, previousPage);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetPreviousPage() {
+        return (DataGUI<T>) super.unsetPreviousPage();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNextPage(int slot, @NotNull Item nextPage) {
+        return (DataGUI<T>) super.setNextPage(slot, nextPage);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNextPage(int slot, @NotNull GUIContent nextPage) {
+        return (DataGUI<T>) super.setNextPage(slot, nextPage);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetNextPage() {
+        return (DataGUI<T>) super.unsetNextPage();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTitle(@Nullable String title) {
+        return (DataGUI<T>) super.setTitle(title);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMovable(int slot, boolean movable) {
+        return (DataGUI<T>) super.setMovable(slot, movable);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> addContent(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.addContent(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setContents(int slot, GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setContents(slot, contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setContents(int slot, @NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setContents(slot, contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetContent(int slot) {
+        return (DataGUI<T>) super.unsetContent(slot);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onClickOutside(@NotNull GUIAction action) {
+        return (DataGUI<T>) super.onClickOutside(action);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onOpenGUI(@NotNull GUIAction action) {
+        return (DataGUI<T>) super.onOpenGUI(action);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onCloseGUI(@NotNull GUIAction action) {
+        return (DataGUI<T>) super.onCloseGUI(action);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onChangeGUI(@NotNull BiGUIAction action) {
+        return (DataGUI<T>) super.onChangeGUI(action);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllMovable() {
+        return (DataGUI<T>) super.setAllMovable();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllUnmovable() {
+        return (DataGUI<T>) super.setAllUnmovable();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> addContent(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.addContent(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> addContent(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.addContent(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setContents(int slot, Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setContents(slot, contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setContents(int slot, ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setContents(slot, contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllSides(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setAllSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllSides(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setAllSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllSides(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setAllSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setAllSides(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setAllSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetAllSides() {
+        return (DataGUI<T>) super.unsetAllSides();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopAndBottomSides(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopAndBottomSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopAndBottomSides(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopAndBottomSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopAndBottomSides(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopAndBottomSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopAndBottomSides(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setTopAndBottomSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetTopAndBottomSides() {
+        return (DataGUI<T>) super.unsetTopAndBottomSides();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftAndRightSides(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftAndRightSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftAndRightSides(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftAndRightSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftAndRightSides(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftAndRightSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftAndRightSides(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setLeftAndRightSides(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetLeftAndRightSides() {
+        return (DataGUI<T>) super.unsetLeftAndRightSides();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopSide(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopSide(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopSide(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setTopSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setTopSide(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setTopSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetTopSide() {
+        return (DataGUI<T>) super.unsetTopSide();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftSide(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftSide(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftSide(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setLeftSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setLeftSide(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setLeftSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetLeftSide() {
+        return (DataGUI<T>) super.unsetLeftSide();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setBottomSide(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setBottomSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setBottomSide(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setBottomSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setBottomSide(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setBottomSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setBottomSide(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setBottomSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetBottomSide() {
+        return (DataGUI<T>) super.unsetBottomSide();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setRightSide(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setRightSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setRightSide(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setRightSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setRightSide(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setRightSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setRightSide(@NotNull Collection<GUIContent> contents) {
+        return (DataGUI<T>) super.setRightSide(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetRightSide() {
+        return (DataGUI<T>) super.unsetRightSide();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthWest(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthWest(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthWest(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetNorthWest() {
+        return (DataGUI<T>) super.unsetNorthWest();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorth(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorth(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorth(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetNorth() {
+        return (DataGUI<T>) super.unsetNorth();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthEast(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthEast(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setNorthEast(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setNorthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetNorthEast() {
+        return (DataGUI<T>) super.unsetNorthEast();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleWest(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleWest(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleWest(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetMiddleWest() {
+        return (DataGUI<T>) super.unsetMiddleWest();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddle(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddle(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddle(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddle(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddle(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddle(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetMiddle() {
+        return (DataGUI<T>) super.unsetMiddle();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleEast(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleEast(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setMiddleEast(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setMiddleEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetMiddleEast() {
+        return (DataGUI<T>) super.unsetMiddleEast();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthWest(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthWest(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthWest(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthWest(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetSouthWest() {
+        return (DataGUI<T>) super.unsetSouthWest();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouth(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouth(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouth(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouth(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetSouth() {
+        return (DataGUI<T>) super.unsetSouth();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthEast(Item @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthEast(ItemGUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setSouthEast(GUIContent @NotNull ... contents) {
+        return (DataGUI<T>) super.setSouthEast(contents);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetSouthEast() {
+        return (DataGUI<T>) super.unsetSouthEast();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> clear() {
+        return (DataGUI<T>) super.clear();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onClickOutside(@NotNull String command) {
+        return (DataGUI<T>) super.onClickOutside(command);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onOpenGUI(@NotNull String command) {
+        return (DataGUI<T>) super.onOpenGUI(command);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onCloseGUI(@NotNull String command) {
+        return (DataGUI<T>) super.onCloseGUI(command);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> onChangeGUI(@NotNull String command) {
+        return (DataGUI<T>) super.onChangeGUI(command);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> setVariable(@NotNull String name, @NotNull String value) {
+        return (DataGUI<T>) super.setVariable(name, value);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> unsetVariable(@NotNull String name) {
+        return (DataGUI<T>) super.unsetVariable(name);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> copyAll(@NotNull GUI other, boolean replace) {
+        return (DataGUI<T>) super.copyAll(other, replace);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> copyFrom(@NotNull GUI other, boolean replace) {
+        return (DataGUI<T>) super.copyFrom(other, replace);
+    }
+
+    @Override
+    public DataGUI<T> copy() {
+        return (DataGUI<T>) super.copy();
+    }
+
+    @Override
+    public @NotNull DataGUI<T> copyAll(@NotNull Metadatable other, boolean replace) {
+        return (DataGUI<T>) super.copyAll(other, replace);
+    }
+
+    @Override
+    public @NotNull DataGUI<T> copyFrom(@NotNull Metadatable other, boolean replace) {
+        return (DataGUI<T>) super.copyFrom(other, replace);
     }
 
     /**
