db8dad215e29e2a873db401b977ca7d61ec1b244
Fulminazzo <fulminazzo21@gmail.com>
2024-04-26 22:54:46 +0200

    Added test suite for GUIItemClose

--- gui/serializer/src/test/java/it/fulminazzo/yagl/actions/ActionParsersTest.java
+++ gui/serializer/src/test/java/it/fulminazzo/yagl/actions/ActionParsersTest.java
@@ -2,13 +2,14 @@ package it.fulminazzo.yagl.actions;
 
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
 import it.fulminazzo.yamlparser.utils.FileUtils;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ActionParsersTest {
 
@@ -33,5 +34,22 @@ class ActionParsersTest {
         configuration = new FileConfiguration(file);
         CommandAction action1 = configuration.get("tmp", action.getClass());
         assertEquals(action, action1);
+    }
+
+    @Test
+    void testCloseGUI() throws IOException {
+        GUIItemAction expected = new GUIItemClose();
+        ActionParsers.addParsers();
+        File file = new File("build/resources/test/close-action.yml");
+        if (file.exists()) FileUtils.deleteFile(file);
+        FileUtils.createNewFile(file);
+        FileConfiguration configuration = new FileConfiguration(file);
+        configuration.set("tmp", expected);
+        configuration.save();
+        configuration = new FileConfiguration(file);
+        GUIItemAction actual = configuration.get("tmp", expected.getClass());
+        assertNotNull(actual);
+        assertEquals(expected.getClass(), actual.getClass());
+        assertNull(configuration.getString("tmp.content"), "Content should be empty");
     }
 }
