65e0736e9dd3367ac3441329698d34efb7b25e1e
Fulminazzo <fulminazzo21@gmail.com>
2024-12-13 00:53:26 +0100

    Added test suite for SingleInstance

---
+++ common/base/src/test/java/it/fulminazzo/yagl/SingleInstanceTest.java
@@ -0,0 +1,66 @@
+package it.fulminazzo.yagl;
+
+import it.fulminazzo.fulmicollection.objects.Refl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleInstanceTest {
+
+    @BeforeEach
+    void setUp() {
+        // Manually clear instances map to avoid conflicts
+        new Refl<>(SingleInstance.class)
+                .getFieldRefl("INSTANCES_MAP")
+                .invokeMethod("clear");
+    }
+
+    @Test
+    void testNormalBehaviour() {
+        SingleInstance instance = new MockInstance();
+        assertDoesNotThrow(() -> {
+            instance.initialize();
+            SingleInstance.getInstance(MockInstance.class);
+            instance.terminate();
+        });
+    }
+
+    @Test
+    void testInitAlreadyInitialized() {
+        SingleInstance instance = new MockInstance();
+        instance.initialize();
+        InstanceAlreadyInitializedException exception = assertThrowsExactly(
+                InstanceAlreadyInitializedException.class, instance::initialize
+        );
+        assertEquals(instance, exception.getInstance());
+    }
+
+    @Test
+    void testTerminateNotInitialized() {
+        SingleInstance instance = new MockInstance();
+        InstanceNotInitialized exception = assertThrowsExactly(
+                InstanceNotInitialized.class, instance::terminate
+        );
+        assertEquals("Instance of type \"" + MockInstance.class.getSimpleName() +
+                "\" has not been initialized yet", exception.getMessage());
+    }
+
+    @Test
+    void testGetInstanceNotInitialized() {
+        InstanceNotInitialized exception = assertThrowsExactly(
+                InstanceNotInitialized.class, () -> SingleInstance.getInstance(MockInstance.class)
+        );
+        assertEquals("Instance of type \"" + MockInstance.class.getSimpleName() +
+                "\" has not been initialized yet", exception.getMessage());
+    }
+
+    private static class MockInstance extends SingleInstance {
+
+        public MockInstance() {
+
+        }
+
+    }
+
+}
