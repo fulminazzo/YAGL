65c04f18f4ac8096cc59c882248a5ef1392c40af
Fulminazzo <fulminazzo21@gmail.com>
2024-12-20 22:09:12 +0100

    Updated ObsoleteItemAdapterTest range to Minecraft 1.12.2 and below

--- item/bukkit/obsolete/src/test/java/it/fulminazzo/yagl/ObsoleteItemAdapterTest.java
+++ item/bukkit/obsolete/src/test/java/it/fulminazzo/yagl/ObsoleteItemAdapterTest.java
@@ -20,7 +20,7 @@ import java.lang.reflect.Field;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
-@Before1_(11.2)
+@Before1_(12.2)
 class ObsoleteItemAdapterTest extends BukkitUtils {
 
     @BeforeEach
