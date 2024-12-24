83a00a4c6e0d2ecaa54b9e1cf2f8fb489893d9af
Fulminazzo <fulminazzo21@gmail.com>
2024-12-24 16:42:14 +0100

    Added tests for TestUtils and Jacoco coverage

--- testing/base/src/test/java/it/fulminazzo/yagl/TestUtilsTest.java
+++ testing/base/src/test/java/it/fulminazzo/yagl/TestUtilsTest.java
@@ -1,8 +1,13 @@
 package it.fulminazzo.yagl;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.fulmicollection.utils.ExceptionUtils;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.Mockito.*;
 
 class TestUtilsTest {
 
@@ -12,6 +17,18 @@ class TestUtilsTest {
         assertThrowsExactly(MockExecutor.MockException.class, () ->
                 TestUtils.testSingleMethod(executor, MockExecutor.class.getDeclaredMethod("throwException"),
                         new Object[0], executor, "throwException"));
+    }
+
+    @Test
+    void mockNoExceptionThrownForJacocoCoverage() throws NoSuchMethodException {
+        try (MockedStatic<ExceptionUtils> ignored = mockStatic(ExceptionUtils.class)) {
+            when(new Refl<>(ExceptionUtils.class).invokeMethod("throwException", any(Exception.class))).then(a -> null);
+            MockExecutor executor = new MockExecutor();
+            Object object = TestUtils.testSingleMethod(executor,
+                    MockExecutor.class.getDeclaredMethod("throwException"),
+                    new Object[0], executor, "throwException");
+            assertNull(object);
+        }
     }
 
 }
