817c9ac5fd3629a3c84a4a146236dd2480c29b29
Fulminazzo <fulminazzo21@gmail.com>
2024-12-24 18:22:57 +0100

    Fixed PotionWrapper not available for Minecraft 1.20.5+

--- wrappers/bukkit/src/test/java/it/fulminazzo/yagl/PotionWrapperTest.java
+++ wrappers/bukkit/src/test/java/it/fulminazzo/yagl/PotionWrapperTest.java
@@ -1,13 +1,17 @@
 package it.fulminazzo.yagl;
 
+import it.fulminazzo.jbukkit.BukkitUtils;
+import it.fulminazzo.jbukkit.annotations.Before1_;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
-public class PotionWrapperTest {
+public class PotionWrapperTest extends BukkitUtils {
 
     @Test
+    @Before1_(20.4)
     void testCreationOfPotionWrapperFromForeignObject() {
+        super.setUp();
         assertThrowsExactly(IllegalArgumentException.class, () -> new PotionWrapper("Mock potion"));
     }
 
