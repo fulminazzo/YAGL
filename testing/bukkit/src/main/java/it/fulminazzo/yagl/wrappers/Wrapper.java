6b42de5d4346b86bde0f3b668d7ba0754d6e8dfe
Fulminazzo <fulminazzo21@gmail.com>
2024-12-24 03:51:21 +0100

    Created Wrapper class

---
+++ testing/bukkit/src/main/java/it/fulminazzo/yagl/wrappers/Wrapper.java
@@ -0,0 +1,35 @@
+package it.fulminazzo.yagl.wrappers;
+
+import it.fulminazzo.fulmicollection.objects.Refl;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a wrapper for an object.
+ * This is required for Minecraft version 1.20.6+ tests since
+ * many objects are not an abstract classes anymore.
+ *
+ * @param <O> the type parameter
+ */
+public abstract class Wrapper<O> {
+    protected final Refl<O> internalObject;
+
+    /**
+     * Instantiates a new Wrapper.
+     *
+     * @param clazz the clazz
+     * @param parameters  the parameters
+     */
+    protected Wrapper(final @NotNull Class<O> clazz, final Object @NotNull ... parameters) {
+        this.internalObject = new Refl<>(clazz, parameters);
+    }
+
+    /**
+     * Gets the wrapped object.
+     *
+     * @return the object
+     */
+    public @NotNull O getWrapped() {
+        return this.internalObject.getObject();
+    }
+
+}
