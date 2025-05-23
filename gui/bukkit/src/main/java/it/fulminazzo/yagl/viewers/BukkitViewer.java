2492d8622d11b4400e65d2fa9048bcdc71c0612c
user <fulminazzo21@gmail.com>
2025-05-23 23:26:17 +0200

    !release **Totally reworked whole project**. Added compatibility for **Minecraft 1.21**. Added [wiki](https://github.com/Fulminazzo/YAGL/wiki) for both **server admins** and **developers**. Added several **modules** and **submodules** with proper documentation. Added several automatic **Github actions**. Renamed artifacts with _lowercase_ names: now the project will be accessible from the [Fulminazzo repository](https://repo.fulminazzo.it/#/releases) as `it.fulminazzo:yagl`. Renamed packages to `it.fulminazzo`. Reworked [README.md](https://github.com/Fulminazzo/YAGL). Updated [JBukkit library](https://github.com/Fulminazzo/JBukkit).

--- gui/bukkit/src/main/java/it/fulminazzo/yagl/viewers/BukkitViewer.java
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/viewers/BukkitViewer.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 /**
  * An implementation of {@link Viewer} for the Bukkit platform.
  */
-public class BukkitViewer extends Viewer {
+class BukkitViewer extends Viewer {
 
     /**
      * Instantiates a new Viewer.
