abf439c0b604875452559cd6a0f5680db76f3958
user <fulminazzo21@gmail.com>
2025-06-14 22:55:06 +0200

    Made FullSizeGUI implement GUI and implemented several methods

--- gui/base/src/main/java/it/fulminazzo/yagl/guis/FullSizeGUI.java
+++ gui/base/src/main/java/it/fulminazzo/yagl/guis/FullSizeGUI.java
@@ -1,8 +1,17 @@
 package it.fulminazzo.yagl.guis;
 
 import it.fulminazzo.fulmicollection.objects.FieldEquable;
+import it.fulminazzo.yagl.actions.BiGUIAction;
+import it.fulminazzo.yagl.actions.GUIAction;
+import it.fulminazzo.yagl.contents.GUIContent;
+import it.fulminazzo.yagl.viewers.Viewer;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Represents a {@link GUI} that supports a full size display.
@@ -10,7 +19,7 @@ import org.jetbrains.annotations.NotNull;
  * show contents.
  */
 @Getter
-public class FullSizeGUI extends FieldEquable {
+public class FullSizeGUI extends FieldEquable implements GUI {
     private static final int SECOND_INVENTORY_SIZE = 45;
 
     private final @NotNull GUI upperGUI;
@@ -58,6 +67,135 @@ public class FullSizeGUI extends FieldEquable {
         GUIUtils.checkSlot(slot, size());
         if (slot >= upperGUI.size()) return lowerGUI;
         else return upperGUI;
+    }
+
+    @Override
+    public void open(@NotNull Viewer viewer) {
+        //TODO:
+    }
+
+    @Override
+    public @NotNull GUI setTitle(@Nullable String title) {
+        this.upperGUI.setTitle(title);
+        return this;
+    }
+
+    @Override
+    public @Nullable String getTitle() {
+        return this.upperGUI.getTitle();
+    }
+
+    @Override
+    public int size() {
+        return this.upperGUI.size() + this.lowerGUI.size();
+    }
+
+    @Override
+    public boolean isMovable(int slot) {
+        return getCorrespondingGUI(slot).isMovable(getCorrespondingSlot(slot));
+    }
+
+    @Override
+    public @NotNull GUI setMovable(int slot, boolean movable) {
+        getCorrespondingGUI(slot).setMovable(getCorrespondingSlot(slot), movable);
+        return this;
+    }
+
+    @Override
+    public @NotNull List<GUIContent> getContents(int slot) {
+        return getCorrespondingGUI(slot).getContents(getCorrespondingSlot(slot));
+    }
+
+    @Override
+    public @NotNull List<GUIContent> getContents() {
+        return Stream.concat(
+                this.upperGUI.getContents().stream(),
+                this.lowerGUI.getContents().stream()
+        ).collect(Collectors.toList());
+    }
+
+    @Override
+    public @NotNull GUI addContent(GUIContent @NotNull ... contents) {
+        //TODO:
+        return null;
+    }
+
+    @Override
+    public @NotNull GUI setContents(int slot, GUIContent @NotNull ... contents) {
+        getCorrespondingGUI(slot).setContents(getCorrespondingSlot(slot), contents);
+        return this;
+    }
+
+    @Override
+    public @NotNull GUI unsetContent(int slot) {
+        getCorrespondingGUI(slot).unsetContent(getCorrespondingSlot(slot));
+        return this;
+    }
+
+    @Override
+    public int rows() {
+        return this.upperGUI.rows() + this.lowerGUI.rows();
+    }
+
+    @Override
+    public int columns() {
+        return this.upperGUI.columns() + this.lowerGUI.columns();
+    }
+
+    @Override
+    public @NotNull GUI clear() {
+        this.upperGUI.clear();
+        this.lowerGUI.clear();
+        return this;
+    }
+
+    @Override
+    public @NotNull GUI onClickOutside(@NotNull GUIAction action) {
+        this.upperGUI.onClickOutside(action);
+        return this;
+    }
+
+    @Override
+    public @NotNull Optional<GUIAction> clickOutsideAction() {
+        return this.upperGUI.clickOutsideAction();
+    }
+
+    @Override
+    public @NotNull GUI onOpenGUI(@NotNull GUIAction action) {
+        this.upperGUI.onOpenGUI(action);
+        return this;
+    }
+
+    @Override
+    public @NotNull Optional<GUIAction> openGUIAction() {
+        return this.upperGUI.openGUIAction();
+    }
+
+    @Override
+    public @NotNull GUI onCloseGUI(@NotNull GUIAction action) {
+        this.upperGUI.onCloseGUI(action);
+        return this;
+    }
+
+    @Override
+    public @NotNull Optional<GUIAction> closeGUIAction() {
+        return this.upperGUI.closeGUIAction();
+    }
+
+    @Override
+    public @NotNull GUI onChangeGUI(@NotNull BiGUIAction action) {
+        this.upperGUI.onChangeGUI(action);
+        return this;
+    }
+
+    @Override
+    public @NotNull Optional<BiGUIAction> changeGUIAction() {
+        return this.upperGUI.changeGUIAction();
+    }
+
+    @Override
+    public @NotNull Map<String, String> variables() {
+        return this.upperGUI.variables();
     }
 
 }
