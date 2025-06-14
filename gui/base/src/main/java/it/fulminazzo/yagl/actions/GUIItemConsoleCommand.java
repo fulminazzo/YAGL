8ab9b1e93f911ae12e326e152dc79047bad348f0
user <fulminazzo21@gmail.com>
2025-06-14 19:50:41 +0200

    Created GUIItemConsoleCommand class. Closes #57

---
+++ gui/base/src/main/java/it/fulminazzo/yagl/actions/GUIItemConsoleCommand.java
@@ -0,0 +1,27 @@
+package it.fulminazzo.yagl.actions;
+
+import it.fulminazzo.yagl.contents.GUIContent;
+import it.fulminazzo.yagl.guis.GUI;
+import it.fulminazzo.yagl.viewers.Viewer;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An implementation of {@link GUIItemAction} that forces the execution of
+ * the given command upon {@link #execute(Viewer, GUI, GUIContent)} from the console.
+ */
+public class GUIItemConsoleCommand extends CommandAction implements GUIItemAction {
+
+    /**
+     * Instantiates a new GUIItemConsoleCommand.
+     *
+     * @param command the command
+     */
+    public GUIItemConsoleCommand(@NotNull String command) {
+        super(command);
+    }
+
+    @Override
+    public void execute(@NotNull Viewer viewer, @NotNull GUI gui, @NotNull GUIContent content) {
+        viewer.consoleExecute(this.command);
+    }
+}
