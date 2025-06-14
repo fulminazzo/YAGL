5b94e1b2535bd1eedc3d783b43c9f81bbcbc761b
user <fulminazzo21@gmail.com>
2025-06-14 22:40:13 +0200

    Added getCorrespondingSlot and getCorrespondingGUI methods

--- gui/base/src/main/java/it/fulminazzo/yagl/guis/FullSizeGUI.java
+++ gui/base/src/main/java/it/fulminazzo/yagl/guis/FullSizeGUI.java
@@ -1,9 +1,11 @@
 package it.fulminazzo.yagl.guis;
 
 import it.fulminazzo.fulmicollection.objects.FieldEquable;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
-public class FullSizeGUI extends FieldEquable implements GUI {
+@Getter
+public class FullSizeGUI extends FieldEquable {
     private static final int SECOND_INVENTORY_SIZE = 45;
 
     private final @NotNull GUI upperGUI;
@@ -17,6 +19,16 @@ public class FullSizeGUI extends FieldEquable implements GUI {
     FullSizeGUI(final @NotNull GUIType type) {
         this.upperGUI = GUI.newGUI(type);
         this.lowerGUI = GUI.newGUI(SECOND_INVENTORY_SIZE);
+    }
+
+    int getCorrespondingSlot(final int slot) {
+        if (slot >= upperGUI.size()) return slot - upperGUI.size();
+        else return slot;
+    }
+
+    @NotNull GUI getCorrespondingGUI(final int slot) {
+        if (slot >= upperGUI.size()) return lowerGUI;
+        else return upperGUI;
     }
 
 }
