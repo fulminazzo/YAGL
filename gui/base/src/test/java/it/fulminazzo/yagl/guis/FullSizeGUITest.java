147ae0753e88b7f20abacd46a7f0dba349ef255c
user <fulminazzo21@gmail.com>
2025-06-14 23:03:10 +0200

    Added tests for methods of FullSizeGUI that wrap upperGUI

--- gui/base/src/test/java/it/fulminazzo/yagl/guis/FullSizeGUITest.java
+++ gui/base/src/test/java/it/fulminazzo/yagl/guis/FullSizeGUITest.java
@@ -1,10 +1,18 @@
 package it.fulminazzo.yagl.guis;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.yagl.TestUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class FullSizeGUITest {
 
@@ -44,6 +52,32 @@ class FullSizeGUITest {
         GUI actual = gui.getCorrespondingGUI(slot);
 
         assertInstanceOf(expectedClass, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"setTitle", "getTitle",
+            "onClickOutside", "clickOutsideAction",
+            "onOpenGUI", "openGUIAction",
+            "onCloseGUI", "closeGUIAction",
+            "onChangeGUI", "changeGUIAction",
+            "variables"})
+    void testFullSizeGUIDelegatesToUpperGUI(String methodName) {
+        Refl<?> fullSizeGUI = new Refl<>(new FullSizeGUI(9));
+
+        GUI upperGUI = mock(GUI.class);
+        fullSizeGUI.setFieldObject("upperGUI", upperGUI);
+
+        Method method = Arrays.stream(FullSizeGUI.class.getMethods())
+                .filter(m -> m.getName().equals(methodName))
+                .findFirst().orElseThrow(IllegalStateException::new);
+
+        Object[] parameters = Arrays.stream(method.getParameterTypes())
+                .map(TestUtils::mockParameter)
+                .toArray(Object[]::new);
+
+        fullSizeGUI.invokeMethod(methodName, parameters);
+
+        new Refl<>(verify(upperGUI)).invokeMethod(methodName, parameters);
     }
 
 }
