b08e2caa4a75f6dc268977a44e2d5289a12a1495
user <fulminazzo21@gmail.com>
2025-06-15 20:16:26 +0200

    Added tests for copyAll and copy methods

--- gui/base/src/test/java/it/fulminazzo/yagl/guis/SearchGUITest.java
+++ gui/base/src/test/java/it/fulminazzo/yagl/guis/SearchGUITest.java
@@ -1,11 +1,39 @@
 package it.fulminazzo.yagl.guis;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.yagl.TestUtils;
+import it.fulminazzo.yagl.contents.ItemGUIContent;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SearchGUITest {
+
+    @Test
+    void testCopy() {
+        SearchGUI<String> src = SearchGUI.newGUI(s -> null, (f, s) -> true);
+        src.setContents(0, ItemGUIContent.newInstance("stone"));
+
+        SearchGUI<String> dst = src.copy();
+
+        new Refl<>(dst).setFieldObject("searchFunction", new Refl<>(src).getFieldObject("searchFunction"));
+        assertEquals(src, dst);
+    }
+
+    @Test
+    void testCopyAll() {
+        SearchGUI<String> src = SearchGUI.newGUI(s -> null, (f, s) -> true);
+        src.setContents(0, ItemGUIContent.newInstance("stone"));
+
+        SearchGUI<String> dst = SearchGUI.newGUI(s -> null, (f, s) -> true);
+
+        src.copyAll(dst, true);
+
+        new Refl<>(dst).setFieldObject("searchFunction", new Refl<>(src).getFieldObject("searchFunction"));
+        assertEquals(src, dst);
+    }
 
     @Test
     void testReturnTypes() {
