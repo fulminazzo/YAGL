1656d3b457052d4eaee0c0c64bd6ac176d2f31c2
user <fulminazzo21@gmail.com>
2025-06-15 19:32:57 +0200

    Made SearchGUI extend DataGUi and added SearchFullSizeGUI logic

--- gui/base/src/main/java/it/fulminazzo/yagl/guis/SearchGUI.java
+++ gui/base/src/main/java/it/fulminazzo/yagl/guis/SearchGUI.java
@@ -1,18 +1,32 @@
 package it.fulminazzo.yagl.guis;
 
+import it.fulminazzo.yagl.contents.GUIContent;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Function;
+
 /**
  * A special implementation of {@link DataGUI} that supports dynamic
  * searching of data from a string.
  */
-public final class SearchGUI {
+public final class SearchGUI<T> extends DataGUI<T> {
     @Getter
     @Setter
     private @Nullable String query;
+
+    /**
+     * Instantiates a new Search gui.
+     *
+     * @param templateGUI   the templateGUI
+     * @param dataConverter the data converter
+     */
+    SearchGUI(@NotNull SearchFullSizeGUI templateGUI, @NotNull Function<T, GUIContent> dataConverter) {
+        super(templateGUI, dataConverter);
+        templateGUI.setSearchGui(this);
+    }
 
     /**
      * An implementation of {@link FullSizeGUI} that provides methods to interface with
@@ -20,7 +34,7 @@ public final class SearchGUI {
      */
     static class SearchFullSizeGUI extends FullSizeGUI {
         @Setter
-        private @Nullable SearchGUI searchGui;
+        private @Nullable SearchGUI<?> searchGui;
 
         /**
          * Instantiates a new Search full size gui.
@@ -55,7 +69,7 @@ public final class SearchGUI {
          *
          * @return the search gui
          */
-        public @NotNull SearchGUI getSearchGui() {
+        public @NotNull SearchGUI<?> getSearchGui() {
             if (this.searchGui == null) throw new IllegalStateException("SearchGUI has not been set yet.");
             return this.searchGui;
         }
