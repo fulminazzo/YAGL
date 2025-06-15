e4051317d07f1e10841c5f69434cec6e1bb3399a
user <fulminazzo21@gmail.com>
2025-06-15 19:24:13 +0200

    Added internal FullSizeGUI implementation

--- gui/base/src/main/java/it/fulminazzo/yagl/guis/SearchGUI.java
+++ gui/base/src/main/java/it/fulminazzo/yagl/guis/SearchGUI.java
@@ -2,6 +2,7 @@ package it.fulminazzo.yagl.guis;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -12,5 +13,44 @@ public final class SearchGUI {
     @Getter
     @Setter
     private @Nullable String query;
+
+    /**
+     * An implementation of {@link FullSizeGUI} that provides methods to interface with
+     * the corresponding {@link SearchGUI}.
+     */
+    static class SearchFullSizeGUI extends FullSizeGUI {
+        private final @NotNull SearchGUI gui;
+
+        /**
+         * Instantiates a new Search full size gui.
+         *
+         * @param gui the gui
+         */
+        SearchFullSizeGUI(final @NotNull SearchGUI gui) {
+            super(GUIType.ANVIL);
+            this.gui = gui;
+        }
+
+        /**
+         * Gets the search text from the search GUI.
+         *
+         * @return the query
+         */
+        public @Nullable String getQuery() {
+            return this.gui.getQuery();
+        }
+
+        /**
+         * Sets the search text to the search GUI.
+         *
+         * @param query the query
+         * @return this gui
+         */
+        public @NotNull SearchFullSizeGUI setQuery(final @Nullable String query) {
+            this.gui.setQuery(query);
+            return this;
+        }
+
+    }
 
 }
