3511b71efbae51732ff7fc636b9adab4341b83d2
user <fulminazzo21@gmail.com>
2025-06-15 19:50:18 +0200

    Added tests for SearchGUI return types

---
+++ gui/base/src/test/java/it/fulminazzo/yagl/guis/SearchGUITest.java
@@ -0,0 +1,19 @@
+package it.fulminazzo.yagl.guis;
+
+import it.fulminazzo.yagl.TestUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class SearchGUITest {
+
+    @Test
+    void testReturnTypes() {
+        TestUtils.testReturnType(SearchGUI.newGUI(27, c -> null, (t, s) -> false), DataGUI.class, m -> {
+            for (String s : Arrays.asList("copy", "setPages", "getPage"))
+                if (s.equals(m.getName())) return true;
+            return false;
+        });
+    }
+
+}
