9edd7977420ee7da00c9595c131440481fcdf405
user <fulminazzo21@gmail.com>
2025-06-16 15:53:18 +0200

    Added tests for normal read

--- gui/bukkit/src/test/java/it/fulminazzo/yagl/handlers/AnvilRenameHandlerTest.java
+++ gui/bukkit/src/test/java/it/fulminazzo/yagl/handlers/AnvilRenameHandlerTest.java
@@ -1,7 +1,11 @@
 package it.fulminazzo.yagl.handlers;
 
+import io.netty.channel.ChannelHandlerContext;
+import lombok.Getter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.logging.Logger;
 
@@ -12,6 +16,7 @@ class AnvilRenameHandlerTest {
 
     private Logger logger;
     private Player player;
+    private ChannelHandlerContext context;
 
     private String lastRead;
 
@@ -21,11 +26,33 @@ class AnvilRenameHandlerTest {
     void setUp() {
         this.logger = Logger.getLogger(getClass().getSimpleName());
         this.player = mock(Player.class);
+        this.context = mock(ChannelHandlerContext.class);
 
         this.handler = new AnvilRenameHandler(
                 this.logger, this.player,
                 (p, n) -> this.lastRead = n
         );
+    }
+
+    @Test
+    void testRead() throws Exception {
+        String expected = "Hello, world!";
+
+        Object packet = new ServerboundRenameItemPacket(expected);
+
+        this.handler.channelRead(this.context, packet);
+
+        assertEquals(expected, this.lastRead);
+    }
+
+    @Getter
+    static class ServerboundRenameItemPacket {
+        private final @NotNull String name;
+
+        ServerboundRenameItemPacket(@NotNull String name) {
+            this.name = name;
+        }
+
     }
 
 }
