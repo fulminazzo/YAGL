7816e2af12d90fc1e29f4fae7368d6b45caebb99
user <fulminazzo21@gmail.com>
2025-06-16 16:10:01 +0200

    Updated tests

--- gui/bukkit/src/test/java/it/fulminazzo/yagl/handlers/AnvilRenameHandlerTest.java
+++ gui/bukkit/src/test/java/it/fulminazzo/yagl/handlers/AnvilRenameHandlerTest.java
@@ -3,16 +3,24 @@ package it.fulminazzo.yagl.handlers;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import it.fulminazzo.jbukkit.BukkitUtils;
+import it.fulminazzo.yagl.utils.BukkitTestUtils;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class AnvilRenameHandlerTest {
 
@@ -23,6 +31,19 @@ class AnvilRenameHandlerTest {
     private String lastRead;
 
     private AnvilRenameHandler handler;
+
+    @BeforeAll
+    static void setUpAll() {
+        BukkitUtils.setupServer();
+        Server server = Bukkit.getServer();
+        BukkitScheduler scheduler = mock(BukkitScheduler.class);
+        when(scheduler.runTaskAsynchronously(any(), any(Runnable.class))).thenAnswer(a -> {
+            Runnable runnable = a.getArgument(1);
+            runnable.run();
+            return null;
+        });
+        when(server.getScheduler()).thenReturn(scheduler);
+    }
 
     @BeforeEach
     void setUp() {
@@ -37,14 +58,20 @@ class AnvilRenameHandlerTest {
     }
 
     @Test
-    void testObsoleteRead() throws Exception {
-        String expected = "Hello, world!";
+    void testObsoleteRead() {
+        BukkitTestUtils.mockPlugin(p -> {
+            try {
+                String expected = "Hello, world!";
 
-        Object packet = new PacketPlayInCustomPayload("MC|ItemName", expected);
+                Object packet = new PacketPlayInCustomPayload("MC|ItemName", expected);
 
-        this.handler.channelRead(this.context, packet);
+                this.handler.channelRead(this.context, packet);
 
-        assertEquals(expected, this.lastRead);
+                assertEquals(expected, this.lastRead);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Test
@@ -57,25 +84,37 @@ class AnvilRenameHandlerTest {
     }
 
     @Test
-    void testLegacyRead() throws Exception {
-        String expected = "Hello, world!";
+    void testLegacyRead() {
+        BukkitTestUtils.mockPlugin(p -> {
+            try {
+                String expected = "Hello, world!";
 
-        Object packet = new PacketPlayInItemName(expected);
+                Object packet = new PacketPlayInItemName(expected);
 
-        this.handler.channelRead(this.context, packet);
+                this.handler.channelRead(this.context, packet);
 
-        assertEquals(expected, this.lastRead);
+                assertEquals(expected, this.lastRead);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Test
-    void testRead() throws Exception {
-        String expected = "Hello, world!";
+    void testRead() {
+        BukkitTestUtils.mockPlugin(p -> {
+            try {
+                String expected = "Hello, world!";
 
-        Object packet = new ServerboundRenameItemPacket(expected);
+                Object packet = new ServerboundRenameItemPacket(expected);
 
-        this.handler.channelRead(this.context, packet);
+                this.handler.channelRead(this.context, packet);
 
-        assertEquals(expected, this.lastRead);
+                assertEquals(expected, this.lastRead);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Test
