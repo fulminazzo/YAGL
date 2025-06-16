c580f3a67815b2cf0ffa6b982637c591956abcf5
user <fulminazzo21@gmail.com>
2025-06-16 21:16:50 +0200

    Removed unused getProvidingPlugin method

--- gui/bukkit/src/main/java/it/fulminazzo/yagl/handlers/AnvilRenameHandler.java
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/handlers/AnvilRenameHandler.java
@@ -9,7 +9,6 @@ import it.fulminazzo.fulmicollection.objects.Refl;
 import it.fulminazzo.yagl.utils.NMSUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
@@ -144,10 +143,6 @@ public final class AnvilRenameHandler extends ChannelDuplexHandler {
         if (player == null)
             throw new IllegalStateException(String.format("Player '%s' is not online", this.playerId));
         return player;
-    }
-
-    private static @NotNull JavaPlugin getProvidingPlugin() {
-        return JavaPlugin.getProvidingPlugin(AnvilRenameHandler.class);
     }
 
 }
