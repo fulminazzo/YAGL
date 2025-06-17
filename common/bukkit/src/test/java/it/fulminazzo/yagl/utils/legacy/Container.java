c349b55746404765ba69a4e0c6b6d7d572b25d0a
user <fulminazzo21@gmail.com>
2025-06-17 18:39:52 +0200

    Created ObfuscatedContainers enum for testing purposes

--- common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/Container.java
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/Container.java
@@ -79,4 +79,24 @@ public class Container {
 
     }
 
+    @Getter
+    public enum ObfuscatedContainers implements Containers {
+
+        A(9, InventoryType.CHEST),
+        B(18, InventoryType.CHEST),
+        C(27, InventoryType.CHEST),
+        D(36, InventoryType.CHEST),
+        E(45, InventoryType.CHEST),
+        F(54, InventoryType.CHEST);
+
+        private final int size;
+        private final @NotNull InventoryType inventoryType;
+
+        ObfuscatedContainers(final int size, final @NotNull InventoryType inventoryType) {
+            this.size = size;
+            this.inventoryType = inventoryType;
+        }
+
+    }
+
 }
