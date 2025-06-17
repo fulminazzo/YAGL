7e6e04722a7b780df541aa67d8e3a72baca002cd
user <fulminazzo21@gmail.com>
2025-06-17 19:32:00 +0200

    Created LegacyContainer for testing purposes

---
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/LegacyContainer.java
@@ -0,0 +1,14 @@
+package it.fulminazzo.yagl.utils.legacy;
+
+import lombok.Getter;
+import org.bukkit.craftbukkit.v1_14_R1.util.CraftChatMessage;
+
+@Getter
+public final class LegacyContainer extends Container {
+    private final String title;
+
+    public LegacyContainer(String title) {
+        this.title = CraftChatMessage.fromString(title)[0];
+    }
+
+}
