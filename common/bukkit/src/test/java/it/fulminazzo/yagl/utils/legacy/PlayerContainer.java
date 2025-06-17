57b91f4e008af46e12d0142853b71e0aaa7e2ca7
user <fulminazzo21@gmail.com>
2025-06-17 19:42:04 +0200

    Created PlayerContainer class

---
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/PlayerContainer.java
@@ -0,0 +1,21 @@
+package it.fulminazzo.yagl.utils.legacy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlayerContainer extends Container {
+    private final Container delegate;
+
+    private String cachedTitle;
+
+    public PlayerContainer() {
+        this.delegate = new Container(
+                DefaultContainers.GENERIC_9x3,
+                new ObsoleteContainer(""),
+                null
+        );
+    }
+
+}
