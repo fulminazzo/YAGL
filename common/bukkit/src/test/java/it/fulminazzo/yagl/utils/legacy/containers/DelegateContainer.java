4c239cf3236e70269258e1b5bf16277eeb20321b
user <fulminazzo21@gmail.com>
2025-06-17 19:56:48 +0200

    Added cachedTitle field

--- common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/containers/DelegateContainer.java
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/containers/DelegateContainer.java
@@ -1,16 +1,20 @@
 package it.fulminazzo.yagl.utils.legacy.containers;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public final class DelegateContainer extends Container {
     private final Container delegate;
+    private String cachedTitle;
 
     public DelegateContainer(String title) {
         this.delegate = new Container(
                 DefaultContainers.GENERIC_9x3,
                 new ObsoleteContainer(title)
         );
+        this.cachedTitle = title;
     }
 
 }
