d5b17697bbe007712446e6bd82348ed7e28b60c0
user <fulminazzo21@gmail.com>
2025-06-17 19:25:28 +0200

    Created LegacyMockInventoryView for testing purposes

---
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/LegacyMockInventoryView.java
@@ -0,0 +1,28 @@
+package it.fulminazzo.yagl.utils.legacy;
+
+import it.fulminazzo.jbukkit.inventory.MockInventoryView;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class LegacyMockInventoryView extends MockInventoryView {
+    private final Container container;
+
+    /**
+     * Instantiates a new Mock inventory view.
+     *
+     * @param topInventory the inventory displayed
+     * @param player       the player viewing it
+     * @param title        the title
+     */
+    public LegacyMockInventoryView(@NotNull Inventory topInventory,
+                                   @NotNull Player player,
+                                   @NotNull String title,
+                                   @NotNull Container container) {
+        super(topInventory, player, title);
+        this.container = container;
+    }
+
+}
