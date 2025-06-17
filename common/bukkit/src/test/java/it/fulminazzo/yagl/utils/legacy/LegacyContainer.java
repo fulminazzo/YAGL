440a5e84aac12677f5bdd7c062d32d4a71490377
user <fulminazzo21@gmail.com>
2025-06-17 19:35:37 +0200

    Updated tests

--- common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/LegacyContainer.java
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/legacy/LegacyContainer.java
@@ -5,7 +5,7 @@ import org.bukkit.craftbukkit.v1_14_R1.util.CraftChatMessage;
 
 @Getter
 public final class LegacyContainer extends Container {
-    private final String title;
+    private final CraftChatMessage.IChatBaseComponent title;
 
     public LegacyContainer(String title) {
         this.title = CraftChatMessage.fromString(title)[0];
