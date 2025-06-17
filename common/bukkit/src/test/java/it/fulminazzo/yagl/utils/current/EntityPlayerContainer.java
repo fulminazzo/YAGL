5f3a7f8417b031dbbba8a05b92d63379d0dada4c
user <fulminazzo21@gmail.com>
2025-06-17 21:07:44 +0200

    Removed unused fields

--- common/bukkit/src/test/java/it/fulminazzo/yagl/utils/current/EntityPlayerContainer.java
+++ common/bukkit/src/test/java/it/fulminazzo/yagl/utils/current/EntityPlayerContainer.java
@@ -1,6 +1,5 @@
 package it.fulminazzo.yagl.utils.current;
 
-import io.netty.channel.Channel;
 import it.fulminazzo.yagl.utils.current.containers.Container;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class EntityPlayerContainer {
-    private final EntityPlayer.ServerGamePacketListenerImpl connection;
-
     private Container container;
-
-    public EntityPlayerContainer(Channel channel) {
-        this.connection = new EntityPlayer.ServerGamePacketListenerImpl(channel);
-    }
 
 }
