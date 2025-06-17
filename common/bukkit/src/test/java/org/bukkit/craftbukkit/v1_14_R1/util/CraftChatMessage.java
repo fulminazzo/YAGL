12b4604e986a9add0dbecc7a7b572f859ecdfdd0
user <fulminazzo21@gmail.com>
2025-06-17 19:34:35 +0200

    Updated CraftChatMessage to use internal IChatBaseComponent mock class instead of String as return type

--- common/bukkit/src/test/java/org/bukkit/craftbukkit/v1_14_R1/util/CraftChatMessage.java
+++ common/bukkit/src/test/java/org/bukkit/craftbukkit/v1_14_R1/util/CraftChatMessage.java
@@ -2,10 +2,29 @@ package org.bukkit.craftbukkit.v1_14_R1.util;
 
 public class CraftChatMessage {
 
-    public static String[] fromString(final String message) {
-        return new String[]{
-                "IChatBaseComponent{" + message + "}"
+    public static IChatBaseComponent[] fromString(final String message) {
+        return new IChatBaseComponent[]{
+                new IChatBaseComponent(message)
         };
+    }
+
+    public static class IChatBaseComponent {
+        private final String message;
+
+        private IChatBaseComponent(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o instanceof IChatBaseComponent && this.message.equals(((IChatBaseComponent) o).message);
+        }
+
+        @Override
+        public String toString() {
+            return "IChatBaseComponent{" + this.message + "}";
+        }
+
     }
 
 }
