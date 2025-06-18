906875db9dcda339dec43b5b3498f3b52ff5ca31
user <fulminazzo21@gmail.com>
2025-06-18 20:44:35 +0200

    Fixed invalid creation of corner items

--- demo/src/main/resources/commands/OpenSearchGUI.groovy
+++ demo/src/main/resources/commands/OpenSearchGUI.groovy
@@ -13,8 +13,8 @@ def run = { CommandSender sender, String label, String[] args ->
     if (sender instanceof Player)
         try {
             def corner = (NMSUtils.serverVersion >= 13 ?
-                    ItemGUIContent.newInstance(Material.BLACK_STAINED_GLASS_PANE) :
-                    ItemGUIContent.newInstance(Material.valueOf('STAINED_GLASS_PANE')).setDurability(15))
+                    ItemGUIContent.newInstance(Material.BLACK_STAINED_GLASS_PANE.name()) :
+                    ItemGUIContent.newInstance(Material.valueOf('STAINED_GLASS_PANE').name()).setDurability(15))
                     .setDisplayName(' ')
 
             def data = Arrays.stream(Material.values())
