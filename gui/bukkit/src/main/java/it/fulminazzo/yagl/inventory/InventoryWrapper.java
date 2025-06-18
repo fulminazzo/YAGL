20b8f1ff55f89e76156b43065e6ee63e31c63bf5
user <fulminazzo21@gmail.com>
2025-06-18 15:25:23 +0200

    Created InventoryWrapper with default initialization functions

---
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/inventory/InventoryWrapper.java
@@ -0,0 +1,80 @@
+package it.fulminazzo.yagl.inventory;
+
+import it.fulminazzo.yagl.exceptions.NotImplemented;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A wrapper for {@link Inventory} objects.
+ */
+public interface InventoryWrapper {
+
+    /**
+     * Opens the inventory for the given player.
+     *
+     * @param player the player
+     */
+    void open(final @NotNull Player player);
+
+    /**
+     * Gets the actual bukkit inventory.
+     *
+     * @return the actual inventory
+     */
+    @NotNull Inventory getActualInventory();
+
+    /**
+     * Creates a new inventory wrapper.
+     *
+     * @param player the owner of the inventory
+     * @param size   the size
+     * @return the inventory wrapper
+     */
+    static @NotNull InventoryWrapper createInventory(final @NotNull Player player,
+                                                     final int size) {
+        throw new NotImplemented();
+    }
+
+    /**
+     * Creates a new inventory wrapper.
+     *
+     * @param player the owner of the inventory
+     * @param type   the type of the inventory
+     * @return the inventory wrapper
+     */
+    static @NotNull InventoryWrapper createInventory(final @NotNull Player player,
+                                                     final @NotNull InventoryType type) {
+        throw new NotImplemented();
+    }
+
+    /**
+     * Creates a new inventory wrapper.
+     *
+     * @param player the owner of the inventory
+     * @param size   the size
+     * @param title  the title
+     * @return the inventory wrapper
+     */
+    static @NotNull InventoryWrapper createInventory(final @NotNull Player player,
+                                                     final int size,
+                                                     final @NotNull String title) {
+        throw new NotImplemented();
+    }
+
+    /**
+     * Creates a new inventory wrapper.
+     *
+     * @param player the owner of the inventory
+     * @param type   the type of the inventory
+     * @param title  the title
+     * @return the inventory wrapper
+     */
+    static @NotNull InventoryWrapper createInventory(final @NotNull Player player,
+                                                     final @NotNull InventoryType type,
+                                                     final @NotNull String title) {
+        throw new NotImplemented();
+    }
+
+}
