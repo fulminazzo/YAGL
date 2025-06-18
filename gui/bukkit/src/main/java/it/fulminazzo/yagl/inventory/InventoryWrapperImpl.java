d2b1ecb4e35e1051bdc3cf205e42825120305306
user <fulminazzo21@gmail.com>
2025-06-18 15:27:59 +0200

    Created InventoryWrapperImpl with implementations of functions

---
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/inventory/InventoryWrapperImpl.java
@@ -0,0 +1,29 @@
+package it.fulminazzo.yagl.inventory;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A basic {@link InventoryWrapper} that simply wraps the provided inventory.
+ */
+@Getter
+class InventoryWrapperImpl implements InventoryWrapper {
+    private final @NotNull Inventory actualInventory;
+
+    /**
+     * Instantiates a new Inventory wrapper.
+     *
+     * @param actualInventory the actual inventory
+     */
+    public InventoryWrapperImpl(final @NotNull Inventory actualInventory) {
+        this.actualInventory = actualInventory;
+    }
+
+    @Override
+    public void open(final @NotNull Player player) {
+        player.openInventory(this.actualInventory);
+    }
+
+}
