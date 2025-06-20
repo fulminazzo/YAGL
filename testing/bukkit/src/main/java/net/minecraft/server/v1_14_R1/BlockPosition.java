689df0340dd12100efe463d6d9ee8159255dad97
user <fulminazzo21@gmail.com>
2025-06-20 23:18:13 +0200

    Created BlockPosition for testing purposes

---
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/BlockPosition.java
@@ -0,0 +1,14 @@
+package net.minecraft.server.v1_14_R1;
+
+public class BlockPosition {
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public BlockPosition(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
