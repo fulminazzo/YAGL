4e46122aad88616e02c7e27c9cacd20e4644f6c4
user <fulminazzo21@gmail.com>
2025-06-20 22:38:33 +0200

    Added getBukkitView method

--- testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_14_R1/inventory/CraftContainer.java
+++ testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_14_R1/inventory/CraftContainer.java
@@ -1,10 +1,12 @@
 package org.bukkit.craftbukkit.v1_14_R1.inventory;
 
+import it.fulminazzo.jbukkit.inventory.MockInventoryView;
 import lombok.Getter;
 import net.minecraft.server.v1_14_R1.Container;
 import net.minecraft.server.v1_14_R1.ContainerAnvil;
 import net.minecraft.server.v1_14_R1.EntityPlayer;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 
 @Getter
 public class CraftContainer {
@@ -17,6 +19,10 @@ public class CraftContainer {
         this.inventory = inventory;
         this.owner = owner;
         this.delegate = new ContainerAnvil(windowId);
+    }
+
+    public InventoryView getBukkitView() {
+        return new MockInventoryView(this.inventory, this.owner.getPlayer(), "Hello, world!");
     }
 
 }
