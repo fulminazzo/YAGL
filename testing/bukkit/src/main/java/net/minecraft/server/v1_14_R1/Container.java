c4797aac4b2c25eac0b8baddbf5fda79c72456e4
user <fulminazzo21@gmail.com>
2025-06-20 22:33:54 +0200

    Added items and slotListener fields with related methods

--- testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/Container.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/Container.java
@@ -4,11 +4,14 @@ import it.fulminazzo.jbukkit.inventory.MockInventoryView;
 import lombok.Getter;
 import net.minecraft.server.v1_14_R1.containers.Containers;
 import net.minecraft.server.v1_14_R1.containers.DefaultContainers;
+import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static org.mockito.Mockito.mock;
@@ -19,6 +22,10 @@ public class Container {
     private final Container container;
 
     private final int windowId;
+
+    private final List<CraftItemStack> items;
+
+    private final List<EntityPlayer> slotListener;
 
     private InventoryView bukkitView;
 
@@ -42,6 +49,9 @@ public class Container {
         this.type = type;
         this.container = container;
         this.windowId = windowId;
+        this.items = new ArrayList<>();
+        for (int i = 0; i < type.getSize(); i++) this.items.add(null);
+        this.slotListener = new ArrayList<>();
     }
 
     public void setBukkitView(InventoryView bukkitView) {
@@ -50,6 +60,14 @@ public class Container {
 
     public void setBukkitView(final @NotNull Inventory inventory) {
         this.bukkitView = new MockInventoryView(inventory, mock(Player.class), "");
+    }
+
+    public void setItem(final int slot, CraftItemStack itemStack) {
+        this.items.set(slot, itemStack);
+    }
+
+    public void addSlotListener(EntityPlayer entityPlayer) {
+        this.slotListener.add(entityPlayer);
     }
 
 }
