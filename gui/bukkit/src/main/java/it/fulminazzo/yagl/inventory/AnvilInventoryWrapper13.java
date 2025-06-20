b992e03a004063e56597ed01bd845ed5b2f00798
user <fulminazzo21@gmail.com>
2025-06-21 01:53:53 +0200

    Overridden itemStackToNMSItem to add support for SearchGUI items

--- gui/bukkit/src/main/java/it/fulminazzo/yagl/inventory/AnvilInventoryWrapper13.java
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/inventory/AnvilInventoryWrapper13.java
@@ -1,6 +1,11 @@
 package it.fulminazzo.yagl.inventory;
 
+import it.fulminazzo.fulmicollection.objects.Refl;
+import it.fulminazzo.yagl.guis.SearchGUI;
+import it.fulminazzo.yagl.utils.NMSUtils;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,6 +20,27 @@ class AnvilInventoryWrapper13 extends AnvilInventoryWrapper12 {
      */
     public AnvilInventoryWrapper13(final @NotNull Inventory actualInventory) {
         super(actualInventory);
+    }
+
+    @Override
+    @NotNull Object itemStackToNMSItem(ItemStack item) {
+        Object nmsItem = super.itemStackToNMSItem(item);
+        Refl<?> nmsStack = new Refl<>(nmsItem);
+        ItemMeta meta = item.getItemMeta();
+        if (meta != null) {
+            String name = meta.getDisplayName();
+            // If the item has EMPTY_RENAME_TEXT as name, set an empty name instead.
+            if (SearchGUI.EMPTY_RENAME_TEXT.equals(name)) {
+                Refl<?> tagName = new Refl<>(NMSUtils.getLegacyNMSClass("NBTTagString"), "{\"text\":\"\"}");
+
+                Refl<?> tagDisplay = new Refl<>(NMSUtils.getLegacyNMSClass("NBTTagCompound"), new Object[0])
+                        .callMethod("set", "Name", tagName.getObject());
+
+                nmsStack.invokeMethodRefl("getOrCreateTag")
+                        .invokeMethod("set", "display", tagDisplay.getObject());
+            }
+        }
+        return nmsItem;
     }
 
 }
