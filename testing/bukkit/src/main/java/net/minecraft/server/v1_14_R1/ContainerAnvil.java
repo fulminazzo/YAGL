2c040cd6c34143c3613114cbe2aa6231f768dc42
user <fulminazzo21@gmail.com>
2025-06-20 22:34:05 +0200

    Created ContainerAnvil class implementation of Container for anvils

---
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/ContainerAnvil.java
@@ -0,0 +1,11 @@
+package net.minecraft.server.v1_14_R1;
+
+import net.minecraft.server.v1_14_R1.containers.DefaultContainers;
+
+public class ContainerAnvil extends Container {
+
+    public ContainerAnvil(int windowId) {
+        super(DefaultContainers.ANVIL, windowId);
+    }
+
+}
