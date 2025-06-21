31531fb8ebdc18126786cac0ad5e7be8954b5090
user <fulminazzo21@gmail.com>
2025-06-21 13:05:43 +0200

    Removed unused ContainerAnvil class

--- testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/ContainerAnvil.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.server.v1_8_R3;
-
-public class ContainerAnvil extends Container {
-
-    public ContainerAnvil(int windowId) {
-        super("minecraft:anvil", 3, windowId);
-    }
-
-}
