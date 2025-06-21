29a97ba07e241625127f5add5a98ba8b264c1a14
user <fulminazzo21@gmail.com>
2025-06-21 13:04:45 +0200

    Fixed constructor

--- testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftContainer.java
+++ testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftContainer.java
@@ -4,24 +4,24 @@ import it.fulminazzo.jbukkit.inventory.MockInventoryView;
 import lombok.Getter;
 import net.minecraft.server.v1_8_R3.Container;
 import net.minecraft.server.v1_8_R3.ContainerAnvil;
-import net.minecraft.server.v1_8_R3.EntityPlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 @Getter
 public class CraftContainer extends Container {
     private final Inventory inventory;
-    private final EntityPlayer owner;
+    private final Player owner;
 
-    public CraftContainer(Inventory inventory, EntityPlayer owner, int windowId) {
+    public CraftContainer(Inventory inventory, Player owner, int windowId) {
         this(inventory, owner, windowId, new ContainerAnvil(windowId));
     }
 
-    public CraftContainer(Inventory inventory, EntityPlayer owner, int windowId, Container delegate) {
+    public CraftContainer(Inventory inventory, Player owner, int windowId, Container delegate) {
         super(delegate.getType(), 0, windowId);
         this.inventory = inventory;
         this.owner = owner;
 
-        setBukkitView(new MockInventoryView(this.inventory, this.owner.getPlayer(), "Hello, world!"));
+        setBukkitView(new MockInventoryView(this.inventory, this.owner, "Hello, world!"));
     }
 
 }
