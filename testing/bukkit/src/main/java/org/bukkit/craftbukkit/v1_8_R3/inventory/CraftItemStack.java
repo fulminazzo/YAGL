39118286307690f081d58782cf004b78d8a9247b
user <fulminazzo21@gmail.com>
2025-06-21 13:18:45 +0200

    Fixed invalid return of asNMSCopy function

--- testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftItemStack.java
+++ testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftItemStack.java
@@ -1,11 +1,11 @@
 package org.bukkit.craftbukkit.v1_8_R3.inventory;
 
 import it.fulminazzo.fulmicollection.objects.FieldEquable;
-import org.bukkit.inventory.ItemStack;
+import net.minecraft.server.v1_8_R3.ItemStack;
 
 public class CraftItemStack extends FieldEquable {
 
-    public static ItemStack asNMSCopy(ItemStack itemStack) {
+    public static ItemStack asNMSCopy(org.bukkit.inventory.ItemStack itemStack) {
         return new ItemStack(itemStack.getType(), itemStack.getAmount());
     }
 
