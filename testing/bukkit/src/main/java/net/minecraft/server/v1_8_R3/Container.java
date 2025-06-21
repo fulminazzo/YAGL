d07fb0a9aa4e04ba4b080e7ad66b2d218ba69398
user <fulminazzo21@gmail.com>
2025-06-21 12:58:16 +0200

    Added windowId and constructor

--- testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/Container.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/Container.java
@@ -1,4 +1,19 @@
 package net.minecraft.server.v1_8_R3;
 
+import lombok.Getter;
+
+import java.util.Random;
+
+@Getter
 public class Container {
+    private final int windowId;
+
+    public Container() {
+        this(new Random().nextInt());
+    }
+
+    public Container(int windowId) {
+        this.windowId = windowId;
+    }
+
 }
