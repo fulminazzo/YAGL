e17fde00b3b7b768c5678f353ee44dd3f08723de
user <fulminazzo21@gmail.com>
2025-06-21 12:57:04 +0200

    Added PlayerConnection class and field

--- testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/EntityPlayer.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/EntityPlayer.java
@@ -4,11 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 public class EntityPlayer {
     private final Player player;
     private final Container defaultContainer;
+
+    private final PlayerConnection playerConnection;
 
     private Container playerContainer;
 
@@ -18,8 +23,25 @@ public class EntityPlayer {
 
     public EntityPlayer(Player player) {
         this.player = player;
+
+        this.playerConnection = new PlayerConnection();
+
         this.defaultContainer = new Container();
         this.playerContainer = new Container();
+    }
+    
+    @Getter
+    public static class PlayerConnection {
+        private final List<Packet> sentPackets;
+
+        public PlayerConnection() {
+            this.sentPackets = new ArrayList<>();
+        }
+
+        public void sendPacket(Packet packet) {
+            this.sentPackets.add(packet);
+        }
+
     }
 
 }
