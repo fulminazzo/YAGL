b7fc60e0652edf0bf7a868e264f4d0995ade5db6
user <fulminazzo21@gmail.com>
2025-06-21 13:00:45 +0200

    Added items and slotListeners fields and updated constructor

--- testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/Container.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/Container.java
@@ -2,8 +2,11 @@ package net.minecraft.server.v1_8_R3;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.InventoryView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Getter
@@ -13,15 +16,22 @@ public class Container {
 
     private final int windowId;
 
+    private final List<CraftItemStack> items;
+
+    private final List<EntityPlayer> slotListeners;
+
     private InventoryView bukkitView;
 
     public Container() {
-        this("minecraft:chest", new Random().nextInt());
+        this("minecraft:chest", 27, new Random().nextInt());
     }
 
-    public Container(String type, int windowId) {
+    public Container(String type, int size, int windowId) {
         this.type = type;
         this.windowId = windowId;
+        this.items = new ArrayList<>();
+        for (int i = 0; i < size; i++) this.items.add(null);
+        this.slotListeners = new ArrayList<>();
     }
 
 }
