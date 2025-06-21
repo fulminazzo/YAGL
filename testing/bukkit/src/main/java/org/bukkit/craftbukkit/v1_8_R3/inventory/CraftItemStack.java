2031288b5bdd2a0d78c9d9f3d19be7a250fefde4
user <fulminazzo21@gmail.com>
2025-06-21 13:12:30 +0200

    Made asNMSCopy return NMS ItemStack

--- testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftItemStack.java
+++ testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/inventory/CraftItemStack.java
@@ -1,22 +1,12 @@
 package org.bukkit.craftbukkit.v1_8_R3.inventory;
 
 import it.fulminazzo.fulmicollection.objects.FieldEquable;
-import lombok.Getter;
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-@Getter
 public class CraftItemStack extends FieldEquable {
-    private final Material material;
-    private final int amount;
 
-    public CraftItemStack(Material material, int amount) {
-        this.material = material;
-        this.amount = amount;
-    }
-
-    public static CraftItemStack asNMSCopy(ItemStack itemStack) {
-        return new CraftItemStack(itemStack.getType(), itemStack.getAmount());
+    public static ItemStack asNMSCopy(ItemStack itemStack) {
+        return new ItemStack(itemStack.getType(), itemStack.getAmount());
     }
 
 }
