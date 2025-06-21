0783eb48b80715110b4d20ef8b27eb2c693e2909
user <fulminazzo21@gmail.com>
2025-06-21 17:12:24 +0200

    Added player field and updated constructors

--- gui/bukkit/src/main/java/it/fulminazzo/yagl/metadatable/PAPIParser.java
+++ gui/bukkit/src/main/java/it/fulminazzo/yagl/metadatable/PAPIParser.java
@@ -1,6 +1,7 @@
 package it.fulminazzo.yagl.metadatable;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
@@ -11,28 +12,34 @@ import java.util.Collections;
  * variables to the specified object.
  */
 public final class PAPIParser extends MetadatableHelper {
+    private final @NotNull Player player;
 
     /**
      * Instantiates a new PAPIParser helper.
+     *
+     * @param player the player
      */
-    PAPIParser() {
+    PAPIParser(final @NotNull Player player) {
         super(Collections::emptyMap);
+        this.player = player;
     }
 
     @Override
     String apply(@NotNull String string) {
-        return PlaceholderAPI.setPlaceholders(null, string);
+        return PlaceholderAPI.setPlaceholders(this.player, string);
     }
 
     /**
-     * Parses the given object strings using PlaceholderAPI.
+     * Parses the given object strings using {@link PlaceholderAPI}.
      *
      * @param <T>    the type of the object
+     * @param player the target player of the placeholders
      * @param object the object
      * @return the parsed object
      */
-    public static <T> T parse(final @NotNull T object) {
-        return new PAPIParser().apply(object);
+    public static <T> T parse(final @NotNull Player player,
+                              final @NotNull T object) {
+        return new PAPIParser(player).apply(object);
     }
 
 }
