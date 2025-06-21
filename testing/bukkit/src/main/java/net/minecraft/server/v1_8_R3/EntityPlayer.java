924c6313f7522584bd41586c90e6a705ceab4036
user <fulminazzo21@gmail.com>
2025-06-21 12:56:04 +0200

    Updated EntityPlayer with local Container class and player field

--- testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/EntityPlayer.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_8_R3/EntityPlayer.java
@@ -2,16 +2,22 @@ package net.minecraft.server.v1_8_R3;
 
 import lombok.Getter;
 import lombok.Setter;
-import net.minecraft.server.v1_14_R1.Container;
+import org.bukkit.entity.Player;
 
 @Getter
 @Setter
 public class EntityPlayer {
+    private final Player player;
     private final Container defaultContainer;
 
     private Container playerContainer;
 
     public EntityPlayer() {
+        this(null);
+    }
+
+    public EntityPlayer(Player player) {
+        this.player = player;
         this.defaultContainer = new Container();
         this.playerContainer = new Container();
     }
