f5ea2cf6cba242fdbe2292c2d2f0657bee909bc6
user <fulminazzo21@gmail.com>
2025-06-22 19:01:41 +0200

    Added missing windowId field

--- testing/bukkit/src/main/java/net/minecraft/world/inventory/AbstractContainerMenu.java
+++ testing/bukkit/src/main/java/net/minecraft/world/inventory/AbstractContainerMenu.java
@@ -1,6 +1,7 @@
 package net.minecraft.world.inventory;
 
 public class AbstractContainerMenu {
+    private int windowId;
 
     public static class PlayerContainerMenu extends AbstractContainerMenu {
 
