35eb01bb159999e072e6e220bd1a4a2d4dffac64
user <fulminazzo21@gmail.com>
2025-06-22 21:36:34 +0200

    Created CraftTask for testing purposes

---
+++ testing/bukkit/src/main/java/org/bukkit/craftbukkit/v1_8_R3/scheduler/CraftTask.java
@@ -0,0 +1,21 @@
+package org.bukkit.craftbukkit.v1_8_R3.scheduler;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+
+@Getter
+@Setter
+public class CraftTask implements BukkitTask {
+    private Plugin owner;
+    private int taskId;
+    private boolean sync;
+    private long period;
+
+    @Override
+    public void cancel() {
+        this.period = -2;
+    }
+
+}
