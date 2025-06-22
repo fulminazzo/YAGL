36a4336e21dbbe161dc8f9e53416480f8049be70
user <fulminazzo21@gmail.com>
2025-06-22 23:06:21 +0200

    Removed InventoryType field and replaced constructors with raw string. Rewritten getInventoryType

--- testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/containers/ObfuscatedContainers.java
+++ testing/bukkit/src/main/java/net/minecraft/server/v1_14_R1/containers/ObfuscatedContainers.java
@@ -7,19 +7,23 @@ import org.jetbrains.annotations.NotNull;
 @Getter
 public enum ObfuscatedContainers implements Containers {
 
-    A(9, InventoryType.CHEST),
-    B(18, InventoryType.CHEST),
-    C(27, InventoryType.CHEST),
-    D(36, InventoryType.CHEST),
-    E(45, InventoryType.CHEST),
-    F(54, InventoryType.CHEST);
+    A(9, "CHEST"),
+    B(18, "CHEST"),
+    C(27, "CHEST"),
+    D(36, "CHEST"),
+    E(45, "CHEST"),
+    F(54, "CHEST");
 
     private final int size;
-    private final @NotNull InventoryType inventoryType;
+    private final @NotNull String rawInventoryType;
 
-    ObfuscatedContainers(final int size, final @NotNull InventoryType inventoryType) {
+    ObfuscatedContainers(final int size, final @NotNull String rawInventoryType) {
         this.size = size;
-        this.inventoryType = inventoryType;
+        this.rawInventoryType = rawInventoryType;
+    }
+
+    public @NotNull InventoryType getInventoryType() {
+        return InventoryType.valueOf(this.rawInventoryType);
     }
 
 }
