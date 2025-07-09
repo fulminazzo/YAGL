d4f91974c08d35aef6f0bab374d3dc93c17d55d7
user <fulminazzo21@gmail.com>
2025-07-09 23:57:12 +0200

    Updated GivePotionItem support

--- demo/src/main/resources/commands/GivePotionItem.groovy
+++ demo/src/main/resources/commands/GivePotionItem.groovy
@@ -1,6 +1,7 @@
 import it.fulminazzo.yagl.item.BukkitItem
 import it.fulminazzo.yagl.item.Item
 import it.fulminazzo.yagl.item.field.ItemFlag
+import it.fulminazzo.yagl.util.NMSUtils
 import it.fulminazzo.yagl.wrapper.Enchantment
 import it.fulminazzo.yagl.wrapper.PotionEffect
 import org.bukkit.command.CommandSender
@@ -8,15 +9,17 @@ import org.bukkit.entity.Player
 
 def run = { CommandSender sender, String label, String[] args ->
     if (sender instanceof Player) {
+        def version = NMSUtils.serverVersion
+
         Item item = Item.newItem('potion')
                 .setDisplayName('&6Super Potion')
                 .setLore('&eUse this potion to', '&efeel like Superman!')
-                .addEnchantments(new Enchantment('luck', 1))
-                .addItemFlags(ItemFlag.HIDE_DESTROYS, ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_POTION_EFFECTS)
+                .addEnchantments(new Enchantment('lure', 1))
+                .addItemFlags(ItemFlag.HIDE_DESTROYS, ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS)
                 .addPotionEffects(
-                        new PotionEffect('increase_damage', 30, 3),
+                        new PotionEffect(version < 20 ? 'increase_damage' : 'strength', 30, 3),
                         new PotionEffect('speed', 90, 2),
-                        new PotionEffect('jump', 10, 2)
+                        new PotionEffect(version < 20 ? 'jump' : 'jump_boost', 10, 2)
                 )
                 .setUnbreakable(true)
         sender.inventory.addItem(item.copy(BukkitItem).create())
