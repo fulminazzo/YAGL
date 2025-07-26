af047e54362365f9e65e593e53b80a584c6f5208
user <fulminazzo21@gmail.com>
2025-07-26 20:59:17 +0200

    Added legacy execute(Viewer, GUI, GUIContent) default method

--- gui/base/src/main/java/it/fulminazzo/yagl/action/GUIItemAction.java
+++ gui/base/src/main/java/it/fulminazzo/yagl/action/GUIItemAction.java
@@ -1,7 +1,11 @@
 package it.fulminazzo.yagl.action;
 
 import it.fulminazzo.yagl.SerializableFunction;
+import it.fulminazzo.yagl.content.GUIContent;
 import it.fulminazzo.yagl.event.ClickItemEvent;
+import it.fulminazzo.yagl.event.ClickType;
+import it.fulminazzo.yagl.gui.GUI;
+import it.fulminazzo.yagl.viewer.Viewer;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -16,5 +20,21 @@ public interface GUIItemAction extends SerializableFunction {
      * @param event the event
      */
     void execute(final @NotNull ClickItemEvent event);
+
+    /**
+     * Execute.
+     *
+     * @param viewer  the viewer
+     * @param gui     the gui
+     * @param content the content
+     */
+    default void execute(final @NotNull Viewer viewer, final @NotNull GUI gui, final @NotNull GUIContent content) {
+        execute(ClickItemEvent.builder()
+                .viewer(viewer)
+                .gui(gui)
+                .content(content)
+                .clickType(ClickType.LEFT)
+                .build());
+    }
 
 }
